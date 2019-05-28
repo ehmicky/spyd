5c633503770faa4a40eb2b5f4b803b6a08454f05
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Add tests

--- src/results.js
+++ src/results.js
@@ -1,12 +1,15 @@
-import { getArray } from './array.js'
 import { measure } from './measure.js'
 import { getStats } from './stats.js'
 
 export const getResults = function({ tasks, options, options: { repeat } }) {
-  const loop = getArray(repeat)
+  const loop = Array.from({ length: repeat }, getIndex)
   return Object.entries(tasks).flatMap(([id, task]) =>
     getResult({ id, task, loop, options }),
   )
+}
+
+const getIndex = function(value, index) {
+  return index
 }
 
 const getResult = function({
@@ -16,29 +19,39 @@ const getResult = function({
   loop,
   options,
 }) {
+  const taskA = { ...task, title }
+
   if (variants === undefined) {
-    return getArgResult({ title, main, loop, options })
+    return getArgResult({ title, main, loop, task: taskA, options })
   }
 
-  return Object.entries(variants).map(([variantTitle, args]) =>
-    getArgResult({ title, main, args, variantTitle, loop, task, options }),
+  return Object.entries(variants).map(([variant, variantArgs]) =>
+    getArgResult({
+      title,
+      main,
+      variantArgs,
+      variant,
+      loop,
+      task: taskA,
+      options,
+    }),
   )
 }
 
 const getArgResult = function({
   title,
   main,
-  args,
-  variantTitle,
+  variantArgs,
+  variant,
   loop,
   task,
   options,
 }) {
-  const titleA =
-    variantTitle === undefined ? title : `${title} (${variantTitle})`
+  const titleA = variant === undefined ? title : `${title} (${variant})`
+  const mainA =
+    variantArgs === undefined ? main : main.bind(null, ...variantArgs)
 
-  const mainA = args === undefined ? main : main.bind(null, ...args)
   const durations = loop.map(() => measure(mainA))
   const duration = getStats(durations)
-  return { title: titleA, task, args, duration, options }
+  return { title: titleA, task, variant, duration, options }
 }
---
+++ test/main.js
@@ -0,0 +1,30 @@
+import test from 'ava'
+import testEach from 'test-each'
+
+import checkSpeed from '../src/main.js'
+
+// eslint-disable-next-line no-empty-function
+const main = function() {}
+
+const normalizeResults = function(results) {
+  return results.map(normalizeResult)
+}
+
+const normalizeResult = function({ duration: { average, all }, ...result }) {
+  const averageA = typeof average
+  const allA = all.length
+  return { ...result, duration: { average: averageA, all: allA } }
+}
+
+testEach(
+  [
+    { one: { main }, opts: { repeat: 1 } },
+    { one: { main }, opts: { repeat: 3 } },
+    { one: { main, variants: { blue: ['blue'], green: ['green'] } }, opts: { repeat: 1 } },
+  ],
+  ({ title }, { opts, ...tasks }) => {
+    test(`checkSpeed() | ${title}`, t => {
+      t.snapshot(normalizeResults(checkSpeed(tasks, opts)))
+    })
+  },
+)
