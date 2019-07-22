474bff625b1ca5e00a420d52908e41f3a6a36641
ehmicky <ehmicky@gmail.com>
2019-07-22 10:00:00 +0200

    Small optimization

--- src/measure.js
+++ src/measure.js
@@ -1,12 +1,15 @@
 import { performance } from 'perf_hooks'
 
+// Using dot notation would require few picoseconds
+const { now } = performance
+
 // Measure how long a function takes to execute
 export const measure = function(func) {
-  const start = performance.now()
+  const start = now()
   const returnValue = func()
   // We measure synchronous time right away, because checking if `returnValue`
   // is a promise takes some CPU cycles
-  const end = performance.now()
+  const end = now()
 
   if (!(returnValue instanceof Promise)) {
     return end - start
@@ -14,7 +17,7 @@ export const measure = function(func) {
 
   // eslint-disable-next-line promise/prefer-await-to-then
   return returnValue.then(() => {
-    const asyncEnd = performance.now()
+    const asyncEnd = now()
     return asyncEnd - start
   })
 }
