32be88748163da489daae24c9a71e53bb36d3523
ehmicky <ehmicky@gmail.com>
2019-07-26 10:00:00 +0200

    Improve biases

--- src/temp.js
+++ src/temp.js
@@ -8,29 +8,42 @@ import { getTimeResolution } from './resolution.js'
 // We remove those two biases from the calculated times.
 // This function calculates those biases by benchmarking them.
 // Biases calculation does not account into the spent `duration`.
+// On top of this, the more the benchmarking code itself is run, the faster it
+// is optimized. Calculating biases first performs a cold start so that the
+// benchmarking code is already "hot" when we start the actual measurements.
 export const getBiases = function(duration) {
+  initialMeasure()
+
   const biasDuration = Math.max(
     MIN_BIAS_DURATION,
     duration / BIAS_DURATION_RATIO,
   )
-  const nowBias = benchmark(now, biasDuration, 0, 0, 0, NOW_BIAS_REPEAT)
-  const loopBias = benchmark(noop, biasDuration, 0, 0, 0, LOOP_BIAS_REPEAT)
+  const nowBias = benchmark(noop, biasDuration, 0, 0, 0, 0)
   const minTime = getMinTime(nowBias)
+  const loopBias = benchmark(noop, biasDuration, nowBias, 0, minTime)
   return { nowBias, loopBias, minTime }
 }
 
 const noop = function() {}
 
+// For some reasons I ignore (likely engine optimizations), when `measure()`
+// is benchmarking its first function, it's running much faster than for the
+// next functions passed to it.
+// We fix this by doing a cold start using an empty function
+const initialMeasure = function() {
+  measure(noopTwo, 0, 0, 1)
+}
+
+const noopTwo = function() {}
+
+// TODO: Min good biasDuration for nowBias: 5e7 - 1e8
+// TODO: Min good biasDuration for loopBias: 5e6 - 1e7
 // Biases must be very precise to benchmark fast tasks accurately.
 // So we impose a minimum duration when calculating them.
-const MIN_BIAS_DURATION = 1e8
+const MIN_BIAS_DURATION = 5e6
 // However if the duration is high enough, we spend even more time calculating
 // biases.
 const BIAS_DURATION_RATIO = 1e2
-
-// Hard-code the number of `repeat` when calculating biases
-const NOW_BIAS_REPEAT = 1e3
-const LOOP_BIAS_REPEAT = 1e4
 
 // If a task duration is too close to `nowBias, the returned variance will be
 // mostly due to the timestamp function itself.
@@ -79,7 +92,6 @@ export const benchmark = function(
 //  - stop benchmarking exactly when the `duration` has been reached
 //  - adjust some parameters as we take more measurements (e.g. `repeat`)
 //  - reduce the sample size when we hit the max memory limit
-// eslint-disable-next-line max-statements, complexity
 const benchmarkLoop = function(
   main,
   nowBias,
@@ -122,16 +134,20 @@ const benchmarkLoop = function(
 
 // Main measuring code. If `repeat` is specified, we perform an arithmetic mean.
 const measure = function(main, nowBias, loopBias, repeat) {
+  // When calculating `nowBias`
+  if (repeat === 0) {
+    return Math.abs(now() - now())
+  }
+
   // eslint-disable-next-line fp/no-let
   let count = repeat
   const start = now()
 
-  // We use a do/while loop for speed purpose.
-  // eslint-disable-next-line fp/no-loops
-  do {
+  // We use a while loop for speed purpose.
+  // eslint-disable-next-line no-plusplus, fp/no-mutation, fp/no-loops
+  while (count--) {
     main()
-    // eslint-disable-next-line no-plusplus, fp/no-mutation
-  } while (--count)
+  }
 
   const end = now()
   // The final time might be negative if the task is as fast or faster than the
@@ -178,18 +194,36 @@ const getRepeat = function(
   previousRepeat,
   callibration,
 ) {
-  const repeat = computeRepeat(times, minTime, loopBias, constRepeat)
+  const repeat = computeRepeat(
+    times,
+    minTime,
+    loopBias,
+    constRepeat,
+    previousRepeat,
+  )
   callibrateRepeat(callibration, times, repeat, previousRepeat)
   return repeat
 }
 
-const computeRepeat = function(times, minTime, loopBias, constRepeat) {
+const computeRepeat = function(
+  times,
+  minTime,
+  loopBias,
+  constRepeat,
+  previousRepeat,
+) {
   // `constRepeat` is used during bias calculation to set a fixed `repeat` value
   if (constRepeat !== undefined) {
     return constRepeat
   }
 
   const median = getMedian(times)
+
+  // When calculating `loopBias`, `median` might initially be `0`
+  if (loopBias === 0 && median === 0) {
+    return previousRepeat * 2
+  }
+
   const repeat = Math.ceil(minTime / (median + loopBias))
   return repeat
 }
