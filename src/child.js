ff42285def9c8076235a6e3d8b125bbfee963ef5
ehmicky <ehmicky@gmail.com>
2019-07-30 10:00:00 +0200

    Add parameters

--- src/child.js
+++ src/child.js
@@ -7,10 +7,34 @@ import { sendParentMessage, getParentMessage } from './ipc_helpers.js'
 const run = async function() {
   await sendParentMessage('ready')
 
+  const parameter = undefined
+  const [mainA, beforeA, afterA] = bindParameter(PARAMETERS, parameter, [
+    main,
+    before,
+    after,
+  ])
+
   const duration = await getParentMessage('run')
 
-  const result = await benchmark(main, before, after, duration)
+  const result = await benchmark(mainA, beforeA, afterA, duration)
   await sendParentMessage('result', result)
+}
+
+const bindParameter = function(parameters, parameter, funcs) {
+  if (parameter === undefined) {
+    return funcs
+  }
+
+  const parameterValue = parameters[parameter]
+  return funcs.map(func => bindFunction(func, parameterValue))
+}
+
+const bindFunction = function(func, parameterValue) {
+  if (func === undefined) {
+    return
+  }
+
+  return func.bind(null, parameterValue)
 }
 
 const before = undefined
@@ -18,5 +42,7 @@ const before = undefined
 const main = Math.random
 
 const after = undefined
+
+const PARAMETERS = undefined
 
 run()
