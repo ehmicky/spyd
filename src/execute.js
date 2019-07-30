468d0b6c79a47bd7c00e7698db30b752d324c7c2
ehmicky <ehmicky@gmail.com>
2019-07-30 10:00:00 +0200

    Remove execute.js

--- src/execute.js
+++
@@ -1,8 +0,0 @@
-import { benchmark } from './temp.js'
-import { printStats } from './print.js'
-
-const func = Math.random
-
-Array.from({ length: 10 }, () => benchmark(func, 1e8)).map(
-  (stats, index, allStats) => printStats(stats, allStats),
-)
