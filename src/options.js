63d1f34f123f38090cb87e93b733a13f87a209e9
ehmicky <ehmicky@gmail.com>
2019-07-30 10:00:00 +0200

    Fix duration unit

--- src/options.js
+++ src/options.js
@@ -5,10 +5,20 @@ import { handleConfig } from './config.js'
 export const getOpts = async function(opts = {}) {
   const optsA = await handleConfig(opts)
 
-  return { ...DEFAULT_OPTS, ...optsA }
+  const optsB = { ...DEFAULT_OPTS, ...optsA }
+
+  const optsC = normalizeDuration(optsB)
+  return optsC
 }
 
 const DEFAULT_OPTS = {
   duration: 10,
   cwd: getCwd(),
 }
+
+const normalizeDuration = function({ duration, ...opts }) {
+  const durationA = duration * SECONDS_TO_NANOSECONDS
+  return { ...opts, duration: durationA }
+}
+
+const SECONDS_TO_NANOSECONDS = 1e9
