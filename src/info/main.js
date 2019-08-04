62359b619d2979c8d3fd3b5b1f8600f7321a423f
ehmicky <ehmicky@gmail.com>
2019-08-04 10:00:00 +0200

    Move files

--- src/info/main.js
+++ src/info/main.js
@@ -1,5 +1,5 @@
 import { sortBy } from '../utils/sort.js'
-import { addPrintedStats } from '../stats/print/main.js'
+import { addPrintedStats } from '../print/main.js'
 
 import { getTasks, getParameters } from './group.js'
 import { addFastestIterations } from './fastest.js'
