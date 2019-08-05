9f8abbc936ec80d441ea27a42f6e3f1289813f0e
ehmicky <ehmicky@gmail.com>
2019-08-05 10:00:00 +0200

    Remove duplicate `files`

--- src/iterations/path.js
+++ src/iterations/path.js
@@ -7,7 +7,8 @@ import locatePath from 'locate-path'
 export const getTaskPaths = async function(files, cwd) {
   const filesA = await getDefaultFiles(files, cwd)
   const taskPaths = filesA.map(file => resolve(cwd, file))
-  return taskPaths
+  const taskPathsA = [...new Set(taskPaths)]
+  return taskPathsA
 }
 
 // `files` option defaults to looking up files in the current directory
