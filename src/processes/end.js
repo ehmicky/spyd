08f7666604195b23c004a7e741b6f4e22e375fc7
ehmicky <ehmicky@gmail.com>
2019-08-06 10:00:00 +0200

    Refactoring

--- src/processes/end.js
+++ src/processes/end.js
@@ -1,4 +1,4 @@
-import { getExit, forwardChildError } from './exit.js'
+import pEvent from 'p-event'
 
 // Wait for child process exit. Forward any termination errors.
 export const endChild = async function(child) {
@@ -11,4 +11,40 @@ export const endChild = async function(child) {
   }
 
   forwardChildError(signal, exitCode)
+}
+
+// Wait for child process exit when it's not suppose to.
+// Forward any exit as an early exit error.
+export const failOnExit = async function(child) {
+  const { exitCode, signal } = await getExit(child)
+
+  await reportStderr(child)
+
+  forwardChildError(signal, exitCode)
+}
+
+const getExit = async function(child) {
+  if (child.exitCode !== null || child.signalCode !== null) {
+    return { exitCode: child.exitCode, signal: child.signalCode }
+  }
+
+  const [exitCode, signal] = await pEvent(child, 'exit', { multiArgs: true })
+  return { exitCode, signal }
+}
+
+// If there's an issue with the child process, reports its stderr
+export const reportStderr = async function(child) {
+  const stderr = await child.stderrPromise
+  const stderrA = stderr.trim()
+
+  if (stderrA !== '') {
+    throw new Error(stderrA)
+  }
+}
+
+const forwardChildError = function(signal, exitCode) {
+  const signalStr = signal === null ? '' : ` with ${signal}`
+  const exitCodeStr =
+    exitCode === 0 || exitCode === null ? '' : ` (exit code ${exitCode})`
+  throw new Error(`Child process exited ${signalStr}${exitCodeStr}`)
 }
--- src/processes/ipc.js
+++ src/processes/ipc.js
@@ -2,7 +2,7 @@ import { promisify } from 'util'
 
 import pEvent from 'p-event'
 
-import { failOnExit, reportStderr } from './exit.js'
+import { failOnExit, reportStderr } from './end.js'
 
 // Receive IPC message from parent to child
 export const getChildMessage = async function(child, eventName) {
