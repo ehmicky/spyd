1f78006f718bd1633d808034e3dcb4d59de677bb
ehmicky <ehmicky@gmail.com>
2019-08-06 10:00:00 +0200

    Refactoring

---
+++ src/iterations/load.js
@@ -0,0 +1,49 @@
+import { getRunners } from '../run/main.js'
+import { loadTaskFile } from '../processes/load.js'
+
+// Load iterations by launching each runner
+export const loadIterations = async function(taskPaths, requireOpt) {
+  const promises = taskPaths.map(taskPath => loadFiles(taskPath, requireOpt))
+  const iterations = await Promise.all(promises)
+  const iterationsA = iterations.flat()
+  return iterationsA
+}
+
+const loadFiles = async function(taskPath, requireOpt) {
+  const runners = getRunners(taskPath)
+  const promises = runners.map(runner =>
+    loadFile({ taskPath, runner, requireOpt }),
+  )
+  const iterations = await Promise.all(promises)
+  const iterationsA = iterations.flat()
+  return iterationsA
+}
+
+const loadFile = async function({
+  taskPath,
+  runner,
+  runner: { command },
+  requireOpt,
+}) {
+  const iterations = await loadTaskFile({ taskPath, command, requireOpt })
+  return iterations.map(iteration =>
+    normalizeIteration(iteration, runner, taskPath),
+  )
+}
+
+const normalizeIteration = function(
+  { taskId, taskTitle = taskId, variationId, variationTitle = variationId },
+  { id: runnerId, title: runnerTitle = runnerId, command },
+  taskPath,
+) {
+  return {
+    taskPath,
+    taskId,
+    taskTitle,
+    variationId,
+    variationTitle,
+    runnerId,
+    runnerTitle,
+    command,
+  }
+}
--- src/iterations/main.js
+++ src/iterations/main.js
@@ -1,7 +1,5 @@
-import { getRunners } from '../run/main.js'
-import { loadTaskFile } from '../processes/load.js'
-
 import { getTaskPaths } from './path.js'
+import { loadIterations } from './load.js'
 import { selectIterations } from './select.js'
 import { addPaddings } from './paddings.js'
 
@@ -15,26 +13,9 @@ export const getIterations = async function({
 }) {
   const taskPaths = await getTaskPaths(files, cwd)
 
-  const iterations = await loadIterations({
-    taskPaths,
-    taskIds,
-    variationIds,
-    requireOpt,
-  })
+  const iterations = await loadIterations(taskPaths, requireOpt)
 
-  const iterationsA = addPaddings(iterations)
-  return iterationsA
-}
-
-const loadIterations = async function({
-  taskPaths,
-  taskIds,
-  variationIds,
-  requireOpt,
-}) {
-  const promises = taskPaths.map(taskPath => loadFiles(taskPath, requireOpt))
-  const iterations = await Promise.all(promises)
-  const iterationsA = iterations.flat().filter(removeDuplicates)
+  const iterationsA = iterations.filter(removeDuplicates)
   const iterationsB = selectIterations({
     iterations: iterationsA,
     taskIds,
@@ -45,46 +26,8 @@ const loadIterations = async function({
     throw new Error('No tasks to bechmark')
   }
 
-  return iterationsB
-}
-
-const loadFiles = async function(taskPath, requireOpt) {
-  const runners = getRunners(taskPath)
-  const promises = runners.map(runner =>
-    loadFile({ taskPath, runner, requireOpt }),
-  )
-  const iterations = await Promise.all(promises)
-  const iterationsA = iterations.flat()
-  return iterationsA
-}
-
-const loadFile = async function({
-  taskPath,
-  runner,
-  runner: { command },
-  requireOpt,
-}) {
-  const iterations = await loadTaskFile({ taskPath, command, requireOpt })
-  return iterations.map(iteration =>
-    normalizeIteration(iteration, runner, taskPath),
-  )
-}
-
-const normalizeIteration = function(
-  { taskId, taskTitle = taskId, variationId, variationTitle = variationId },
-  { id: runnerId, title: runnerTitle = runnerId, command },
-  taskPath,
-) {
-  return {
-    taskPath,
-    taskId,
-    taskTitle,
-    variationId,
-    variationTitle,
-    runnerId,
-    runnerTitle,
-    command,
-  }
+  const iterationsC = addPaddings(iterationsB)
+  return iterationsC
 }
 
 // When two `files` define the same iteration, the last one prevails
