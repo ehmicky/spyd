f669ee2caae9729e8f0d76c125bdba4b97f8e299
ehmicky <ehmicky@gmail.com>
2019-08-07 10:00:00 +0200

    Refactoring

--- src/nve/main.js
+++ src/nve/main.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-import { platform, arch, argv, exit } from 'process'
-import { createGunzip } from 'zlib'
+import { argv, exit } from 'process'
 import { spawn } from 'child_process'
-import { readFile, writeFile, createWriteStream } from 'fs'
+import { readFile, writeFile } from 'fs'
 import { promisify } from 'util'
 
-import { extract as tarExtract } from 'tar-fs'
 import pEvent from 'p-event'
 import findCacheDir from 'find-cache-dir'
 import pathExists from 'path-exists'
 import { validRange, clean as cleanRange, maxSatisfying, ltr } from 'semver'
 
+import { downloadNode, NODE_FILENAME } from './download.js'
 import { cleanupOnError } from './cleanup.js'
 import { fetchUrl } from './fetch.js'
 
@@ -178,55 +177,6 @@ const getNodePath = async function(version) {
 
   return nodePath
 }
-
-// Retrieve the Node binary from the Node website and persist it
-// The URL depends on the current OS and CPU architecture
-const downloadNode = function(version, outputDir, nodePath) {
-  if (platform === 'win32') {
-    return downloadWindowsNode(version, nodePath)
-  }
-
-  return downloadUnixNode(version, outputDir)
-}
-
-// The Windows Node binary comes as a regular file
-const downloadWindowsNode = async function(version, nodePath) {
-  const { body } = await fetchUrl(
-    `${URL_BASE}/v${version}/win-${arch}/${NODE_FILENAME}`,
-  )
-
-  const writeStream = createWriteStream(nodePath, { mode: NODE_MODE })
-  body.pipe(writeStream)
-  await pEvent(writeStream, 'finish')
-}
-
-const NODE_MODE = 0o755
-
-// The Unix Node binary comes in a tar.gz folder
-const downloadUnixNode = async function(version, outputDir) {
-  const { body } = await fetchUrl(
-    `${URL_BASE}/v${version}/node-v${version}-${platform}-${arch}.tar.gz`,
-  )
-
-  const archive = body.pipe(createGunzip())
-  await unarchive(archive, outputDir)
-}
-
-const URL_BASE = 'https://nodejs.org/dist'
-
-const unarchive = async function(archive, outputDir) {
-  const extract = tarExtract(outputDir, { ignore: shouldExclude, strip: 2 })
-  archive.pipe(extract)
-  await pEvent(extract, 'finish')
-}
-
-// As a performance optimization, we only unpack the node binary, not the other
-// files.
-const shouldExclude = function(path) {
-  return !path.endsWith(`/${NODE_FILENAME}`)
-}
-
-const NODE_FILENAME = platform === 'win32' ? 'node.exe' : 'node'
 
 // Forward arguments to another node binary located at `nodePath`.
 // We also forward standard streams and exit code.
