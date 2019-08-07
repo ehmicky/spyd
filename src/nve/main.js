19ea81f453b5a149fd5a39d2f85bbdcedde5eda3
ehmicky <ehmicky@gmail.com>
2019-08-07 10:00:00 +0200

    Refactoring

--- src/nve/main.js
+++ src/nve/main.js
@@ -1,32 +1,12 @@
-#!/usr/bin/env node
-import { argv, exit } from 'process'
-
 import { validateInput } from './validate.js'
 import { getVersion } from './versions.js'
 import { runNode } from './run.js'
 
-// CLI that forwards its arguments to another node instance of a specific
-// version range. The version range is specified as the first argument.
-const runCli = async function() {
-  try {
-    const [versionRange, ...args] = argv.slice(2)
-    const { exitCode } = await nve(versionRange, args)
-    // Forward the exit code from the child process
-    exit(exitCode)
-  } catch (error) {
-    // eslint-disable-next-line no-console, no-restricted-globals
-    console.error(error.message)
-    exit(1)
-  }
-}
-
 // Forwards `args` to another node instance of a specific `versionRange`
-const nve = async function(versionRange, args = []) {
+export const nve = async function(versionRange, args = []) {
   validateInput(versionRange, args)
 
   const versionA = await getVersion(versionRange)
   const { exitCode, signal } = await runNode(versionA, args)
   return { exitCode, signal }
 }
-
-runCli()
