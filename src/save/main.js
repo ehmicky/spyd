1f420f9c911ebeb1a7ba3077f6d4490217cd6cb3
ehmicky <ehmicky@gmail.com>
2019-08-11 16:58:21 +0200

    Do not persist histogram|percentiles

--- src/save/main.js
+++ src/save/main.js
@@ -1,3 +1,5 @@
+import { omitBy } from '../utils/main.js'
+
 import { add } from './file/main.js'
 
 // Save benchmark results so they can be compared or shown later
@@ -7,12 +9,33 @@ export const save = async function(benchmark, { save: saveOpt }) {
   }
 
   const dataDir = '/tmp/spyd'
+  const benchmarkA = normalizeBenchmark(benchmark)
 
   try {
-    await add(dataDir, benchmark)
+    await add(dataDir, benchmarkA)
   } catch (error) {
     throw new Error(
       `Could not save benchmark to '${dataDir}':\n\n${error.stack}`,
     )
   }
 }
+
+// Some benchmark information are not persisted:
+//  - when too big: histograms, percentiles
+//  - when related to current report-related options
+const normalizeBenchmark = function({ iterations, ...benchmark }) {
+  const iterationsA = iterations.map(normalizeIteration)
+  return { ...benchmark, iterations: iterationsA }
+}
+
+const normalizeIteration = function({ stats, printedStats, ...iteration }) {
+  const statsA = removeBigStats(stats)
+  const printedStatsA = removeBigStats(printedStats)
+  return { ...iteration, stats: statsA, printedStats: printedStatsA }
+}
+
+const removeBigStats = function(stats) {
+  return omitBy(stats, key => OMITTED_STATS_PROPS.includes(key))
+}
+
+const OMITTED_STATS_PROPS = ['histogram', 'percentiles']
