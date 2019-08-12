3a79d10c50e9325e036c9ee9fb5dc44c228d28c5
ehmicky <ehmicky@gmail.com>
2019-08-12 10:00:00 +0200

    Refactoring

--- src/iterations/load.js
+++ src/iterations/load.js
@@ -6,27 +6,22 @@ import { loadTaskFile } from '../processes/load.js'
 export const loadIterations = async function({ taskPaths, runOpts, cwd }) {
   const { runners, versions } = await loadRunners(runOpts)
 
-  const commands = getFilesCommands(taskPaths, runners)
+  const iterations = await Promise.all(
+    taskPaths.map(taskPath => getFileIterations(taskPath, runners, cwd)),
+  )
+  const iterationsA = iterations.flat()
 
-  const iterations = await loadFilesIterations(commands, cwd)
-
-  return { iterations, versions }
+  return { iterations: iterationsA, versions }
 }
 
-// Couple each taskPath with one or several commands
-const getFilesCommands = function(taskPaths, runners) {
-  return taskPaths.flatMap(taskPath => getFileCommands(taskPath, runners))
-}
-
-const getFileCommands = function(taskPath, runners) {
+const getFileIterations = async function(taskPath, runners, cwd) {
   const runnersA = findRunners(taskPath, runners)
-  const runnersB = runnersA.map(({ commands }) => ({ commands, taskPath }))
-  return runnersB
-}
-
-const loadFilesIterations = async function(runners, cwd) {
-  const promises = runners.map(runner => loadFileIterations(runner, cwd))
-  const iterations = await Promise.all(promises)
+  const iterations = await Promise.all(
+    runnersA.map(
+      ({ commands }) => loadFileIterations({ commands, taskPath }),
+      cwd,
+    ),
+  )
   const iterationsA = iterations.flat()
   return iterationsA
 }
