9b9f4fc08d63f17a959e28402ea022a62e23e29e
ehmicky <ehmicky@gmail.com>
2019-08-13 10:00:00 +0200

    Finish versions

--- src/run/node/versions.js
+++ src/run/node/versions.js
@@ -1,5 +1,6 @@
 import normalizeNodeVersion from 'normalize-node-version'
-import { rcompare } from 'semver'
+import { rcompare, satisfies } from 'semver'
+import readPkgUp from 'read-pkg-up'
 
 // Normalize the node `versions` option
 export const getNodeVersions = async function({ versions }) {
@@ -9,19 +10,25 @@ export const getNodeVersions = async function({ versions }) {
 
   const versionsA = versions.split(WHITESPACE_REGEXP)
 
-  const versionString = await getVersionString(versionsA)
+  const [normalizedVersions, allowedVersions] = await Promise.all([
+    normalizeVersions(versionsA),
+    getAllowedVersions(),
+  ])
+
+  validateVersions(normalizedVersions, allowedVersions)
+
+  const versionString = getVersionString(normalizedVersions)
+
   return { versions: versionsA, versionString }
 }
 
 const WHITESPACE_REGEXP = /\s+/u
 
-// Versions shown in `--system` are normalized.
-// We also validate the versions before starting the benchmarks.
-const getVersionString = async function(versions) {
-  const versionsA = await Promise.all(versions.map(normalizeVersion))
-  // eslint-disable-next-line fp/no-mutating-methods
-  const versionString = versionsA.sort(rcompare).join(' ')
-  return versionString
+// We normalize the Node versions for validation purpose and for `--system`,
+// but not for the command id/title
+const normalizeVersions = async function(versions) {
+  const normalizedVersions = await Promise.all(versions.map(normalizeVersion))
+  return normalizedVersions
 }
 
 const normalizeVersion = async function(version) {
@@ -32,4 +39,35 @@ const normalizeVersion = async function(version) {
     error.message = `In option 'run.node.versions': ${error.message}`
     throw error
   }
+}
+
+// We can only allow Node versions that are valid with the runner's code
+const getAllowedVersions = async function() {
+  const {
+    package: {
+      engines: { node: allowedVersions },
+    },
+  } = await readPkgUp({ cwd: __dirname })
+  return allowedVersions
+}
+
+// We validate the versions before starting the benchmarks.
+const validateVersions = function(normalizedVersions, allowedVersions) {
+  normalizedVersions.forEach(version =>
+    validateVersion(version, allowedVersions),
+  )
+}
+
+const validateVersion = function(version, allowedVersions) {
+  if (!satisfies(version, allowedVersions)) {
+    throw new Error(
+      `In option 'run.node.versions': version ${version} must be ${allowedVersions}`,
+    )
+  }
+}
+
+// Versions shown in `--system` are normalized.
+const getVersionString = function(normalizedVersions) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  return normalizedVersions.sort(rcompare).join(' ')
 }
