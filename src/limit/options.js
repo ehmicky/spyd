c9614ac59f0c27cd25ddc228145d7055ad5dd570
ehmicky <ehmicky@gmail.com>
2019-08-18 17:34:32 +0200

    Allow numbers as limits

--- src/limit/options.js
+++ src/limit/options.js
@@ -1,29 +1,39 @@
-import { validateStringArray } from '../options/validate.js'
-
 // Normalize 'limit' option
-export const normalizeLimits = function({ limit, ...opts }) {
-  validateStringArray(limit, 'limit')
-
-  const limits = limit.map(normalizeLimit)
-  return { ...opts, limits }
+export const normalizeLimits = function({ limit: limits, ...opts }) {
+  const limitsA = limits.map(normalizeLimit)
+  return { ...opts, limits: limitsA }
 }
 
 const normalizeLimit = function(limit) {
-  if (!limit.includes('=')) {
-    return { percentage: getPercentage(limit) }
+  const limitA = stringify(limit)
+
+  if (!limitA.includes('=')) {
+    return { percentage: getPercentage(limitA) }
   }
 
-  const [ids, percentage] = limit.split('=')
+  const [ids, percentage] = limitA.split('=')
   const percentageA = getPercentage(percentage)
   const idsA = ids.split(',').filter(Boolean)
   return { ids: idsA, percentage: percentageA }
+}
+
+const stringify = function(limit) {
+  if (Number.isFinite(limit)) {
+    return String(limit)
+  }
+
+  if (typeof limit !== 'string') {
+    throw new TypeError(`'limit' must be an array of strings: ${limit}`)
+  }
+
+  return limit
 }
 
 const getPercentage = function(string) {
   const percentage = Number(string)
 
   if (!Number.isFinite(percentage) || percentage <= 0) {
-    throw new TypeError(`Limit must be a positive percentage: ${string}`)
+    throw new TypeError(`'limit' must be a positive percentage: ${string}`)
   }
 
   return percentage
