ba4deca8b084925c20d9beb91382aaa8dd3a6502
ehmicky <ehmicky@gmail.com>
2019-08-18 23:16:25 +0200

    Refactoring

--- src/print/diff.js
+++ src/print/diff.js
@@ -2,11 +2,17 @@ import { findBenchmark } from '../store/find.js'
 
 // Retrieve the index of the previous benchmark to compare/diff with
 export const getDiffIndex = function(previous, diff) {
-  if (diff === undefined || previous.length === 0) {
+  if (diff === undefined || isInitialDiff(previous, diff)) {
     return
   }
 
   return findBenchmark(previous, diff.queryType, diff.queryValue)
+}
+
+// We do not error when using the default `--diff` and no benchmarks have been
+// saved yet
+const isInitialDiff = function(previous, { queryType, queryValue }) {
+  return queryType === 'count' && queryValue === 1 && previous.length === 0
 }
 
 // Retrieve the difference of median compared with a previous benchmark
