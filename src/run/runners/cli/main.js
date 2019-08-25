f1d966aba8c241773dc93a340bd6bbfa7016625f
ehmicky <ehmicky@gmail.com>
2019-08-25 11:30:07 +0200

    Refactoring

--- src/run/runners/cli/main.js
+++ src/run/runners/cli/main.js
@@ -1,8 +1,13 @@
 const START_PATH = `${__dirname}/start.js`
-const extensions = ['yml', 'yaml']
 
 const action = function() {
   return [{ value: ['node', START_PATH], versions: [] }]
 }
 
-export const cli = { id: 'cli', title: 'CLI', extensions, action, system: {} }
+export const cli = {
+  id: 'cli',
+  title: 'CLI',
+  extensions: ['yml', 'yaml'],
+  action,
+  system: {},
+}
