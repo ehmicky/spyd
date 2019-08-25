01052f5e3838d6f5be3e46a5e14c4575363be404
ehmicky <ehmicky@gmail.com>
2019-08-25 15:38:21 +0200

    Fix dynamic values

--- src/bin/dynamic.js
+++ src/bin/dynamic.js
@@ -3,28 +3,39 @@ import { isPlainObject } from '../utils/main.js'
 // `--report.json` and `--report.json.option` are normalized to
 // `{ report: { json: { option } } }`.
 // Same thing for `--progress`.
-export const normalizeDynamicOpts = function(opt) {
+export const normalizeDynamicOpts = function(opts) {
+  const dynamicOpts = DYNAMIC_OPTS.map(name => normalizeDynamicOpt(name, opts))
+  const dynamicOptsA = Object.fromEntries(dynamicOpts)
+  return { ...opts, ...dynamicOptsA }
+}
+
+const DYNAMIC_OPTS = ['report', 'progress', 'run', 'store']
+
+const normalizeDynamicOpt = function(name, opts) {
+  const opt = opts[name]
+
   if (!isPlainObject(opt)) {
-    return opt
+    return [name, opt]
   }
 
-  const optA = Object.entries(opt).map(normalizeDynamicOpt)
-  return Object.fromEntries(optA)
+  const optA = Object.entries(opt).map(normalizeOpt)
+  const optB = Object.fromEntries(optA)
+  return [name, optB]
 }
 
 // `value` will be an array when specifying `--report.*` several times
-const normalizeDynamicOpt = function([name, value]) {
+const normalizeOpt = function([name, value]) {
   if (!Array.isArray(value)) {
-    return [name, normalizeDynamic(value)]
+    return [name, normalizeValue(value)]
   }
 
-  const valueA = Object.assign({}, ...value.map(normalizeDynamic))
+  const valueA = Object.assign({}, ...value.map(normalizeValue))
   return [name, valueA]
 }
 
 // `value` will be `true` when specifying `--report.REPORTER` (as opposed to
 // `--report.REPORTER.OPT`
-const normalizeDynamic = function(value) {
+const normalizeValue = function(value) {
   if (value === true) {
     return {}
   }
--- src/bin/parse.js
+++ src/bin/parse.js
@@ -5,24 +5,11 @@ import { normalizeDynamicOpts } from './dynamic.js'
 export const parseOpts = function(yargs) {
   const {
     _: [command = DEFAULT_COMMAND],
-    report,
-    progress,
-    run,
-    store,
     ...opts
   } = yargs.parse()
 
-  const reportA = normalizeDynamicOpts(report)
-  const progressA = normalizeDynamicOpts(progress)
-  const runA = normalizeDynamicOpts(run)
-  const storeA = normalizeDynamicOpts(store)
-  const optsA = {
-    ...opts,
-    report: reportA,
-    progress: progressA,
-    run: runA,
-    store: storeA,
-  }
+  const optsA = normalizeDynamicOpts(opts)
+
   const optsB = omitBy(optsA, isInternalKey)
   return [command, optsB]
 }
