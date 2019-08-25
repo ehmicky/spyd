d37a5f63608fef608898a87830fff5436b45c3f2
ehmicky <ehmicky@gmail.com>
2019-08-25 11:34:31 +0200

    Refactoring

--- src/run/runners/cli/load/shell.js
+++ src/run/runners/cli/load/shell.js
@@ -1,6 +1,7 @@
 import { applyTemplate } from '../template.js'
 
-// `shell` can contain variables, i.e. can be a string 'true' or 'false'
+// Retrieve the `shell` property which indicates whether to run commands through
+// a shell.
 export const getShell = function(
   { shell = DEFAULT_SHELL, ...entries },
   variables,
@@ -9,9 +10,14 @@ export const getShell = function(
     return { shell, entries }
   }
 
+  // `shell` can contain variables, i.e. can be a string 'true' or 'false'
   const shellA = applyTemplate(shell, variables)
   const shellB = shellA === 'true'
   return { shell: shellB, entries }
 }
 
+// We default to `true` because many users would expect this. It only adds a
+// very small amount of duration/variance.
+// However it is less secure and less cross-platform, so we encourage using
+// `false` instead.
 const DEFAULT_SHELL = true
