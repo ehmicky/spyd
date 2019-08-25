f6cc3644139429216bc0e8011707edc32623f2b3
ehmicky <ehmicky@gmail.com>
2019-08-25 11:36:19 +0200

    Refactoring

--- src/run/runners/node/main.js
+++ src/run/runners/node/main.js
@@ -1,6 +1,10 @@
 import { commands } from './commands.js'
 import { system } from './system.js'
 
-const extensions = ['js', 'ts', 'jsx', 'tsx', 'es6', 'mjs']
-
-export const node = { id: 'node', title: 'Node', extensions, commands, system }
+export const node = {
+  id: 'node',
+  title: 'Node',
+  extensions: ['js', 'ts', 'jsx', 'tsx', 'es6', 'mjs'],
+  commands,
+  system,
+}
