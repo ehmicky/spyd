d9e360ece1005e51fe2b388f95e030daf0dc8c2b
ehmicky <ehmicky@gmail.com>
2019-08-25 23:33:23 +0200

    Join files

--- src/group/merge.js
+++ src/group/merge.js
@@ -1,6 +1,7 @@
+import fastDeepEqual from 'fast-deep-equal'
+
 import { groupBy } from '../utils/group.js'
 import { removeDuplicates } from '../iterations/duplicate.js'
-import { mergeSystems } from '../system/merge.js'
 
 // Merge previous benchmarks part of the same `group`.
 // Later benchmarks have priority.
@@ -19,4 +20,36 @@ const mergePair = function(
   const systems = mergeSystems(previousSystems, system)
   const iterationsA = removeDuplicates([...previousIterations, ...iterations])
   return { ...benchmark, id, systems, iterations: iterationsA }
+}
+
+// Several benchmarks can have the same system, providing it is exactly the same
+const mergeSystems = function(previousSystems, system) {
+  const duplicateSystem = previousSystems.find(
+    systemA => systemA.id === system.id,
+  )
+
+  if (duplicateSystem === undefined) {
+    return [...previousSystems, system]
+  }
+
+  SAME_SYSTEM_PROPS.forEach(propName =>
+    validateSystem(duplicateSystem, system, propName),
+  )
+
+  const previousSystemsA = previousSystems.filter(
+    systemA => systemA.id !== system.id,
+  )
+  return [...previousSystemsA, system]
+}
+
+const SAME_SYSTEM_PROPS = ['opts']
+
+const validateSystem = function(duplicateSystem, system, propName) {
+  // TODO: replace with util.isDeepStrictEqual() once dropping support for
+  // Node 8
+  if (!fastDeepEqual(duplicateSystem[propName], system[propName])) {
+    throw new Error(`Several benchmarks with the same "group" and "system" cannot have different ${propName}:
+${JSON.stringify(duplicateSystem[propName])}
+${JSON.stringify(system[propName])}`)
+  }
 }
