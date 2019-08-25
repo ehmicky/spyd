616593c060959aacff9b8137bcbc5d7f826ad95c
ehmicky <ehmicky@gmail.com>
2019-08-25 11:35:59 +0200

    Add comment

--- src/run/runners/node/commands.js
+++ src/run/runners/node/commands.js
@@ -5,6 +5,8 @@ import { getNodeVersions } from './versions.js'
 
 const START_PATH = `${__dirname}/start.js`
 
+// Retrieve Node commands. By default it uses the current Node.js.
+// But the `run.node.versions` can be used to spawn several Node.js versions.
 export const commands = async function(runOpts) {
   const runOptsA = getOpts(runOpts)
 
