21683a7d3bd146e79b9b07d78eeed09bd44d661f
ehmicky <ehmicky@gmail.com>
2019-08-25 11:49:25 +0200

    Sort benchmarks by timestamp

--- src/store/list.js
+++ src/store/list.js
@@ -1,5 +1,13 @@
+import { sortBy } from '../utils/sort.js'
+
 // Call `store.list()`
 export const listStore = async function({ store }) {
+  const benchmarks = await callList(store)
+  sortBy(benchmarks, ['timestamp'])
+  return benchmarks
+}
+
+const callList = async function(store) {
   try {
     return await store.list()
   } catch (error) {
