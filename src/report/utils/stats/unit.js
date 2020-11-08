e196cf554c144a43f385d0d38b797c3c24facc2d
ehmicky <ehmicky@gmail.com>
2020-11-08 21:35:36 +0100

    Refactoring

--- src/report/utils/stats/unit.js
+++ src/report/utils/stats/unit.js
@@ -4,45 +4,42 @@
 // We use the minimum time unit where all medians are >= 1
 export const getUnit = function (iterations) {
   const medians = iterations.map(getMedianStat).filter(isNotZero)
-  const unit = findUnit(medians)
-  const scale = UNITS[unit]
-  return { unit, scale }
+
+  if (medians.length === 0) {
+    return DEFAULT_UNIT
+  }
+
+  const min = Math.min(...medians)
+  const preciseUnit = UNITS.find(({ scale }) => min >= scale)
+
+  if (preciseUnit === undefined) {
+    return MIN_UNIT
+  }
+
+  return preciseUnit
 }
 
 const getMedianStat = function ({ stats: { median } }) {
   return median
 }
 
-const findUnit = function (medians) {
-  // When all medians are 0
-  if (medians.length === 0) {
-    return DEFAULT_UNIT
-  }
-
-  const min = Math.min(...medians)
-
-  const preciseUnit = Object.entries(UNITS).find(
-    ([, minUnit]) => min >= minUnit,
-  )
-
-  // When all medians are extremely fast. Failsafe unlikely to happen.
-  if (preciseUnit === undefined) {
-    return MIN_UNIT
-  }
-
-  return preciseUnit[0]
-}
-
 const isNotZero = function (median) {
   return median !== 0
 }
 
+// When all medians are 0, i.e. could not measure precisely enough.
+const DEFAULT_UNIT = { unit: 'ns', scale: 1 }
+// When all medians are extremely fast. Failsafe unlikely to happen.
+const MIN_UNIT = { unit: 'fs', scale: 1e-6 }
 // The maximum unit is seconds:
 //  - minutes require writing two units (e.g. 1m56s) which is harder to read
 //  - minutes are not base 10 which makes it harder to visually compare
 //  - hours-long tasks are unlikely
-// eslint-disable-next-line id-length
-const UNITS = { s: 1e9, ms: 1e6, μs: 1e3, ns: 1, ps: 1e-3, fs: 1e-6 }
-
-const DEFAULT_UNIT = 'ns'
-const MIN_UNIT = 'fs'
+const UNITS = [
+  { unit: 's', scale: 1e9 },
+  { unit: 'ms', scale: 1e6 },
+  { unit: 'μs', scale: 1e3 },
+  DEFAULT_UNIT,
+  { unit: 'ps', scale: 1e-3 },
+  MIN_UNIT,
+]
