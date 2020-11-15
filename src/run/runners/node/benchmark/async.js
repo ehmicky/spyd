acd7c850e6a2fa938c69cfbe5bc22132ee82e3dc
ehmicky <ehmicky@gmail.com>
2020-11-15 19:36:36 +0100

    Improve async

--- src/run/runners/node/benchmark/async.js
+++ src/run/runners/node/benchmark/async.js
@@ -1,26 +1,14 @@
-import { isPromiseLike } from '../../../../utils/main.js'
+import { promisify } from 'util'
 
 // Async functions use different benchmarking logic.
 // We only check once if `main()` is async in order to simplify the logic.
 // This means `main()` cannot be sometimes sync and other times async.
 // This does not apply to `before()` nor `after()`.
-export const isAsyncFunc = function (func) {
-  // Only meant as a shortcut to avoid calling `main()` when possible.
-  // The function might still be async if it returns a promise but was not
-  // declared with `async/await`.
-  if (func[Symbol.toStringTag] === 'AsyncFunction') {
-    return true
-  }
-
-  const returnValue = func()
-
-  if (!isPromiseLike(returnValue)) {
-    return false
-  }
-
-  returnValue.catch(errorNoop)
-  return true
+// We check by looking for the `async` keyword or `util.promisify`
+// We do not call `main()` since it might be very slow.
+export const isAsyncFunc = function (main) {
+  return (
+    main[Symbol.toStringTag] === 'AsyncFunction' ||
+    main[promisify.custom] !== undefined
+  )
 }
-
-// eslint-disable-next-line no-empty-function
-const errorNoop = function () {}
--- src/utils/main.js
+++
@@ -1,9 +0,0 @@
-// ES specifications mostly use promiseLike objects not promises
-export const isPromiseLike = function (value) {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    // eslint-disable-next-line promise/prefer-await-to-then
-    typeof value.then === 'function'
-  )
-}
