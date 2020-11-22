860afb28f36b12b1deabd9390876b653b8708350
ehmicky <ehmicky@gmail.com>
2020-11-22 23:48:00 +0100

    Remove `initialMeasure`

--- src/run/runners/node/benchmark/main.js
+++ src/run/runners/node/benchmark/main.js
@@ -1,9 +1,8 @@
 import { benchmarkLoop } from './loop.js'
-import { measure } from './measure.js'
 
 // Measure how long a task takes.
 // Run the benchmark for a specific amount of time.
-export const benchmark = async function ({
+export const benchmark = function ({
   main,
   before,
   after,
@@ -12,9 +11,7 @@ export const benchmark = async function ({
   maxDuration,
   maxTimes,
 }) {
-  await initialMeasure(async, before)
-
-  const times = await benchmarkLoop({
+  return benchmarkLoop({
     main,
     before,
     after,
@@ -23,21 +20,4 @@ export const benchmark = async function ({
     maxDuration,
     maxTimes,
   })
-  return times
 }
-
-// For some reasons I ignore (likely engine optimizations), when `measure()`
-// is benchmarking its first function, it's running much faster than for the
-// next functions passed to it.
-// We fix this by doing a cold start using an empty function.
-// The `before()` and `async` arguments need to match what is used during
-// the bias calculation.
-const initialMeasure = async function (async, before) {
-  const beforeFunc = before === undefined ? undefined : noop
-  await measure({ main: noop, before: beforeFunc, repeat: 1, async })
-}
-
-// This needs to be a different function from the `noop` used during bias
-// calculation.
-// eslint-disable-next-line no-empty-function
-const noop = function () {}
