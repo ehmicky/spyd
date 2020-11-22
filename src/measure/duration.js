4ae3825b5671be135896c1f020792c4f807ebb44
ehmicky <ehmicky@gmail.com>
2020-11-22 13:51:45 +0100

    Add comments

--- src/measure/duration.js
+++ src/measure/duration.js
@@ -11,21 +11,38 @@ import { denormalizeTime } from './normalize.js'
 //     compared to it.
 //  3. Must run a minimal amount of loops per process. This is to ensure cold
 //     starts do not impact benchmarking.
-// Fast tasks are most likely time-limited by `2.` while slow tasks are most
-// likely time-limited by `3.`.
+// This means:
+//   - Fast tasks are most likely time-limited by `2.`. Since `benchmarkCost`
+//     mostly depends on the hardware speed, the time to load a given runner and
+//     the time to load benchmark files, this is rather stable between runs.
+//     A multiple of `benchmarkCost` is then used as the target duration for
+//     each process.
+//   - Slow tasks are most likely time-limited by `3.`. Their target duration
+//     is computed to run them a specific amount of times.
+//   - As fast tasks get slower, their target duration based on `2.` becomes
+//     closer to the target duration based on `3.`, ensuring a perfect
+//     transition.
 // The above algorithm has several goals:
 //   - Ensures processes to roughly use the same `maxDuration` both inside a
 //     specific run and between runs with different `duration` options.
 //     Processes with different `maxDuration` might give different results to
-//     the runtime having optimized the code longer or not.
+//     the runtime having optimized hot paths for longer periods of time.
 //   - Ensures processes are short enough to provide with frequent realtime
 //     reporting
 //   - Ensures many processes are run to decrease the overall variance, while
 //     still making sure enough loops are run inside each of those processes
-// We remove the `loopTime` (time for the runner to perform a single benchmark
-// loop) (which is estimated from previous processes) to guarantee runners stop
-// right under the target duration, not right above. This ensures users are not
-// experiencing slow downs of the progress counter at the end of an iteration.
+// We remove the `loopTime` to guarantee runners stop right under the target
+// duration, not right above:
+//   - `loopTime` is the time for the runner to perform a single benchmark loop.
+//     It is estimated from previous processes.
+//   - This ensures users are not experiencing slow downs of the progress
+//     counter at the end of an iteration.
+// We pass a single `maxDuration` parameters for runners to know when to stop
+// benchmarking
+//   - We purposely avoid others like `maxTimes` or `minDuration` to keep it
+//     simple for runners.
+//   - This means `targetTimes` needs to be time-estimated, as opposed to simply
+//     pass a target integer to runners as parameter.
 export const getMaxDuration = function ({
   runEnd,
   benchmarkCost,
