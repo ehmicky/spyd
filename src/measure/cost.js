7b5cb2acb226fbd44dfedfa9bde7f7756b5b4e6f
ehmicky <ehmicky@gmail.com>
2020-11-22 17:25:12 +0100

    Add comment

--- src/measure/cost.js
+++ src/measure/cost.js
@@ -1,23 +1,46 @@
 import { getUnsortedMedian } from '../stats/median.js'
 
-// Computes how much time is spent spawning processes/runners as opposed to
-// running the benchmarked task.
-// Note that this does not include the time spent by the runner iterating on
-// the benchmark loop itself, since that time increases proportionally to the
-// number of loops. It only includes the time initially spent when each process
-// loads.
-// We measure this continuously for each new process. Runners returns the
-// `duration` of the benchmarking logic so it can be excluded from this.
-// We use a median of the previous processes' `benchmarkCost`. We do not include
-// it in the `previous` array though since it might differ significantly for
-// some runners.
-// Since sorting big arrays is very slow, we only sort a sample of them.
-// The initial value is based on the time it took to load the iterations.
-// Each iteration estimates its own `benchmarkCost`. In most cases, that value
-// should be similar for iterations using the same runner. However, it is
-// possible that a runner might be doing some extra logic at `run` time
-// (instead of load time) when retrieving a task with a specific option, such
-// as dynamically loading some code for that specific task.
+// Estimates how much time is spent loading runners as opposed to running the
+// benchmarked task.
+// That estimation is used when computing the optimal process `maxDuration`.
+// This includes the time spent:
+//  - spawning the child process
+//  - loading the runner, e.g. loading files and dependencies
+//  - loading the task
+// This does not include the time spent:
+//  - iterating on the benchmark loop itself
+//  - sending the results (runner)
+//  - receiving the results (parent process)
+//  - normalizing and aggregating the results
+// The above are not included because their duration depends on the number of
+// benchmark loops performed
+//  - that number depends on `maxDuration`, which is based on `benchmarkCost`
+//  - this would create a cycle making the `benchmarkCost` increase forever
+// The estimation is based on the difference between two timestamps:
+//  - in the parent process, right before spawning a new runner process
+//  - in the runner process, right before benchmarking, but after loading
+//    dependencies and the task. This is sent back to parent in the results.
+// Those timestamps are in nanoseconds
+//  - some runners or systems might not allow such a high resolution, but they
+//    should stil use that unit
+//  - runners with lower resolution make the `benchmarkCost` vary more
+// Runners should strive to load dependencies and tasks as fast as possible,
+// since it:
+//  - increases the time spent on benchmarking
+//  - maximizes the number of processes per run
+// The estimation is made for each new process. A median of the `previous`
+// processes' `benchmarkCost` is used
+//  - the initial default value is based on the time it took to load the
+//    iterations
+//  - it is not included it in the `previous` array though since it might differ
+//    significantly for some runners
+//  - since sorting big arrays is very slow, we only sort a sample of them
+// Each iteration estimates its own `benchmarkCost`:
+//  - in most cases, that value should be similar for iterations using the
+//    same runner
+//  - however, it is possible that a runner might be doing some extra logic at
+//    `run` time (instead of load time) when retrieving a task with a specific
+//    option, such as dynamically loading some code for that specific task
 export const updateBenchmarkCost = function (
   childBenchmarkCost,
   benchmarkCost,
