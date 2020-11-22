e8bd78ed6f316169e7da1c0da5c2be846b1175ff
ehmicky <ehmicky@gmail.com>
2020-11-22 11:12:15 +0100

    Move files

--- src/processes/main.js
+++ src/processes/main.js
@@ -1,8 +1,12 @@
+import { promisify } from 'util'
+
 import now from 'precise-now'
 
 import { getStats } from '../stats/compute.js'
 
 import { runChildren } from './run.js'
+
+const pSetTimeout = promisify(setTimeout)
 
 // Start several child processes benchmarking the same task.
 // eslint-disable-next-line max-lines-per-function
@@ -46,6 +50,8 @@ export const runProcesses = async function ({
 
   const stats = getStats({ times, count, processes })
 
+  await waitForTimeLeft(runEnd)
+
   return {
     row,
     column,
@@ -62,3 +68,22 @@ export const runProcesses = async function ({
     stats,
   }
 }
+
+// We stop running processes when the next process is most likely to go beyond
+// the target `duration`. We do not try to run it with a lower duration since
+// this would skew results due to comparing processes with a different number
+// of loops.
+// However, we still wait for the time left, without running any benchmark.
+// This is wasteful time-wise but prevents the timer from jumping fast-forward
+// at the end, giving the feeling of a smooth countdown instead
+const waitForTimeLeft = async function (runEnd) {
+  const timeLeft = (runEnd - now()) / NANOSECS_TO_MSECS
+
+  if (timeLeft <= 0) {
+    return
+  }
+
+  await pSetTimeout(timeLeft)
+}
+
+const NANOSECS_TO_MSECS = 1e6
