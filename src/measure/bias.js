d38d93d6204838ebe311ea8e6ffa3b364c9d4183
ehmicky <ehmicky@gmail.com>
2020-11-22 17:28:11 +0100

    Move file

--- src/measure/bias.js
+++ src/measure/bias.js
@@ -1,8 +1,7 @@
-import timeResolution from 'time-resolution'
-
 import { getSortedMedian } from '../stats/median.js'
 
 import { runMeasureLoop } from './loop.js'
+import { getMinTime } from './min_time.js'
 import { getRepeat } from './repeat.js'
 
 // The following biases are introduced by the benchmarking code itself:
@@ -108,22 +107,3 @@ const getBias = async function ({
   const median = getSortedMedian(times)
   return median
 }
-
-// If a task duration is too close to `nowBias`, the variance will be mostly due
-// to the timestamp function itself.
-// Also if a task duration is too close to the minimum system time resolution,
-// it will lack precision.
-// To fix this we run the task in a loop to increase its running time. We then
-// perform an arithmetic mean.
-// `minTime` is the minimum time under which we consider a task should do this.
-const getMinTime = function (nowBias) {
-  const minPrecisionTime = TIME_RESOLUTION * MIN_PRECISION
-  const minNowBiasTime = nowBias * MIN_NOW_BIAS
-  return Math.max(minPrecisionTime, minNowBiasTime)
-}
-
-const TIME_RESOLUTION = timeResolution()
-// The task loop must be at least `MIN_PRECISION` slower than time resolution
-const MIN_PRECISION = 1e2
-// The task loop must be at least `MIN_NOW_BIAS` slower than `nowBias`
-const MIN_NOW_BIAS = 1e2
---
+++ src/measure/min_time.js
@@ -0,0 +1,20 @@
+import timeResolution from 'time-resolution'
+
+// If a task duration is too close to `nowBias`, the variance will be mostly due
+// to the timestamp function itself.
+// Also if a task duration is too close to the minimum system time resolution,
+// it will lack precision.
+// To fix this we run the task in a loop to increase its running time. We then
+// perform an arithmetic mean.
+// `minTime` is the minimum time under which we consider a task should do this.
+export const getMinTime = function (nowBias) {
+  const minPrecisionTime = TIME_RESOLUTION * MIN_PRECISION
+  const minNowBiasTime = nowBias * MIN_NOW_BIAS
+  return Math.max(minPrecisionTime, minNowBiasTime)
+}
+
+const TIME_RESOLUTION = timeResolution()
+// The task loop must be at least `MIN_PRECISION` slower than time resolution
+const MIN_PRECISION = 1e2
+// The task loop must be at least `MIN_NOW_BIAS` slower than `nowBias`
+const MIN_NOW_BIAS = 1e2
