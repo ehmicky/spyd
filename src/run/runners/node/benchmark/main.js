13d3aaf7ad59a32652c98fccdf78ebd0b9adbcb2
ehmicky <ehmicky@gmail.com>
2020-11-22 23:48:23 +0100

    Move file

--- src/run/runners/node/benchmark/main.js
+++ src/run/runners/node/benchmark/main.js
@@ -1,8 +1,13 @@
-import { benchmarkLoop } from './loop.js'
+import now from 'precise-now'
 
-// Measure how long a task takes.
-// Run the benchmark for a specific amount of time.
-export const benchmark = function ({
+import { measure } from './measure.js'
+
+// We perform benchmarking iteratively in order to stop benchmarking exactly
+// when the `duration` has been reached.
+// We also adjust or increment some `state` variables as we take more
+// measurements.
+// We ensure `times` contains at least one measurement.
+export const benchmark = async function ({
   main,
   before,
   after,
@@ -11,13 +16,20 @@ export const benchmark = function ({
   maxDuration,
   maxTimes,
 }) {
-  return benchmarkLoop({
-    main,
-    before,
-    after,
-    async,
-    repeat,
-    maxDuration,
-    maxTimes,
-  })
+  const times = []
+  const runEnd = now() + maxDuration
+
+  // eslint-disable-next-line fp/no-loops
+  do {
+    // eslint-disable-next-line no-await-in-loop
+    const time = await measure({ main, before, after, repeat, async })
+    // eslint-disable-next-line fp/no-mutating-methods
+    times.push(time)
+  } while (!shouldStopLoop(maxTimes, times, runEnd))
+
+  return times
+}
+
+const shouldStopLoop = function (maxTimes, times, runEnd) {
+  return (maxTimes !== undefined && times.length >= maxTimes) || now() >= runEnd
 }
