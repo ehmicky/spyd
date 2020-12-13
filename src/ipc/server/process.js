d040f365d67b24638148245929362c746cba6a9f
ehmicky <ehmicky@gmail.com>
2020-12-13 21:48:41 +0100

    Add comment

--- src/ipc/server/process.js
+++ src/ipc/server/process.js
@@ -7,6 +7,24 @@ import { getServerUrl } from './url.js'
 
 const CLIENT_ENTRYFILE = `${__dirname}/../client/main.js`
 
+// Each combination is spawned in its own process:
+//  - This ensures runtime optimization is bound to each combination
+//  - This allows runners with different programming languages
+// We use a single long process per combination instead of spawning multiple
+// ones:
+//  - This would require setting a maximum duration or amount of measures. To
+//    estimate those for different types of tasks is hard and brittle:
+//      - Using hard-coded durations does not work since machines or tasks have
+//        very different speeds
+//      - Using the speed of the machine (by measuring the duration to spawn
+//        process, load tasks, etc.) makes results vary based on non-obvious
+//        factors. It is also hard to callibrate.
+//      - Using the `duration` configuration property does not work when it is
+//        `0`
+//  - This would require loading both the runner and the task multiple times,
+//    which wastes duration and does not allow runners with long initialization.
+//  - Variance is higher due to task cold starts having a higher share of the
+//    total measures.
 export const runProcesses = async function ({
   combinations,
   origin,
