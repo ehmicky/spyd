47d44b73edf81a5efcdcf690614a847ca9bea2ff
ehmicky <ehmicky@gmail.com>
2020-12-13 11:53:47 +0100

    Fix `empty`

--- src/run/runners/common/start.js
+++ src/run/runners/common/start.js
@@ -5,7 +5,7 @@ import { preciseTimestamp } from '../../../measure/precise_timestamp.js'
 // Start measuring a task
 export const startMeasuring = function (maxDuration, empty) {
   const mainMeasures = []
-  const emptyMeasures = empty === undefined ? undefined : []
+  const emptyMeasures = empty ? [] : undefined
   const start = String(preciseTimestamp())
   const measureEnd = now() + maxDuration
   return { mainMeasures, emptyMeasures, start, measureEnd }
