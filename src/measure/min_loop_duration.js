0faaab1f393bd9f03f8c45fca5c24d9d4dcc396d
ehmicky <ehmicky@gmail.com>
2020-12-13 11:53:03 +0100

    Move code

--- src/measure/min_loop_duration.js
+++ src/measure/min_loop_duration.js
@@ -1,4 +1,6 @@
-import { getMeasureCost } from './measure_cost.js'
+import { OUTLIERS_THRESHOLD } from '../stats/outliers.js'
+import { getMediansMedian } from '../stats/quantile.js'
+
 import { getResolution } from './resolution.js'
 
 // `measureCost` is the time taken to take a measurement.
@@ -73,6 +75,22 @@ export const getMinLoopDuration = function ({
   )
   return [newMinLoopDuration, newResolution, newResolutionSize]
 }
+
+// This function estimates `measureCost` by making runners measure empty tasks.
+// That cost must be computed separately for each combination since they might
+// vary depending on the task, input or system. For example, tasks with more
+// iterations per process have more time to optimize `measureCost`, which is
+// usually faster then.
+const getMeasureCost = function (measureCosts, emptyMeasures) {
+  return getMediansMedian({
+    medians: measureCosts,
+    array: emptyMeasures,
+    precision: EMPTY_MEASURES_PRECISION,
+    threshold: OUTLIERS_THRESHOLD,
+  })
+}
+
+const EMPTY_MEASURES_PRECISION = 1e2
 
 // How many times slower each repeat loop iteration must be compared to
 // `measureCost`.
