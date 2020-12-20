d07c6c1281abfd27bc6ebc263987c36050b65b3f
ehmicky <ehmicky@gmail.com>
2020-12-20 11:41:33 +0100

    Simplify code

--- src/run/runners/node/benchmark/main.js
+++ src/run/runners/node/benchmark/main.js
@@ -5,56 +5,23 @@ import { performLoopsSync } from './sync/loops.js'
 
 // Call the `main` function iteratively and return an array of numbers measuring
 // how long each call took.
+// We separate async and sync measurements because following a promise (`await`)
+// can take several microseconds, which does not work when measuring fast
+// synchronous functions.
 export const benchmark = async function (
   { repeat, maxLoops, empty },
   { main, before, after, async },
 ) {
   const { mainMeasures, emptyMeasures } = startMeasuring(empty)
-  await performLoops({
+  const performLoopsFunc = async ? performLoopsAsync : performLoopsSync
+  await performLoopsFunc({
     main,
     before,
     after,
-    async,
     repeat,
     maxLoops,
     mainMeasures,
     emptyMeasures,
   })
   return { mainMeasures, emptyMeasures }
-}
-
-// We separate async and sync measurements because following a promise (`await`)
-// can take several microseconds, which does not work when measuring fast
-// synchronous functions.
-const performLoops = function ({
-  main,
-  before,
-  after,
-  async,
-  repeat,
-  maxLoops,
-  mainMeasures,
-  emptyMeasures,
-}) {
-  if (async) {
-    return performLoopsAsync({
-      main,
-      before,
-      after,
-      repeat,
-      maxLoops,
-      mainMeasures,
-      emptyMeasures,
-    })
-  }
-
-  return performLoopsSync({
-    main,
-    before,
-    after,
-    repeat,
-    maxLoops,
-    mainMeasures,
-    emptyMeasures,
-  })
 }
