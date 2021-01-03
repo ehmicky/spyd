a46df6ebf784b253a6d610bf4e48f9f0afa76e03
ehmicky <ehmicky@gmail.com>
2021-01-03 13:54:54 +0100

    Fix plugins load

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -7,9 +7,10 @@ import { STORES } from '../store/stores/main.js'
 // Several configuration properties (`runner`, `reporter`, `progress`, `store`)
 // can be customized with custom modules. This loads them. Each type can specify
 // builtin modules too.
-// Configuration properties can be passed to each module.
-// We use a nested object to set those. This uses a dot notation when in the
-// CLI. This format is chosen because this:
+// We separate selecting and configuring plugins to make it easier to
+// include and exclude specific plugins on-the-fly.
+// We use a nested object for configuration. This uses a dot notation when in
+// the CLI. This format is chosen because this:
 //   - allows more complex properties in spyd.yml
 //   - only requires a single delimiter character (dot) instead of mixing others
 //     like - or _
@@ -18,8 +19,8 @@ import { STORES } from '../store/stores/main.js'
 //   - works unescaped with YAML and JSON
 export const loadAllPlugins = async function (config) {
   const pluginsConfigs = await Promise.all(
-    TYPES.map(({ type, prefix, builtins }) =>
-      loadPlugins({ config, type, prefix, builtins }),
+    TYPES.map(({ type, selector, prefix, builtins }) =>
+      loadPlugins({ config, type, selector, prefix, builtins }),
     ),
   )
   const pluginsConfigA = Object.fromEntries(pluginsConfigs)
@@ -27,34 +28,70 @@ export const loadAllPlugins = async function (config) {
 }
 
 const TYPES = [
-  { type: 'runner', prefix: 'spyd-runner-', builtins: RUNNERS },
-  { type: 'reporter', prefix: 'spyd-reporter-', builtins: REPORTERS },
-  { type: 'progress', prefix: 'spyd-progress-', builtins: PROGRESS_REPORTERS },
-  { type: 'store', prefix: 'spyd-store-', builtins: STORES },
+  {
+    type: 'runner',
+    selector: 'tasks.*',
+    prefix: 'spyd-runner-',
+    builtins: RUNNERS,
+  },
+  {
+    type: 'reporter',
+    selector: 'reporters',
+    prefix: 'spyd-reporter-',
+    builtins: REPORTERS,
+  },
+  {
+    type: 'progress',
+    selector: 'progresses',
+    prefix: 'spyd-progress-',
+    builtins: PROGRESS_REPORTERS,
+  },
+  {
+    type: 'store',
+    selector: 'stores',
+    prefix: 'spyd-store-',
+    builtins: STORES,
+  },
 ]
 
-const loadPlugins = async function ({ config, type, prefix, builtins }) {
+const loadPlugins = async function ({
+  config,
+  type,
+  selector,
+  prefix,
+  builtins,
+}) {
+  const pluginIds = selectPlugins(selector, config)
   const pluginConfigs = config[type]
   const plugins = await Promise.all(
-    Object.entries(pluginConfigs).map(([id, pluginConfig]) =>
-      loadPlugin({ type, prefix, id, pluginConfig, builtins }),
+    pluginIds.map((id) =>
+      loadPlugin({ id, type, prefix, pluginConfigs, builtins }),
     ),
   )
   return [type, plugins]
 }
 
+const selectPlugins = function (selector, config) {
+  if (selector === 'tasks.*') {
+    return Object.keys(config.tasks)
+  }
+
+  return config[selector]
+}
+
 const loadPlugin = async function ({
+  id,
   type,
   prefix,
-  id,
-  pluginConfig,
+  pluginConfigs,
   builtins,
 }) {
-  const plugin = await importPlugin({ type, prefix, id, builtins })
+  const plugin = await importPlugin({ id, type, prefix, builtins })
+  const pluginConfig = pluginConfigs[id] === undefined ? {} : pluginConfigs[id]
   return { ...plugin, id, config: pluginConfig }
 }
 
-const importPlugin = async function ({ type, prefix, id, builtins }) {
+const importPlugin = async function ({ id, type, prefix, builtins }) {
   const builtin = builtins[id]
 
   if (builtin !== undefined) {
