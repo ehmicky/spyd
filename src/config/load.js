c98586581b4b72877f7e24b8febeec1e4d196e8f
ehmicky <ehmicky@gmail.com>
2021-01-03 21:21:51 +0100

    Remove environment configuration

--- src/config/load.js
+++ src/config/load.js
@@ -1,5 +1,4 @@
 import { removeEmptyValues } from './empty.js'
-import { getEnvVarConfig } from './env.js'
 import { loadConfigFile } from './file.js'
 import { mergeConfigs } from './merge.js'
 import { getConfigPath } from './path.js'
@@ -7,11 +6,17 @@ import { getCwd, resolveConfigPaths } from './resolve.js'
 
 // Load the configuration, shallow merged in priority order:
 //  - any CLI or programmatic flags
-//  - any `SPYD_*` environment variables
 //  - `spyd.*`
 // We make sure the `config` property from `spyd.*` is ignored.
+// We do not allow configuring using environment variables because they:
+//  - are not very useful
+//  - add too many constraints related to naming configuration properties:
+//    case-sensitiveness (due to Windows) and fewer allowed delimiters (due
+//    to underscores only being allowed in Unix)
+// We still provide with an `SPYD_CONFIG` environment variable if environment
+// configuration is really needed.
 export const loadConfig = async function (configFlags) {
-  const configNonFile = await loadConfigNonFile(configFlags)
+  const configNonFile = await resolveConfigPaths(configFlags, '.')
 
   const configPath = await getConfigPath(configNonFile)
   const cwd = getCwd(configPath)
@@ -21,12 +26,4 @@ export const loadConfig = async function (configFlags) {
 
   const configC = removeEmptyValues(configB)
   return { config: configC, cwd }
-}
-
-// Load configuration not in `spyd.*`
-const loadConfigNonFile = async function (configFlags) {
-  const envVarConfig = getEnvVarConfig()
-  const configNonFile = { ...envVarConfig, ...configFlags }
-  const configNonFileA = await resolveConfigPaths(configNonFile, '.')
-  return configNonFileA
 }
