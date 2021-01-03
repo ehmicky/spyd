ae06a4e8da03597fb24b2bf056e5d56e423979a5
ehmicky <ehmicky@gmail.com>
2021-01-03 17:50:38 +0100

    Improve task loading

--- src/combination/task.js
+++ src/combination/task.js
@@ -1,14 +1,32 @@
+import { basename } from 'path'
+
 import { UserError } from '../error/main.js'
 
-// Retrieve the absolute paths to the tasks files using the `files` and
-// `cwd` configuration properties
-export const getTasks = function (files) {
-  const taskPathsA = taskPaths.flat()
+// Normalize all tasks into a single array, combining task paths, ids and runner
+export const getTasks = function (tasks) {
+  const tasksA = Object.entries(tasks).flatMap(getRunnerTasks)
 
-  if (taskPathsA.length === 0) {
-    throw new UserError('No tasks file found')
+  if (tasksA.length === 0) {
+    throw new UserError('No tasks files were specified or found.')
   }
 
-  const taskPathsB = [...new Set(taskPathsA)]
-  return taskPathsB
+  return tasksA
 }
+
+const getRunnerTasks = function ([runnerId, taskPaths]) {
+  return taskPaths.map((taskPath) => getRunnerTask({ taskPath, runnerId }))
+}
+
+const getRunnerTask = function ({ taskPath, runnerId }) {
+  const taskId = getTaskId(taskPath)
+  return { taskId, taskPath, runnerId }
+}
+
+// The task id is the filename excluding any file extension.
+// If several dots are present in the filename, they are all considered part of
+// the file extension.
+const getTaskId = function (taskPath) {
+  return basename(taskPath).replace(TASK_ID_REGEXP, '')
+}
+
+const TASK_ID_REGEXP = /\..*/u
