ea976c1a19935f9018d740b8b2aace5a7521fe3e
ehmicky <ehmicky@gmail.com>
2021-01-03 19:48:06 +0100

    Add validation

--- src/plugin/validate.js
+++ src/plugin/validate.js
@@ -1,3 +1,5 @@
+import { PluginError } from '../error/main.js'
+
 // Validate plugins shape
 export const validatePlugins = function (plugins, type) {
   plugins.forEach((plugin) => {
@@ -5,4 +7,17 @@ export const validatePlugins = function (plugins, type) {
   })
 }
 
-const validatePlugin = function ({ id }, type) {}
+const validatePlugin = function ({ id }, type) {
+  if (!PLUGIN_ID_REGEXP.test(id)) {
+    throw new PluginError(`The identifier of the ${type} "${id}" is invalid.
+It should only contain lowercase letters and digits.`)
+  }
+}
+
+// We purposely do not allow any delimiter characters such as . _ - nor
+// uppercase letters because:
+//  - the id is part of the npm package, which has strict validation rules
+//  - we use dots in CLI flags for nested configuration properties
+//  - we want to allow user-defined ids to use either _ or -
+//  - environment variables (`SPYD_*`) only allow _
+const PLUGIN_ID_REGEXP = /^[a-z][a-z0-9]*$/u
