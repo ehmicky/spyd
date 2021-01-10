118a8404f74450ca9622236573ee269268fcb351
ehmicky <ehmicky@gmail.com>
2021-01-10 19:43:14 +0100

    Start adding merge logic

--- src/store/merge.js
+++ src/store/merge.js
@@ -1,26 +1,57 @@
-import { removeDuplicates } from '../combination/duplicate.js'
+import omit from 'omit.js'
+
+import { getCombinationIds } from '../select/ids.js'
 
 // Merge previous results to the last result
 export const mergeResults = function (results) {
-  const result = results[results.length - 1]
-  return result
+  // eslint-disable-next-line fp/no-mutating-methods
+  const [lastResult, ...resultsA] = results.map(addCombinationKeys).reverse()
+  const result = resultsA.reduce(mergePair, lastResult)
+  const resultA = removeCombinationKeys(result)
+  return resultA
+}
+
+// Add stable key describing combinations identity
+const addCombinationKeys = function ({ combinations, ...result }) {
+  const combinationsA = combinations.map(addCombinationKey)
+  return { ...result, combinations: combinationsA }
+}
+
+const addCombinationKey = function (combination) {
+  const key = getCombinationIds(combination).join(' ')
+  return { ...combination, key }
+}
+
+const removeCombinationKeys = function ({ combinations, ...result }) {
+  const combinationsA = combinations.map(removeCombinationKey)
+  return { ...result, combinations: combinationsA }
+}
+
+const removeCombinationKey = function (combination) {
+  return omit(combination, ['combinationsKey'])
 }
 
 const mergePair = function (
-  { systems: previousSystems, combinations: previousCombinations },
-  { id, systems: [system], git, ci, combinations, ...result },
+  { combinations, ...result },
+  { combinations: previousCombinations },
 ) {
-  const systems = mergeSystems(previousSystems, system)
-  const combinationsA = removeDuplicates([
-    ...previousCombinations,
-    ...combinations,
-  ])
-  return { ...result, id, systems, git, ci, combinations: combinationsA }
+  const combinationsA = addNewCombinations(combinations, previousCombinations)
+  return { ...result, combinations: combinationsA }
 }
 
-const mergeSystems = function (previousSystems, system) {
-  const previousSystemsA = previousSystems.filter(
-    (systemA) => systemA.id !== system.id,
+const addNewCombinations = function (combinations, previousCombinations) {
+  const newCombinations = previousCombinations.filter((combination) =>
+    isNewCombination(combination, combinations),
   )
-  return [...previousSystemsA, system]
+  return [...combinations, ...newCombinations]
+}
+
+const isNewCombination = function (combination, combinations) {
+  return !combinations.some((combinationA) =>
+    isSameCombination(combination, combinationA),
+  )
+}
+
+const isSameCombination = function ({ key: keyA }, { key: keyB }) {
+  return keyA === keyB
 }
