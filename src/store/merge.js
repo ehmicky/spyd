5aa896bd73db68d27da171dad27e733c08ed206e
ehmicky <ehmicky@gmail.com>
2021-01-17 23:52:33 +0100

    Add comment

--- src/store/merge.js
+++ src/store/merge.js
@@ -16,7 +16,7 @@ export const mergeResults = function ([lastResult, ...previous]) {
 
 // `include|exclude` can be used to measure specific combinations, allowing
 // incremental benchmarks. However, when reporting benchmarks, we show all
-// combinations:
+// combinations.
 // We do not want to show previous benchmarks' identifiers that have been
 // intentionally deleted.
 //  - However, we still want to show identifiers that have been only filtered
@@ -24,6 +24,20 @@ export const mergeResults = function ([lastResult, ...previous]) {
 //  - We do so by using the `include|exclude` configuration properties to filter
 //    combinations to show.
 //  - This is done in a previous step while loading results.
+// The set of possible combinations is guessed from the current
+// `include`/`exclude` properties:
+//  - We do not use the last measured combinations, nor try to guess what the
+//    current set of possible combinations is.
+//  - This is because guessing the current set of possible combinations is
+//    difficult.
+//     - All combination categories can be changed dynamically with
+//       configuration properties. We cannot whether missing combinations
+//       were temporarily or permanently removed.
+//     - This is especially for systems. There is always only one system per
+//       result. It is hard to know where/whether in the results history the
+//       user intends to stop using each of the previously used systems.
+//  - Instead, we just use any previous combinations matching the current
+//    `include`/`exclude` properties. This is explicit and predictable.
 const mergePair = function (
   { combinations, ...result },
   { combinations: previousCombinations },
