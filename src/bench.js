7b87b6f1a35f390ee674122a47a7f8c93604710a
ehmicky <ehmicky@gmail.com>
2021-01-17 19:17:43 +0100

    Move file

--- src/bench.js
+++ src/bench.js
@@ -1,8 +1,11 @@
+import { v4 as uuidv4 } from 'uuid'
+
 import { getCombinations } from './combination/main.js'
-import { addResultInfo } from './info.js'
 import { measureCombinations } from './measure/main.js'
 import { getStopState } from './measure/stop.js'
 import { startProgress, endProgress } from './progress/start_end.js'
+import { getSystem } from './system/info.js'
+import { cleanObject } from './utils/clean.js'
 
 // Perform a new benchmark
 export const performBenchmark = async function (config) {
@@ -44,4 +47,14 @@ const getResult = async function ({
   })
   const result = addResultInfo({ combinations: combinationsA, config })
   return result
+}
+
+// Add more information to the final result and normalize/sort it
+const addResultInfo = function ({ combinations, config }) {
+  const id = uuidv4()
+  const timestamp = new Date().toISOString()
+  const system = getSystem({ combinations, config })
+  const result = { id, timestamp, system, combinations }
+  const resultA = cleanObject(result)
+  return resultA
 }
