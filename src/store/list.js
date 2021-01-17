d12df4ea3dd95e60d023dd0bef989490ba57f170
ehmicky <ehmicky@gmail.com>
2021-01-17 15:50:14 +0100

    Add comment

--- src/store/list.js
+++ src/store/list.js
@@ -2,6 +2,9 @@ import { UserError } from '../error/main.js'
 import { loadResults } from '../normalize/load.js'
 
 // List, sort, filter and normalize all results
+// This is performed at the beginning of all commands because this allows:
+//  - Failing fast if there is a problem with the store
+//  - Including previous|diff in live reporting
 export const listStore = async function ({ store, include, exclude }) {
   const results = await callList(store)
   const resultsA = loadResults({ results, include, exclude })
