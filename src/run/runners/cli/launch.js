828a722ac9893c83a5ca6c3286ffee7ed8bac631
ehmicky <ehmicky@gmail.com>
2021-01-24 21:47:52 +0100

    Add Bash version

--- src/run/runners/cli/launch.js
+++ src/run/runners/cli/launch.js
@@ -2,7 +2,18 @@ import { validateConfig } from './validate.js'
 
 export const launch = function (runnerConfig) {
   validateConfig(runnerConfig)
-  return { spawn: ['node', MAIN_PATH], versions: {} }
+  const versions = getVersions(runnerConfig)
+  return { spawn: ['node', MAIN_PATH], versions }
 }
 
 const MAIN_PATH = `${__dirname}/main.js`
+
+const getVersions = function ({ shell = 'none' }) {
+  return SHELL_VERSIONS[shell]
+}
+
+const SHELL_VERSIONS = {
+  none: {},
+  sh: {},
+  bash: { Bash: ['bash', '-c', 'echo $BASH_VERSION'] },
+}
