a9c2ded818094aed632848096e0fdf8c890c28dc
ehmicky <ehmicky@gmail.com>
2021-01-31 19:54:55 +0100

    Add comment

--- src/preview/report.js
+++ src/preview/report.js
@@ -1,7 +1,12 @@
 import { getFinalResult } from '../measure/init.js'
 import { reportPreview } from '../report/main.js'
 
-// Preview results progressively, as combinations are being measured
+// Preview results progressively, as combinations are being measured.
+// Reporters should:
+//  - Handle combinations not measured yet, i.e. with `undefined` `stats`
+//  - Try to limit the amount of flicker between previews
+//     - For example, all combinations should be shown even if not measured yet.
+//     - And the size of table should not change between previews.
 export const previewCombinations = async function ({
   combinations,
   newCombination,
