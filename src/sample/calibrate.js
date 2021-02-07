2865e7817a9b84a82de3e1e7fba2bdeaa233f42d
ehmicky <ehmicky@gmail.com>
2021-02-07 13:15:08 +0100

    Add comments

--- src/sample/calibrate.js
+++ src/sample/calibrate.js
@@ -6,11 +6,35 @@
 // During calibration, `repeat` is not calibrated and measures can be greatly
 // both inaccurate and imprecise.
 //   - Therefore we remove the measures taken during calibration.
-//   - In case there is not enough duration to finish calibration, we keep only
-//     the last sample measures.
+//   - We also do not report them, including in previews.
 // We only reset cumulated stats.
 //   - We do not reset stats which only use the last sample when those
 //     cumulated stats are reset, such as `stats` and `repeat`.
+// Calibrating will always remove the first sample (cold start):
+//  - A downside is that this means cold starts will be included or not
+//    depending on whether they are fast enough to use repeat loops.
+//  - This can make it harder to compare some stats (especially `max`) between
+//    combinations if some have repeat loops while others not.
+//  - However, this problem is more general. Using repeat loops will always
+//    change how the mean, min, max, standard deviation are computed since it
+//    uses an arithmetic mean.
+// The alternative solutions have bigger problems:
+//  - Excluding all first samples, even without a repeat loop:
+//     - This prevents running the combination only once, which is especially
+//       problematic with `duration: 1`
+//     - The first sample includes some useful stats since many functions are
+//       only run once in real.
+//     - This makes the first preview take longer to show up even if the
+//       combination does not require any repeat loops.
+//  - Excluding first samples except when there was not enough time to measure
+//    more:
+//     - This creates a big difference of stats (especially median and max)
+//       depending on the `duration`
+//     - This can lead to comparing combinations with and without the cold start
+//       included in their stats
+//  - Excluding first samples except when `duration: 1`
+//     - Switching between `duration: 1` and others `duration` should show the
+//       same results when lasting the same amount of time.
 export const calibrateReset = function ({
   calibrated,
   measures,
