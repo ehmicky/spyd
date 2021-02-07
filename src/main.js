5e1ff990d3347742fb4b3c77712e0da7d1d39e3c
ehmicky <ehmicky@gmail.com>
2021-02-07 23:12:41 +0100

    Refactoring

--- src/main.js
+++ src/main.js
@@ -7,41 +7,38 @@ import {
   addToStore,
   getFromStore,
   listAll,
-  listStore,
   removeFromStore,
 } from './store/main.js'
 
 // Measure code defined in a tasks file and report the results.
 // Default command.
 export const bench = async function (configFlags) {
-  const confiA = await getConfig('bench', configFlags)
-  const results = await listAll(confiA)
-  const { rawResult, result, stopped } = await performBenchmark(confiA, results)
-  await addToStore(rawResult, confiA, stopped)
-  await report(result, confiA)
-  checkLimits(result, confiA)
+  const config = await getConfig('bench', configFlags)
+  const results = await listAll(config)
+  const { rawResult, result, stopped } = await performBenchmark(config, results)
+  await addToStore(rawResult, config, stopped)
+  await report(result, config)
+  checkLimits(result, config)
   return result
 }
 
 // Show a previous result
 export const show = async function (configFlags) {
-  const { delta, ...configA } = await getConfig('show', configFlags)
-  const results = await listStore(configA)
-  const result = await getFromStore(results, delta, configA)
-  await report(result, configA)
+  const { delta, ...config } = await getConfig('show', configFlags)
+  const result = await getFromStore(delta, config)
+  await report(result, config)
   return result
 }
 
 // Remove a previous result
 export const remove = async function (configFlags) {
-  const { delta, ...configA } = await getConfig('remove', configFlags)
-  const results = await listStore(configA)
-  const result = await getFromStore(results, delta, configA)
-  await removeFromStore(result, configA)
+  const { delta, ...config } = await getConfig('remove', configFlags)
+  const result = await getFromStore(delta, config)
+  await removeFromStore(result, config)
 }
 
 // Execute tasks without benchmarking them
 export const exec = async function (configFlags) {
-  const configA = await getConfig('exec', configFlags)
-  await performExec(configA)
+  const config = await getConfig('exec', configFlags)
+  await performExec(config)
 }
--- src/store/main.js
+++ src/store/main.js
@@ -16,18 +16,9 @@ export const listAll = async function (config) {
   return resultsA
 }
 
-// List, sort, filter and normalize all results
-// This is performed at the beginning of all commands because this allows:
-//  - Failing fast if there is a problem with the store
-//  - Including previous|diff in results preview
-export const listStore = async function ({ cwd, include, exclude }) {
-  const results = await listResults(cwd)
-  const resultsA = loadResults({ results, include, exclude })
-  return resultsA
-}
-
 // Get a previous result by `count` or `timestamp`
-export const getFromStore = async function (results, delta, config) {
+export const getFromStore = async function (delta, config) {
+  const results = await listStore(config)
   const { lastResult, previous } = await listResultsByDelta(
     results,
     delta,
@@ -36,6 +27,16 @@ export const getFromStore = async function (results, delta, config) {
   const previousA = await applySince(previous, config)
   const result = mergeResults(lastResult, previousA)
   return result
+}
+
+// List, sort, filter and normalize all results
+// This is performed at the beginning of all commands because this allows:
+//  - Failing fast if there is a problem with the store
+//  - Including previous|diff in results preview
+const listStore = async function ({ cwd, include, exclude }) {
+  const results = await listResults(cwd)
+  const resultsA = loadResults({ results, include, exclude })
+  return resultsA
 }
 
 const listResultsByDelta = async function (results, delta, { cwd }) {
