b303e80ff8f2ae34224750fa0dcb24aae83d11a9
ehmicky <ehmicky@gmail.com>
2021-02-21 13:57:35 +0100

    Improve Windows support

--- src/report/utils/colors.js
+++ src/report/utils/colors.js
@@ -1,11 +1,11 @@
-import { cyan, dim, red } from 'chalk'
+import { cyan, gray, red } from 'chalk'
 
 export const titleColor = cyan.bold
 export const subtitleColor = cyan
-export const fieldColor = dim.italic
+export const fieldColor = gray.italic
 export const goodColor = cyan
 export const badColor = red
 export const errorColor = red.inverse.bold
-export const separatorColor = dim
-export const noteColor = dim
-export const suffixColor = dim
+export const separatorColor = gray
+export const noteColor = gray
+export const suffixColor = gray
