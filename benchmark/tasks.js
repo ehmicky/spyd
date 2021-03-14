5e618e7458d61db6194f1c7882c2fa259373348b
ehmicky <ehmicky@gmail.com>
2021-03-14 23:10:53 +0100

    Add more tasks

--- benchmark/tasks.js
+++ benchmark/tasks.js
@@ -1,7 +1,30 @@
-module.exports.one = () => {
+const { execFile } = require('child_process')
+const { setTimeout } = require('timers/promises')
+const { promisify } = require('util')
+
+const pExecFile = promisify(execFile)
+
+module.exports.random = () => {
   Math.random()
 }
 
-module.exports.two = () => {
-  Math.random()
+module.exports.empty = () => {}
+
+module.exports.fixed = async () => {
+  await setTimeout(50)
+}
+
+module.exports.uniform = async () => {
+  await setTimeout(100 * Math.random())
+}
+
+let count = 0
+
+module.exports.growing = async () => {
+  count += 1
+  await setTimeout(count)
+}
+
+module.exports.real = async () => {
+  await pExecFile('node', [`${__dirname}/..`])
 }
