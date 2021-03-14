d02be9d9eabd74127a3b12d50adf72031275e87e
ehmicky <ehmicky@gmail.com>
2021-03-14 13:53:28 +0100

    Refactoring

--- src/report/utils/histogram/smooth.js
+++ src/report/utils/histogram/smooth.js
@@ -2,8 +2,8 @@ export const smoothHistogram = function (counts, smoothPercentage) {
   const countsLength = counts.length
 
   const smoothSize = Math.max((countsLength * smoothPercentage - 1) / 2, 0)
-  const fullSmoothSize = Math.ceil(smoothSize)
-  const partialSmoothSize = smoothSize - fullSmoothSize + 1
+  const fullSmoothSize = Math.floor(smoothSize)
+  const partialSmoothSize = smoothSize - fullSmoothSize
 
   // eslint-disable-next-line unicorn/no-new-array
   const smoothedCounts = new Array(countsLength)
@@ -31,8 +31,8 @@ const getSmoothedCount = function (
   fullSmoothSize,
   partialSmoothSize,
 ) {
-  const startIndex = countIndex - fullSmoothSize
-  const endIndex = countIndex + fullSmoothSize
+  const startIndex = countIndex - fullSmoothSize - 1
+  const endIndex = countIndex + fullSmoothSize + 1
   const startFullIndex = Math.max(startIndex + 1, 0)
   const endFullIndex = Math.min(endIndex - 1, countsLength - 1)
 
@@ -57,8 +57,13 @@ const getSmoothedCount = function (
   return smoothedCount
 }
 
-// const histogramExampleA = []
-// const histogramExampleB = [5]
-// const histogramExampleC = [5, 10]
+const histogramExampleA = []
+console.log(smoothHistogram(histogramExampleA, 0.5))
+const histogramExampleB = [5]
+console.log(smoothHistogram(histogramExampleB, 0.5))
 const histogramExampleD = [5, 10, 56, 12, 74, 65, 54, 10, 98, 98]
+console.log(smoothHistogram(histogramExampleD, 1))
 console.log(smoothHistogram(histogramExampleD, 0.4))
+console.log(smoothHistogram(histogramExampleD, 0.35))
+console.log(smoothHistogram(histogramExampleD, 0.05))
+console.log(smoothHistogram(histogramExampleD, 0))
