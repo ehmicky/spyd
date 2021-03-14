2b5a3bddc8b1dd7e7a1b602172564490a77f4733
ehmicky <ehmicky@gmail.com>
2021-03-14 21:24:38 +0100

    Add comment

--- src/report/utils/histogram/main.js
+++ src/report/utils/histogram/main.js
@@ -52,8 +52,9 @@ ${bottomLine}
 ${abscissa}`
 }
 
-// Compute position of the median tick
+// Compute position of the median tick.
 // When `histogram` has a single item, it is in the first bucket.
+// Also compute the maximum width between the median and either the start or end
 const getMedianPosition = function ({ median, low, high, width }) {
   const medianPercentage = high === low ? 0 : (median - low) / (high - low)
   const indexWidth = width - 1
