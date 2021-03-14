2e409aae30996fa806eff69a1abab72b808cda3e
ehmicky <ehmicky@gmail.com>
2021-03-14 13:53:53 +0100

    Refactoring

--- src/report/utils/histogram/smooth.js
+++ src/report/utils/histogram/smooth.js
@@ -36,34 +36,39 @@ const getSmoothedCount = function (
   const startFullIndex = Math.max(startIndex + 1, 0)
   const endFullIndex = Math.min(endIndex - 1, countsLength - 1)
 
-  const smoothedCount = []
+  let smoothedCount = 0
+  let size = 0
 
   if (startIndex >= 0) {
-    // eslint-disable-next-line fp/no-mutating-methods
-    smoothedCount.push([startIndex, partialSmoothSize])
+    smoothedCount += counts[startIndex] * partialSmoothSize
+    size += partialSmoothSize
   }
 
   // eslint-disable-next-line fp/no-loops, fp/no-let, fp/no-mutation
   for (let index = startFullIndex; index <= endFullIndex; index += 1) {
-    // eslint-disable-next-line fp/no-mutating-methods
-    smoothedCount.push([index, 1])
+    smoothedCount += counts[index]
+    size += 1
   }
 
   if (endIndex <= countsLength - 1) {
-    // eslint-disable-next-line fp/no-mutating-methods
-    smoothedCount.push([endIndex, partialSmoothSize])
+    smoothedCount += counts[endIndex] * partialSmoothSize
+    size += partialSmoothSize
   }
 
-  return smoothedCount
+  return smoothedCount / size
 }
 
 const histogramExampleA = []
 console.log(smoothHistogram(histogramExampleA, 0.5))
 const histogramExampleB = [5]
 console.log(smoothHistogram(histogramExampleB, 0.5))
-const histogramExampleD = [5, 10, 56, 12, 74, 65, 54, 10, 98, 98]
+const histogramExampleC = [5, 10]
+console.log(smoothHistogram(histogramExampleC, 0.5))
+console.log(smoothHistogram(histogramExampleC, 0.75))
+const histogramExampleD = [98, 10, 56, 12, 74, 65, 54, 10, 98, 98]
 console.log(smoothHistogram(histogramExampleD, 1))
 console.log(smoothHistogram(histogramExampleD, 0.4))
 console.log(smoothHistogram(histogramExampleD, 0.35))
+console.log(smoothHistogram(histogramExampleD, 0.2))
 console.log(smoothHistogram(histogramExampleD, 0.05))
 console.log(smoothHistogram(histogramExampleD, 0))
