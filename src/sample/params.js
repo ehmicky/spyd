608c874c8f35b01990617c5c39451bf2c03df03d
ehmicky <ehmicky@gmail.com>
2021-04-04 17:58:43 +0200

    Improve SAMPLE_DURATION

--- src/sample/params.js
+++ src/sample/params.js
@@ -23,21 +23,17 @@ export const getParams = function ({
 //   - It can be used to prevent memory crash in runners.
 // The `maxLoops` is computed so the sample lasts a specific duration. We use a
 // hardcoded duration because:
-//   - It ensures each sample has roughly the same duration. This is because
-//     some runtime optimization applies as a new sample is run. We do not want
-//     this runtime to spread differently between samples due to different
-//     durations.
-//   - It is small enough to ensure that tasks with slow measures aggregation
-//     (due to having lots of measures) do not prevent other tasks' previews
-//   - It is large enough to spend enough time measuring as opposed to IPC and
-//     combination orchestration
-//   - It is large enough so cold starts at the beginning of each sample are a
-//     small percentage of the overall measures.
-// We purposely avoid using `duration` so that increasing `duration`
-// does not change measures.
+//   - It should be as high as possible. Since the upper limit is based on
+//     user-perceived duration (preview refresh rate, duration to stop, etc.),
+//     we do not need machine-friendly automated durations.
+//   - It ensures each sample has roughly the same duration.
+//      - This is because some runtime optimization applies as a new sample is
+//        run. We do not want this runtime to spread differently between samples
+//        due to different durations.
 // Since `maxLoops` aims to last a specific duration, we need the last
-// `measureDuration` to estimate it. If the `repeat` changes, we need to take it
-// into account as well, which is especially important during calibration.
+// `measureDuration` to estimate it.
+//   - If the `repeat` changes, we need to take it into account as well, which
+//     is especially important during calibration.
 const getMaxLoops = function ({
   measureDuration,
   repeat,
@@ -57,7 +53,20 @@ const getMaxLoops = function ({
   return Math.max(targetMaxLoops, 1)
 }
 
-// 100ms
-// Small enough to give responsive reporting.
-// Large enough to spend enough time measuring.
-export const SAMPLE_DURATION = 1e8
+// 500ms
+// Higher value leads to:
+//   - Less frequent previews
+//   - Longer to wait for combinations to stop when user requests it.
+//       - The sample duration should always be less than the abort delay.
+//   - Higher minimum duration for any combination
+//   - More unnecessary are measured on average, once the `precision` threshold
+//     has been reached.
+// Lower value leads to:
+//   - Higher `stats.stdev` and `stats.high`. This is because new samples have
+//     a small cold start. This is especially true for fast|low-complexity tasks
+//   - Less duration measuring as opposed to IPC, runner internal logic, stats
+//     computation and preview reporting
+//   - Due to the above point, the real sample duration is less close to the
+//     target.
+// The value does not impact `stats.median` much though.
+export const SAMPLE_DURATION = 3e8
