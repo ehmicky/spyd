4ad7e3337a95bb2ddc2238bb044575828a6c3385
ehmicky <ehmicky@gmail.com>
2021-04-18 23:13:19 +0200

    Improve error handling

--- src/measure/handle.js
+++ src/measure/handle.js
@@ -41,15 +41,21 @@ export const handleErrorsAndMeasure = async function ({
   }
 }
 
+const prependTaskPrefix = function (error, { taskId }, stage) {
+  if (stage === 'init') {
+    return
+  }
+
+  const taskPrefix = `In task "${taskId}"`
+  error.message = `${taskPrefix}:\n${error.message}`
+}
+
 // Measure all combinations, until there is no `duration` left.
 // When any combination errors, we end measuring.
 // We also do this when the user stopped the benchmark (e.g. with CTRL-C).
 // We still perform each combination ends, for cleanup.
 // `end` is always called, for runner-specific cleanup, providing `start`
 // completed.
-//  - If an error happens in `end` itself, it is propagated itself.
-//  - This is because `end` should gracefully handle any possible
-//    interruption, regardless of what's the current global state.
 const measureAllCombinations = async function ({
   combination,
   duration,
@@ -60,9 +66,28 @@ const measureAllCombinations = async function ({
   server,
 }) {
   const taskIds = await startCombination(combination, server)
+  const stats = await eRunEvents({
+    duration,
+    previewConfig,
+    previewState,
+    stopState,
+    stage,
+    server,
+  })
+  await endCombination(server)
+  return { stats, taskIds }
+}
 
+const eRunEvents = async function ({
+  duration,
+  previewConfig,
+  previewState,
+  stopState,
+  stage,
+  server,
+}) {
   try {
-    const stats = await runEvents({
+    return await runEvents({
       duration,
       previewConfig,
       previewState,
@@ -70,14 +95,22 @@ const measureAllCombinations = async function ({
       stage,
       server,
     })
-    return { stats, taskIds }
-  } finally {
-    await endCombination(server)
+  } catch (error) {
+    await silentEndCombination(server)
+    throw error
   }
 }
 
+// `after` and `end` are called on exceptions.
+// If an exception happens inside those themselves, it is ignored because it
+// might be due to the runner being in a bad state due to the first exception.
+const silentEndCombination = async function (server) {
+  try {
+    await endCombination(server)
+  } catch {}
+}
+
 // `after` is always called, for cleanup, providing `before` completed.
-// If an error happens in `after` itself, it is propagated instead.
 const runEvents = async function ({
   duration,
   previewConfig,
@@ -91,10 +124,29 @@ const runEvents = async function ({
   }
 
   await beforeCombination(previewState, server)
+  const stats = await ePerformLoop({
+    duration,
+    previewConfig,
+    previewState,
+    stopState,
+    stage,
+    server,
+  })
+  await afterCombination(previewState, server)
+  return stats
+}
 
+const ePerformLoop = async function ({
+  duration,
+  previewConfig,
+  previewState,
+  stopState,
+  stage,
+  server,
+}) {
   try {
     const minLoopDuration = await getMinLoopDuration(server, stage)
-    const stats = await performMeasureLoop({
+    return await performMeasureLoop({
       duration,
       previewConfig,
       previewState,
@@ -103,17 +155,14 @@ const runEvents = async function ({
       server,
       minLoopDuration,
     })
-    return stats
-  } finally {
-    await afterCombination(previewState, server)
+  } catch (error) {
+    await silentAfterCombination(previewState, server)
+    throw error
   }
 }
 
-const prependTaskPrefix = function (error, { taskId }, stage) {
-  if (stage === 'init') {
-    return
-  }
-
-  const taskPrefix = `In task "${taskId}"`
-  error.message = `${taskPrefix}:\n${error.message}`
+const silentAfterCombination = async function (previewState, server) {
+  try {
+    await afterCombination(previewState, server)
+  } catch {}
 }
