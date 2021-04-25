c75cc27e0d5fa411363fc979303169c6da347e66
ehmicky <ehmicky@gmail.com>
2021-04-25 13:53:37 +0200

    Refactoring

--- src/report/main.js
+++ src/report/main.js
@@ -8,8 +8,19 @@ import { endReporters } from './start_end.js'
 
 // Report final results in `bench` and `show` commands.
 export const reportBenchShow = async function (result, { reporters, titles }) {
-  const contents = await endReport(result, { reporters, titles })
+  const contents = await getContents(result, { reporters, titles })
+  await endReporters(reporters)
   await outputContents(contents)
+}
+
+// Report final results in `remove` command.
+// Reporting is shown so the user can be clear about which result was removed,
+// and provide with confirmation. So we only need to print in the terminal,
+// not output|insert files.
+export const reportRemove = async function (result, { reporters, titles }) {
+  const contents = await getContents(result, { reporters, titles })
+  await endReporters(reporters)
+  await outputTtyContents(contents)
 }
 
 // Report preview results in `bench` command.
@@ -19,19 +30,4 @@ export const reportPreview = async function (result, { reporters, titles }) {
   const contents = await getContents(result, { reporters, titles })
   const previewReport = computeTtyContents(contents)
   return previewReport
-}
-
-// Report final results in `remove` command.
-// Reporting is shown so the user can be clear about which result was removed,
-// and provide with confirmation. So we only need to print in the terminal,
-// not output|insert files.
-export const reportRemove = async function (result, { reporters, titles }) {
-  const contents = await endReport(result, { reporters, titles })
-  await outputTtyContents(contents)
-}
-
-const endReport = async function (result, { reporters, titles }) {
-  const contents = await getContents(result, { reporters, titles })
-  await endReporters(reporters)
-  return contents
 }
