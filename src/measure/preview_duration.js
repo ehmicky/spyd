117809d6ee74442567233e58221df658c53c6886
ehmicky <ehmicky@gmail.com>
2021-04-25 23:52:22 +0200

    Add comments

--- src/measure/preview_duration.js
+++ src/measure/preview_duration.js
@@ -4,13 +4,6 @@ import { updatePreview } from '../preview/update.js'
 import { getLoopsFromLength } from '../stats/extreme.js'
 import { getLengthForMoe } from '../stats/moe.js'
 
-// Update the combination start and expected end.
-// This is combined with the current timestamp to compute the expected duration
-// left and percentage in previews.
-// Reporting the expected duration left is important because this helps:
-//   - Chosing between different `precision`
-//   - With user impatience or with planning other things while measuring is
-//     ongoing
 // Done when combination starts
 export const startCombinationPreview = async function (previewState, index) {
   // eslint-disable-next-line fp/no-mutating-assign
@@ -22,6 +15,13 @@ export const startCombinationPreview = async function (previewState, index) {
   await updatePreview(previewState)
 }
 
+// Update the combination start and expected end.
+// This is combined with the current timestamp to compute the expected duration
+// left and percentage in previews.
+// Reporting the expected duration left is important because this helps:
+//   - Chosing between different `precision`
+//   - With user impatience or with planning other things while measuring is
+//     ongoing
 // Done when combination's sample starts
 export const updateCombinationPreview = function ({
   stats,
@@ -76,8 +76,20 @@ const getCombinationEnd = function (samplesTarget, { sampleDurationMean }) {
   return now() + durationLeft
 }
 
-// Blend the previous `combinationEnd` with the new one to stabilize its change
-// between samples.
+// The `combinationEnd` estimation is based on `stdev`.
+//  - However `stdev` increases and decreases a lot and different rates.
+//  - This make `combinationEnd` change a lot from sample to sample.
+// This eventually results in issues with `durationLeft` and the progress bar
+// with a poor developer experience. They:
+//  - Jitter
+//  - Sometimes go backward
+//  - Sometimes increase|decrease a lot for just a few samples
+// We fix this by smoothing the change rate of `combinationEnd` between samples:
+//  - We blend the previous `combinationEnd` with the new one
+//  - We automatically compute the `smoothRatio` used to know how much those
+//    two should be blended
+// `previousCombinationEnd` might be in the past due to that smoothing, so we
+// need to use `Math.max(..., now())` on it.
 const smoothCombinationEnd = function ({
   combinationEnd,
   previousCombinationEnd,
@@ -95,14 +107,50 @@ const smoothCombinationEnd = function ({
   )
 }
 
+// `smoothRatio` decides how much the previous `combinationEnd` should be
+// retained instead of the new one, in order to smooth the change rate.
+// The smoothing is weaker at the beginning or end than the middle of the
+// combination. This is because smoothing decreasing the accuracy of
+// `combinationEnd` which is a problem at the:
+//  - beginning: since the accuracy of stdev is still low, it should not be
+//     retained for too long
+//  - end: since the `combinationEnd` needs to fully converge so percentage
+//    becomes `100%` and `durationLeft` 0
+// We do this by making the smoothing proportional to the number of samples from
+// either the start or the end:
+//  - This also ensures that the smoothing is applied the same regardless of
+//    the total number of samples
+//     - In other words, the smoothing is relative to a percentage of the total
+//       combination duration, not to a specific absolute number of samples
+//  - The start refers to the first sample that shows a preview, not the first
+//    sample measured, reported, nor calibrated.
+//     - This ensures that the first `combinationEnd` always have the same
+//       weight compared to the second one.
+//     - We track this by incrementing `previewSamples` to excluded non-preview
+//       samples
 const getSmoothRatio = function (previewSamples, samplesTarget) {
   const samplesToEdge = Math.min(previewSamples + 1, samplesTarget)
   const smoothSamples = samplesToEdge * ADJUSTED_SMOOTH_PERIOD
   return SMOOTH_CLOSENESS ** (1 / smoothSamples)
 }
 
+// Percentage of the total number of samples.
+// A `combinationEnd` change takes that many samples to be almost fully
+// integrated. How close is "almost" is decided by `SMOOTH_CLOSENESS`.
+// For example, with 100 samples, if the previous `combinationEnd` is 100, the
+// new one is 200 and they are not changing, then after 65 samples,
+// `combinationEnd` would be at 195.
+// A lower value reduces the smoothing done on non-lasting `combinationEnd`
+// changes.
+// A higher value makes lasting `combinationEnd` take longer to take effect.
 const SMOOTH_PERIOD = 0.65
+// Since `smoothPeriod` is applied proportionally to `samplesToEdge`, it is
+// fully used in the middle of the benchmark, but not used at all in the start
+// and end. Therefore, to keep the mean smoothPeriod, we need to multiply by 2.
 const ADJUSTED_SMOOTH_PERIOD = SMOOTH_PERIOD * 2
+// There is no reason to change this value instead of `SMOOTH_PERIOD` since it
+// completely coupled to it: doing `SMOOTH_CLOSENESS **= number` has the same
+// effect as doing `SMOOTH_PERIOD *= number`
 const SMOOTH_CLOSENESS = 0.05
 
 // Done when combination ends
