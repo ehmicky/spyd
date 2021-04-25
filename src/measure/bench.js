11628f973947b4dfa24223f25192ee3811d37a39
ehmicky <ehmicky@gmail.com>
2021-04-25 19:25:09 +0200

    Add comment

--- src/measure/bench.js
+++ src/measure/bench.js
@@ -6,7 +6,14 @@ import { startPreview, endPreview } from '../preview/start_end.js'
 
 import { measureCombinations } from './several.js'
 
-// Perform a new benchmark
+// Perform a new benchmark.
+// There is no watch mode because:
+//   - It would encourage user to perform actions while measuring is ongoing,
+//     making it imprecise
+//   - If file edit was not meant to start measuring, doing so would slow down
+//     subsequent editing experience
+//   - It would require either guessing imported files, or asking user to
+//     specify them with a separate configuration property
 export const performBenchmark = async function (
   config,
   combinations,
