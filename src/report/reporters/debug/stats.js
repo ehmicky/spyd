68b71649b822b16931c9ac5ff00be7f440cf8964
ehmicky <ehmicky@gmail.com>
2021-05-02 17:50:10 +0200

    Refactoring

--- src/report/reporters/debug/stats.js
+++ src/report/reporters/debug/stats.js
@@ -1,4 +1,4 @@
-import { PADDED_SEPARATOR } from '../../utils/separator.js'
+import { PADDING_WIDTH, SEPARATOR_WIDTH } from '../../utils/separator.js'
 
 import { NAME_RIGHT_PADDING_WIDTH, STAT_COLUMNS } from './column.js'
 import { getEmptyRowWidth, getColumnWidth } from './header.js'
@@ -6,15 +6,9 @@ import { getEmptyRowWidth, getColumnWidth } from './header.js'
 export const getAllStatColumns = function ({ titles, stats }, screenWidth) {
   const availableWidth =
     screenWidth - getEmptyRowWidth(titles) - NAME_RIGHT_PADDING_WIDTH
-  const separatorWidth = PADDED_SEPARATOR.length
   const { allStatColumns } = STAT_COLUMNS.reduce(
     (state, name) =>
-      reduceAllStateColumns(state, {
-        name,
-        stats,
-        separatorWidth,
-        availableWidth,
-      }),
+      reduceAllStateColumns(state, { name, stats, availableWidth }),
     { allStatColumns: [[]], widthLeft: availableWidth },
   )
   // eslint-disable-next-line fp/no-mutating-methods
@@ -23,17 +17,21 @@ export const getAllStatColumns = function ({ titles, stats }, screenWidth) {
 
 const reduceAllStateColumns = function (
   { allStatColumns: [statColumns, ...previousStatColumns], widthLeft },
-  { name, stats, separatorWidth, availableWidth },
+  { name, stats, availableWidth },
 ) {
-  const statWidth = getColumnWidth(stats, name) + separatorWidth
+  const isFirstColumn = statColumns.length === 0
+  const columnWidth = getColumnWidth(stats, name)
 
-  return statColumns.length === 0 || widthLeft >= statWidth
+  return isFirstColumn || widthLeft >= columnWidth + SEPARATOR_WIDTH
     ? {
         allStatColumns: [[...statColumns, name], ...previousStatColumns],
-        widthLeft: widthLeft - statWidth,
+        widthLeft:
+          widthLeft -
+          columnWidth -
+          (isFirstColumn ? PADDING_WIDTH : SEPARATOR_WIDTH),
       }
     : {
         allStatColumns: [[name], statColumns, ...previousStatColumns],
-        widthLeft: availableWidth - statWidth,
+        widthLeft: availableWidth - columnWidth - PADDING_WIDTH,
       }
 }
--- src/report/utils/separator.js
+++ src/report/utils/separator.js
@@ -3,5 +3,8 @@ import { separatorColor } from './colors.js'
 // Separators between columns
 // Works with all terminals
 const SEPARATOR_SIGN = '\u2502'
-export const PADDED_SEPARATOR = ` ${SEPARATOR_SIGN} `
+export const PADDING_WIDTH = 1
+const PADDING = ' '.repeat(PADDING_WIDTH)
+const PADDED_SEPARATOR = `${PADDING}${SEPARATOR_SIGN}${PADDING}`
+export const SEPARATOR_WIDTH = PADDED_SEPARATOR.length
 export const COLUMN_SEPARATOR = separatorColor(PADDED_SEPARATOR)
