f5749c614d3a6626af0b7898fdd384694520c3f1
ehmicky <ehmicky@gmail.com>
2021-05-02 18:03:47 +0200

    Fix wrapping

--- src/preview/wrap.js
+++ src/preview/wrap.js
@@ -5,7 +5,9 @@ import { getScreenWidth } from '../report/tty.js'
 // Rows that are larger than the terminal screen width must be broken down
 // (line wrapping). This is because the scrolling logic needs to know the number
 // of physical rows, not logical rows.
+// We need to subtract 1 to take into account final newlines at the end of each
+// row.
 export const wrapRows = function (string) {
   const screenWidth = getScreenWidth()
-  return wrapAnsi(string, screenWidth, { hard: true, trim: false })
+  return wrapAnsi(string, screenWidth - 1, { hard: true, trim: false })
 }
