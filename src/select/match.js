b6336709f97ce334b06e733c7eb1767e2d6b59f6
ehmicky <ehmicky@gmail.com>
2021-05-02 21:14:01 +0200

    Add comment

--- src/select/match.js
+++ src/select/match.js
@@ -1,7 +1,19 @@
 import { getCombinationIds } from '../combination/ids.js'
 
 // Select combinations according to the `select` or `limit` configuration
-// properties
+// properties.
+// This logic:
+//  - Is expressive enough to allow selecting any sets of combinations,
+//    regardless of the number of categories
+//  - While still have a small number of complexity, only: list of identifiers,
+//    "not" and top-level array.
+//  - Avoids punctuation or hard-to-remember syntax
+// The following cases can be expressed for example, assuming two categories
+// going from a-z and 0-99:
+//  - inclusion of almost all (but not all) combinations of a category,
+//    e.g. include a1-99 and b-z0
+//  - vice-versa, e.g. include all but a1-99 and b-z0
+//  - inclusion of all but specific combinations, e.g. b2 and o5
 export const matchSelectors = function (combination, selectors) {
   if (selectors.length === 0) {
     return true
