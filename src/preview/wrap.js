aaa77836bc012139ac9b126e2815f58e804915c6
ehmicky <ehmicky@gmail.com>
2021-05-02 19:28:59 +0200

    Refactoring

--- src/preview/wrap.js
+++ src/preview/wrap.js
@@ -5,10 +5,14 @@ import { getScreenWidth, getPaddedScreenWidth } from '../report/tty.js'
 // Rows that are larger than the terminal screen width must be broken down
 // (line wrapping). This is because the scrolling logic needs to know the number
 // of physical rows, not logical rows.
-export const wrapPaddedRows = function (string) {
-  return wrapAnsi(string, getPaddedScreenWidth(), { hard: true, trim: false })
+export const wrapRows = function (string) {
+  return wrapString(string, getScreenWidth())
 }
 
-export const wrapRows = function (string) {
-  return wrapAnsi(string, getScreenWidth(), { hard: true, trim: false })
+export const wrapPaddedRows = function (string) {
+  return wrapString(string, getPaddedScreenWidth())
+}
+
+const wrapString = function (string, width) {
+  return wrapAnsi(string, width, { hard: true, trim: false })
 }
