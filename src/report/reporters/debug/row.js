76940fa220b5836e43867d8cb6828c7d6d7ca6e5
ehmicky <ehmicky@gmail.com>
2021-05-02 19:46:34 +0200

    Simplify code

--- src/report/reporters/debug/row.js
+++ src/report/reporters/debug/row.js
@@ -7,7 +7,9 @@ import { getCombinationName } from '../../utils/title.js'
 // Retrieve a single row, including the row name
 export const getRow = function ({ titles, stats }, columns, columnWidth) {
   const firstCell = getFirstCell(titles)
-  const cells = getCells(stats, columns, columnWidth)
+  const cells = columns
+    .map((column) => getCell(stats, column, columnWidth))
+    .join(COLUMN_SEPARATOR)
   return `${firstCell}${cells}`
 }
 
@@ -22,13 +24,6 @@ const getFirstCell = function (titles) {
 
 const FIRST_CELL_PADDING_WIDTH = 2
 const FIRST_CELL_PADDING = ' '.repeat(FIRST_CELL_PADDING_WIDTH)
-
-// Retrieve a single row, except the row name
-const getCells = function (stats, columns, columnWidth) {
-  return columns
-    .map((column) => getCell(stats, column, columnWidth))
-    .join(COLUMN_SEPARATOR)
-}
 
 // Retrieve a single cell in the table, with a specific stat
 const getCell = function (stats, column, columnWidth) {
