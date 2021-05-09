6f033ebb7d8443b77ed07cf58d0517d58bca7d7b
ehmicky <ehmicky@gmail.com>
2021-05-09 11:23:14 +0200

    Refactoring

--- src/logs/normalize.js
+++ src/logs/normalize.js
@@ -1,5 +1,7 @@
 import stripFinalNewline from 'strip-final-newline'
 
+import { findIndexReverse } from '../utils/find.js'
+
 // Normalize task logs
 export const normalizeLogs = function (taskLogs, truncated) {
   const taskLogsA = stripFinalNewline(taskLogs.trim())
@@ -24,21 +26,16 @@ const stripPartialLine = function (taskLogs) {
 // Only keep the lines starting from the first non-duplicate lines.
 // Duplicate lines are very likely since the task is repeated.
 const removeDuplicateLines = function (taskLogs) {
-  // eslint-disable-next-line fp/no-mutating-methods
-  const lines = taskLogs.split('\n').reverse()
-  const duplicateIndex = lines.findIndex(isDuplicateLine)
+  const lines = taskLogs.split('\n')
+  const duplicateIndex = findIndexReverse(lines, isDuplicateLine)
 
   if (duplicateIndex === -1) {
     return taskLogs
   }
 
-  // eslint-disable-next-line fp/no-mutating-methods
-  return lines
-    .slice(0, duplicateIndex + 1)
-    .reverse()
-    .join('\n')
+  return lines.slice(duplicateIndex).join('\n')
 }
 
 const isDuplicateLine = function (line, index, lines) {
-  return lines.some((lineA, indexA) => indexA > index && lineA === line)
+  return lines.some((lineA, indexA) => indexA < index && lineA === line)
 }
