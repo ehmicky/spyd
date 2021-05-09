7336cdb28b5617422368af00b5da13f39e40a180
ehmicky <ehmicky@gmail.com>
2021-05-09 21:28:28 +0200

    Remove comment

--- src/normalize/group.js
+++ src/normalize/group.js
@@ -2,11 +2,6 @@ import { groupCategoryInfos } from '../combination/group.js'
 import { sortCombinations } from '../combination/sort.js'
 
 // Add `result.*` properties based on grouping combinations by category.
-// This is done twice:
-//  - When loading results, on each of them so this is available in
-//    `report.previous`
-//  - When merging the final result, after merged combinations have been added
-//    to it
 export const groupCombinations = function (results) {
   return results.map(groupResultCombinations)
 }
