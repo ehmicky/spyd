4f1bfec5bc7a57ceeaac1b557b28c1a4a5567932
ehmicky <ehmicky@gmail.com>
2021-05-09 19:32:51 +0200

    Add comment

--- src/sample/max_measures.js
+++ src/sample/max_measures.js
@@ -1,12 +1,15 @@
 // We end running samples when the `measures` is over `MAX_MEASURES`.
 // This is meant to prevent memory overflow.
+// We also adjust `maxLoops` to make sure runners never send too many measures
+// in a single sample.
 // We also make sure `measures` is garbage-collectable between each combination.
 // We could use a more elaborate logic to allow for the benchmark to continue
 // even after reaching that threshold:
 //  - For example, we could persist measures on the filesysystem, or aggregate
 //    them in-memory
 //  - However, needing more measures is unlikely to be needed:
-//     - With a single step and max `precision`, the number of loops is:
+//     - With a single step and max `precision`, the number of loops is still
+//       lower than `MAX_MEASURES`:
 //        - 4e4 with `rstdev` 10%
 //        - 4e6 with `rstdev` 100%
 //  - This assumption could be wrong in the following unlikely cases:
@@ -17,7 +20,17 @@ export const hasMaxMeasures = function ({ measures }) {
   return measures.length >= MAX_MEASURES
 }
 
-// The default limit for V8 in Node.js is 1.7GB, which allows measures to hold a
-// little more than 1e8 floats. We use a smaller number since this is not the
-// only variable taking memory.
+// The two limits we are avoiding are:
+//  - Default memory limit in V8: 1.7GB
+//     - This allows a little more than 1e8 double floats
+//     - We use a smaller number since this is not the only variable taking
+//       memory.
+//  - Max string length in V8: ~5e8
+//     - Since JSON serialized doubles can be up to 25 bytes (including the
+//       comma), this means runners sending a JSON payload of more than 2e7
+//       double floats would crash or make the parent process crash
+//        - This could be avoided by streaming the HTTP response but this would
+//          be slower
+//        - Note: doubles are often much smaller than 25 bytes when JSON
+//          serialized
 export const MAX_MEASURES = 1e7
