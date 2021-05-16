b442522ec4549ed13ecaaac9e2ec9c77b0ba4589
ehmicky <ehmicky@gmail.com>
2021-05-16 18:03:29 +0200

    Add comments

--- src/normalize/since.js
+++ src/normalize/since.js
@@ -7,6 +7,11 @@ import { mergeResults } from './merge.js'
 //  - Limit the number of results shown in `result.history` which is used with
 //    time series reporters.
 //  - Specify which previous reports should be merged to the main result
+// `since` is used to target both the first result in time series and the one
+// used for `diff` because:
+//  - If is simpler to understand
+//  - The `diff` must be earlier than any combination in `result.history`,
+//    i.e. it makes sense to re-use `since`, and it simplifies the configuration
 // `since` is relative to the main result:
 //  - For `bench`, this is the result being created.
 //  - For `show|remove`, this is the result being reported.
@@ -16,12 +21,28 @@ import { mergeResults } from './merge.js'
 //       bases (the main result and the "since" result)
 // Previous results are filtered by `select`
 //  - This purposely impacts the resolution of `since`.
-// `since` defaults to 0, i.e. must be opted in.
-// `result.history` includes the current result:
+// When `since` does not target any valid result:
+//  - We use the most recent result as `sinceResult` (if any)
+//  - However, we do not merge it
+//  - This allows giving a default behavior with `since: 0` which:
+//     - Makes merging opt-in. This is good since merging results can be
+//       unexpected, especially with `bench` command
+//     - While still showing the `diff` with the last result, which is the most
+//       likely wanted ones
+// The first item in `result.history` is the `sinceResult`, i.e. the result
+// targeted by `since`:
+//  - It is shown first in time series
+//     - This ensure each combination shows where it started
+//     - This allows users to visualize the `diff` by comparing the first and
+//       last item
+// The last item in `result.history` is the current result:
 //  - For `show|remove`, this is the result targeted by delta
 //  - For `bench`, this is the currently measured result
 //  - This allows time series reporters to use `result.history`
 //     - Since those should report the current result before merging
+//     - This ensure each combination shows where it was last measured
+//     - This ensures the time series reflects other reporters when used
+//       together
 // We do not expose some `combination.history` property
 //  - This would complicate the data model by creating copies of the same
 //    properties.
