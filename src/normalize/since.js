75950eb9627e360273675a2159c848ff7055f3d8
ehmicky <ehmicky@gmail.com>
2021-05-16 15:52:31 +0200

    Move file

--- src/normalize/since.js
+++ src/normalize/since.js
@@ -1,11 +1,7 @@
-import {
-  getNewIdInfos,
-  getIdInfos,
-  isSameIdInfos,
-  isSameCategory,
-} from '../combination/ids.js'
+import { isSameCategory } from '../combination/ids.js'
 import { findByDelta } from '../delta/main.js'
-import { mergeSystems } from '../system/merge.js'
+
+import { mergeResults } from './merge.js'
 
 // The `since` configuration property is used to:
 //  - Limit the number of results shown in `result.history` which is used with
@@ -67,84 +63,13 @@ const applyRegularSince = function (result, previous, sinceIndex) {
 
 const removeCombinations = function (result, resultA) {
   const combinations = result.combinations.filter((combination) =>
-    matchesCombinations(combination, resultA),
+    resultHasCombination(resultA, combination),
   )
   return { ...result, combinations }
 }
 
-const matchesCombinations = function (combination, result) {
-  return result.combinations.some((combinationA) =>
-    isSameCategory(combination, combinationA),
+const resultHasCombination = function ({ combinations }, combinationA) {
+  return combinations.some((combinationB) =>
+    isSameCategory(combinationA, combinationB),
   )
-}
-
-// `show|remove` commands allow reporting several reports at once using the
-// `since` configuration property:
-//  - This reports the most recent combination of each sets of categories
-//  - This only uses the results up to the result targeted by `since`
-// We purposely do not apply this with the `bench` command
-//  - Otherwise, users might be confused to see previous combinations being
-//    reported even though they are not being measured
-//  - However, users can perform several results and select combinations with
-//    `select|tasks|runner|inputs|system` then report an aggregation of them
-//    with the `show` command
-//     - This allows incremental benchmarks
-// We explicitly avoid trying to guess the current set of categories beyond
-// the current filtering properties because it is difficult.
-//  - Instead, we just rely on the `since` delta
-//  - All combination categories can be changed dynamically with
-//    configuration properties. We cannot whether missing combinations
-//    were temporarily or permanently removed.
-//  - This is especially true for systems. There is always only one system
-//    per result. It is hard to know where/whether in the results history the
-//    user intends to stop using each of the previously used systems.
-const mergeResults = function (result, previous) {
-  const previousCombinations = getPreviousCombinations(result, previous)
-  return previousCombinations.reduce(mergePreviousCombination, result)
-}
-
-// Retrieve previous combinations the result should be merged with
-const getPreviousCombinations = function (result, previous) {
-  // eslint-disable-next-line fp/no-mutating-methods
-  const previousA = [...previous].reverse()
-  const previousCombinations = previousA.flatMap(getCombinations)
-  const newIdInfos = getNewIdInfos(result, previousA)
-  return newIdInfos.map((idInfos) =>
-    getPreviousCombination(previousCombinations, idInfos),
-  )
-}
-
-const getCombinations = function (result) {
-  return result.combinations.map((combination) => ({
-    combination,
-    idInfos: getIdInfos(combination),
-    result,
-  }))
-}
-
-const getPreviousCombination = function (previousCombinations, idInfos) {
-  return previousCombinations.find((previousCombination) =>
-    isSameIdInfos(previousCombination.idInfos, idInfos),
-  )
-}
-
-// For each possible combination, if the last result already has it, we keep it.
-// Otherwise, we copy the most recent one.
-// When copying previous combinations, we do not remove them from the previous
-// result. This means they are present both in the merged result and in
-// `result.history`. This is intentional as it allows reporters to clearly
-// display both the merged result and the time each combination was
-// actually measured.
-// When merging two results, we keep most of the properties of the latest
-// result. However, we still merge `system` so several systems are reported.
-// This allows comparing different systems.
-// We make sure the latest `combinations` are first in the merged array, so we
-// can prioritize them.
-const mergePreviousCombination = function (
-  result,
-  { combination, result: { systems } },
-) {
-  const combinationsA = [...result.combinations, combination]
-  const systemsA = mergeSystems(result.systems, systems)
-  return { ...result, combinations: combinationsA, systems: systemsA }
 }
