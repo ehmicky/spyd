27c825b9c47baf89ca553ccb68a3dd222346a09e
ehmicky <ehmicky@gmail.com>
2021-05-16 19:36:04 +0200

    Fix CLI description

--- src/bin/config/history.js
+++ src/bin/config/history.js
@@ -4,17 +4,21 @@ import { HISTORY } from './groups.js'
 export const HISTORY_CONFIG = {
   since: {
     group: HISTORY,
-    describe: `Which result to compare to when using the "limit" or "showDiff"
-configuration properties.
-For reporters showing all previous results, this also defines the first result.
+    describe: `Specifies which result to compare to when using the "showDiff" or
+"limit" configuration properties.
+
+That result and all later ones will be shown in the report.
+This allows reporting several previous results at once.
+
 Can be:
+  - 0 (default): compare with last result but do not report any previous results
+  - 1: last result
   - an integer: {integer}-th last result
   - "first": first result
   - a date|time: like "yyyy-mm-dd" or "yyyy-mm-dd hh:mm:ss"
   - a duration: like "1m", "5d", "1 month" or "1y"
   - a result id
   - a git commit, tag or branch
-  - "ci": last CI build
-Default: the last CI build when in CI, or the last result otherwise.`,
+  - "ci": last CI build`,
   },
 }
