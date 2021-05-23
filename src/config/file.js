3bf0edf3597220a78f5a72e403bfabcc7da0c091
ehmicky <ehmicky@gmail.com>
2021-05-23 13:51:39 +0200

    Merge file

--- src/config/file.js
+++ src/config/file.js
@@ -5,7 +5,6 @@ import isPlainObj from 'is-plain-obj'
 import { UserError } from '../error/main.js'
 
 import { loadConfigContents } from './contents.js'
-import { normalizeConfigProp } from './normalize_prop.js'
 import { resolveConfigPath } from './resolve.js'
 
 // Load `spyd.*` and any child configuration files.
@@ -27,6 +26,28 @@ const getConfigsInfos = async function (config, base) {
     configs.map((configA) => getConfigInfos(configA, base)),
   )
   return configInfos.flat()
+}
+
+// The `config` property can optionally be an array.
+// The "default" resolver:
+//  - Is the default of the top-level `config` CLI flags but not inside
+//    configuration files
+//  - Can be specified explicitely by users. This can be useful when overridding
+//    a `config` property inherited from a child configuration.
+const normalizeConfigProp = function (config) {
+  const configs = Array.isArray(config) ? config : [config]
+  configs.forEach((configA) => {
+    validateConfigString(configA)
+  })
+  return configs
+}
+
+const validateConfigString = function (value) {
+  if (typeof value !== 'string' || value.trim() === '') {
+    throw new UserError(
+      `The "config" property must be a non-empty string: ${value}`,
+    )
+  }
 }
 
 const getConfigInfos = async function (config, base) {
