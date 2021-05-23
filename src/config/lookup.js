e4755478057f6f2099d7f11d75b32f3ba8944b32
ehmicky <ehmicky@gmail.com>
2021-05-23 15:50:18 +0200

    Improve default lookup

--- src/config/lookup.js
+++ src/config/lookup.js
@@ -1,6 +1,7 @@
-import { basename, dirname } from 'path'
+import { promises as fs } from 'fs'
 
 import findUp from 'find-up'
+import pathExists from 'path-exists'
 
 import { CONFIG_PLUGIN_TYPE } from '../plugin/types.js'
 
@@ -14,31 +15,48 @@ import { CONFIG_EXTENSIONS } from './contents.js'
 //  - Any other directory: for on-the-fly benchmarking, or for
 //    global/shared configuration.
 export const resolveLookup = async function (base) {
-  return await findUp(DEFAULT_CONFIG, { cwd: base })
+  const lookupFiles = await getLookupFiles(base)
+  return await findUp(lookupFiles, { cwd: base })
 }
 
-const getMainDefaultPath = function (filePath, extName) {
-  return `${filePath}${extName}`
+const getLookupFiles = async function (base) {
+  const configPackageDir = await getConfigPackageDir(base)
+  const lookupDirs =
+    configPackageDir === undefined
+      ? DEFAULT_CONFIG
+      : [...DEFAULT_CONFIG, `${configPackageDir}/spyd`]
+  return lookupDirs.flatMap(getLookupFilesByExt)
 }
 
-// We look inside monorepo `packages/spyd-config-*/spyd.*` by default
-const getPackagesDefaultPath = function (extName, dir) {
-  return isConfigPackage(dir) ? `spyd${extName}` : undefined
+// We look inside monorepo `packages/spyd-config-*/spyd.*`
+const getConfigPackageDir = async function (base) {
+  return await findUp(testConfigPackageDir, { cwd: base, type: 'directory' })
 }
 
-const isConfigPackage = function (dir) {
-  return (
-    basename(dir).startsWith(CONFIG_PLUGIN_TYPE.modulePrefix) &&
-    dirname(dir) === 'packages'
-  )
+const testConfigPackageDir = async function (dir) {
+  const packagesDir = `${dir}/packages`
+
+  if (!(await pathExists(packagesDir))) {
+    return
+  }
+
+  const filenames = await fs.readdir(packagesDir)
+  const filename = filenames.find(isConfigPackageDir)
+
+  if (filename === undefined) {
+    return
+  }
+
+  return `${packagesDir}/${filename}`
 }
 
-const DEFAULT_CONFIG = [
-  ...CONFIG_EXTENSIONS.map(getMainDefaultPath.bind(undefined, './spyd')),
-  ...CONFIG_EXTENSIONS.map(
-    getMainDefaultPath.bind(undefined, './benchmark/spyd'),
-  ),
-  ...CONFIG_EXTENSIONS.map((extName) =>
-    getPackagesDefaultPath.bind(undefined, extName),
-  ),
-]
+const isConfigPackageDir = function (filename) {
+  return filename.startsWith(CONFIG_PLUGIN_TYPE.modulePrefix)
+}
+
+// Order matters here
+const DEFAULT_CONFIG = ['./spyd', './benchmark/spyd']
+
+const getLookupFilesByExt = function (filePath) {
+  return CONFIG_EXTENSIONS.map((extName) => `${filePath}${extName}`)
+}
