d064c59660983f3420f6df838c896f496be96f56
ehmicky <ehmicky@gmail.com>
2021-05-23 23:57:46 +0200

    Add comments

--- src/report/utils/name.js
+++ src/report/utils/name.js
@@ -1,10 +1,12 @@
 import { titleColor } from './colors.js'
 import { getCombinationName } from './title.js'
 
+// Retrieve the width of the combination name's column
 export const getCombinationNameWidth = function (combination) {
   return getCombinationNamePadded(combination).length
 }
 
+// Retrieve the prettified combination name to used as a first column
 export const getCombinationNameColor = function (combination) {
   return titleColor(getCombinationNamePadded(combination))
 }
