00e9f4c8957474b370c79ddad87fc97c8ab8e72d
ehmicky <ehmicky@gmail.com>
2021-06-14 13:52:08 +0200

    Fix self reference in tests

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-
-import { bench } from '../src/main.js'
+// eslint-disable-next-line import/no-unresolved, node/no-missing-import
+import { bench } from 'spyd'
 
 test('Smoke test', (t) => {
   t.is(typeof bench, 'function')
