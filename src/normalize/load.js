36f81aa2caaa5ba32b6262c7c63b2c408652f222
ehmicky <ehmicky@gmail.com>
2021-08-15 15:07:11 +0200

    Remove CI sort

--- src/normalize/load.js
+++ src/normalize/load.js
@@ -1,8 +1,9 @@
+import sortOn from 'sort-on'
+
 import { selectResults } from '../select/main.js'
 
 import { decompressResult } from './compress.js'
 import { migrateResults } from './migrate.js'
-import { sortResults } from './sort.js'
 
 // Normalize results on load
 export const loadResults = function (results, select) {
@@ -11,4 +12,8 @@ export const loadResults = function (results, select) {
   const resultsC = resultsB.map(decompressResult)
   const resultsD = sortResults(resultsC)
   return resultsD
+}
+
+const sortResults = function (results) {
+  return sortOn(results, 'timestamp')
 }
