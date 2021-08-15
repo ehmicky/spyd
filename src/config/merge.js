a6fab984f91af8b367a4ad02063eae5429cfbcab
ehmicky <ehmicky@gmail.com>
2021-08-15 17:52:19 +0200

    Move code

--- src/config/merge.js
+++ src/config/merge.js
@@ -1,8 +1,7 @@
 import isPlainObj from 'is-plain-obj'
 
-import { PLUGIN_TYPES } from '../plugin/types.js'
+import { isTasks, mergeTasks } from '../run/merge.js'
 
-import { normalizeOptionalArray } from './check.js'
 import { removeEmptyValues } from './empty.js'
 
 // Merge two configuration objects. Used to merge:
@@ -40,32 +39,6 @@ const mergeValues = function (valueA, valueB, keys) {
   }
 
   return valueB
-}
-
-// `tasks` or `runnnerConfig.{runnerId}.tasks` are concatenated, not overridden
-// so that shared configurations consumers can add tasks
-const isTasks = function (keys) {
-  return isTopTasks(keys) || isRunnerTasks(keys)
-}
-
-const isTopTasks = function (keys) {
-  return keys.length === 1 && keys[0] === 'tasks'
-}
-
-const isRunnerTasks = function (keys) {
-  return (
-    keys.length === 3 &&
-    keys[0] === PLUGIN_TYPES.runner.configProp &&
-    keys[2] === 'tasks'
-  )
-}
-
-// Order matters since later `tasks` have priority when merging two task files
-// with same task ids.
-const mergeTasks = function (tasksA, tasksB) {
-  const tasksArrA = normalizeOptionalArray(tasksA)
-  const tasksArrB = normalizeOptionalArray(tasksB)
-  return [...tasksArrA, ...tasksArrB]
 }
 
 const CUSTOM_MERGES = [{ condition: isTasks, applyFunc: mergeTasks }]
---
+++ src/run/merge.js
@@ -0,0 +1,29 @@
+import { normalizeOptionalArray } from '../config/check.js'
+import { PLUGIN_TYPES } from '../plugin/types.js'
+
+// Custom merging logic for tasks.
+// `tasks` or `runnnerConfig.{runnerId}.tasks` are concatenated, not overridden
+// so that shared configurations consumers can add tasks
+export const isTasks = function (keys) {
+  return isTopTasks(keys) || isRunnerTasks(keys)
+}
+
+const isTopTasks = function (keys) {
+  return keys.length === 1 && keys[0] === 'tasks'
+}
+
+const isRunnerTasks = function (keys) {
+  return (
+    keys.length === 3 &&
+    keys[0] === PLUGIN_TYPES.runner.configProp &&
+    keys[2] === 'tasks'
+  )
+}
+
+// Order matters since later `tasks` have priority when merging two task files
+// with same task ids.
+export const mergeTasks = function (tasksA, tasksB) {
+  const tasksArrA = normalizeOptionalArray(tasksA)
+  const tasksArrB = normalizeOptionalArray(tasksB)
+  return [...tasksArrA, ...tasksArrB]
+}
