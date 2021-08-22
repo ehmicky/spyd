b3243dac03502a22864cefd41636d7db96f90597
ehmicky <ehmicky@gmail.com>
2021-08-22 15:47:20 +0200

    Move file

--- src/report/reporters/debug/main.js
+++ src/report/reporters/debug/main.js
@@ -3,9 +3,7 @@ import { joinSections } from '../../utils/join.js'
 import { prettifyStats } from '../../utils/stats/main.js'
 import { addTitles } from '../../utils/title.js'
 
-import { getColumns, getColumnWidth, getAllColumns } from './columns.js'
-import { getHeader } from './header.js'
-import { getRow } from './row.js'
+import { getTables } from './tables.js'
 import { getTimeSeries } from './time_series.js'
 
 // Debugging reporter only meant for development purpose
@@ -20,38 +18,6 @@ const reportTerminal = function ({ combinations, screenWidth, history }) {
     screenWidth,
   })
   return joinSections([...tables, ...timeSeries, ...histograms])
-}
-
-const getTables = function (combinations, screenWidth) {
-  const columns = getColumns(combinations)
-
-  // When no stats are available
-  if (columns.length === 0) {
-    return [getTable(combinations, [], 0)]
-  }
-
-  const columnWidth = getColumnWidth(combinations, columns)
-  const allColumns = getAllColumns({
-    combinations,
-    columns,
-    screenWidth,
-    columnWidth,
-  })
-  return allColumns.map((columnsA) =>
-    getTable(combinations, columnsA, columnWidth),
-  )
-}
-
-const getTable = function (combinations, columns, columnWidth) {
-  const header = getHeader(combinations, columns, columnWidth)
-  const rows = getRows(combinations, columns, columnWidth)
-  return `${header}\n${rows}`
-}
-
-const getRows = function (combinations, columns, columnWidth) {
-  return combinations
-    .map((combination) => getRow(combination, columns, columnWidth))
-    .join('\n')
 }
 
 export const debug = { reportTerminal, debugStats: true }
---
+++ src/report/reporters/debug/tables.js
@@ -0,0 +1,35 @@
+import { getColumns, getColumnWidth, getAllColumns } from './columns.js'
+import { getHeader } from './header.js'
+import { getRow } from './row.js'
+
+export const getTables = function (combinations, screenWidth) {
+  const columns = getColumns(combinations)
+
+  // When no stats are available
+  if (columns.length === 0) {
+    return [getTable(combinations, [], 0)]
+  }
+
+  const columnWidth = getColumnWidth(combinations, columns)
+  const allColumns = getAllColumns({
+    combinations,
+    columns,
+    screenWidth,
+    columnWidth,
+  })
+  return allColumns.map((columnsA) =>
+    getTable(combinations, columnsA, columnWidth),
+  )
+}
+
+const getTable = function (combinations, columns, columnWidth) {
+  const header = getHeader(combinations, columns, columnWidth)
+  const rows = getRows(combinations, columns, columnWidth)
+  return `${header}\n${rows}`
+}
+
+const getRows = function (combinations, columns, columnWidth) {
+  return combinations
+    .map((combination) => getRow(combination, columns, columnWidth))
+    .join('\n')
+}
