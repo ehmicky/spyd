bc088d2965f44190b188eacd5b07213085e085f6
ehmicky <ehmicky@gmail.com>
2021-08-22 17:31:07 +0200

    Refactoring

--- src/report/contents/call.js
+++ src/report/contents/call.js
@@ -7,43 +7,6 @@ import { getPaddedScreenWidth, getPaddedScreenHeight } from '../tty.js'
 import { omitResultProps } from './omit.js'
 import { addResultTitles } from './titles.js'
 
-// The `run`, `show` and `remove` commands return the result programmatically.
-// We return the same value that is passed to reporters since:
-//  - It has all the information users might need
-//  - This avoids documenting two separate objects
-// However, there are a few differences:
-//  - We apply specific some reporter configuration:
-//     - to make sure all properties are available
-//     - not to couple the programmatic result with a specific reporter
-//  - We do not apply properties:
-//     - Very specific to reporting such as `footer`
-//     - Mostly internals such as debug stats
-// We purposely avoid returning the reporter's `content` programmatically
-//  - The `content` is fit for reporting only, not programmatic usage
-//  - It can still be accessed by outputting it a specific file then read that
-//    file separately
-export const getProgrammaticResult = async function (result, titles) {
-  return await callReportFunc({
-    result,
-    titles,
-    reporter: PROGRAMMATIC_REPORTER,
-  })
-}
-
-const PROGRAMMATIC_REPORTER = {
-  format: 'external',
-  // eslint-disable-next-line no-empty-function
-  reportExternal() {},
-  debugStats: false,
-  config: {
-    showSystem: true,
-    showMetadata: true,
-    showTitles: true,
-    showPrecision: true,
-    showDiff: true,
-  },
-}
-
 // Call all `reporter.report()`.
 // It can be async, including during results preview.
 // Some of this is currently applied only to `result`, not `result.history[*]`
--- src/report/contents/main.js
+++ src/report/contents/main.js
@@ -1,20 +1,47 @@
-import { callReportFunc, getProgrammaticResult } from './call.js'
+import { callReportFunc } from './call.js'
 import { handleContent } from './handle.js'
 import { joinByOutput } from './join.js'
 
 // Retrieve reporter's contents by calling all `reporter.report()` then
 // normalizing their return value and grouping it by `output`.
 export const getContents = async function (result, { reporters, titles }) {
-  const { result: programmaticResult } = await getProgrammaticResult(
-    result,
-    titles,
-  )
-  const contents = await Promise.all(
-    reporters.map((reporter) => callReportFunc({ result, titles, reporter })),
+  const [{ result: programmaticResult }, ...contents] = await Promise.all(
+    [PROGRAMMATIC_REPORTER, ...reporters].map((reporter) =>
+      callReportFunc({ result, titles, reporter }),
+    ),
   )
   const contentsA = contents.filter(hasContent).map(handleContent)
   const contentsB = joinByOutput(contentsA)
   return { programmaticResult, contents: contentsB }
+}
+
+// The `run`, `show` and `remove` commands return the result programmatically.
+// We return the same value that is passed to reporters since:
+//  - It has all the information users might need
+//  - This avoids documenting two separate objects
+// However, there are a few differences:
+//  - We apply specific some reporter configuration:
+//     - to make sure all properties are available
+//     - not to couple the programmatic result with a specific reporter
+//  - We do not apply properties:
+//     - Very specific to reporting such as `footer`
+//     - Mostly internals such as debug stats
+// We purposely avoid returning the reporter's `content` programmatically
+//  - The `content` is fit for reporting only, not programmatic usage
+//  - It can still be accessed by outputting it a specific file then read that
+//    file separately
+const PROGRAMMATIC_REPORTER = {
+  format: 'external',
+  // eslint-disable-next-line no-empty-function
+  reportExternal() {},
+  debugStats: false,
+  config: {
+    showSystem: true,
+    showMetadata: true,
+    showTitles: true,
+    showPrecision: true,
+    showDiff: true,
+  },
 }
 
 // A reporter can choose not to return anything
