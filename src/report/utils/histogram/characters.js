ab5bba5eba09a58125ec9f982129de5d25dde0f2
ehmicky <ehmicky@gmail.com>
2021-08-29 17:56:24 +0200

    Improve terminal support

--- src/report/utils/histogram/characters.js
+++ src/report/utils/histogram/characters.js
@@ -1,27 +1,28 @@
-import { platform } from 'process'
+import isUnicodeSupported from 'is-unicode-supported'
+
+const canDisplayUnicode = isUnicodeSupported()
 
 // Characters displaying an increasing percentage visually
-export const HISTOGRAM_CHARS =
-  platform === 'win32'
-    ? [' ', '\u2584', '\u2588']
-    : [
-        ' ',
-        '\u2581',
-        '\u2582',
-        '\u2583',
-        '\u2584',
-        '\u2585',
-        '\u2586',
-        '\u2587',
-        '\u2588',
-      ]
+export const HISTOGRAM_CHARS = canDisplayUnicode
+  ? [
+      ' ',
+      '\u2581',
+      '\u2582',
+      '\u2583',
+      '\u2584',
+      '\u2585',
+      '\u2586',
+      '\u2587',
+      '\u2588',
+    ]
+  : [' ', '\u2584', '\u2588']
 // Characters displaying 0% and 100% visually
 export const [EMPTY_HISTOGRAM_CHAR] = HISTOGRAM_CHARS
 export const FULL_HISTOGRAM_CHAR = HISTOGRAM_CHARS[HISTOGRAM_CHARS.length - 1]
 
 // To compensate from the lack of characters on Windows, we increase the number
 // of characters shown per column
-export const EXTRA_HEIGHT = platform === 'win32' ? 3 : 1
+export const EXTRA_HEIGHT = canDisplayUnicode ? 1 : 3
 
 // Characters to display the horizontal separator, including its ticks
 // Works with all terminals
