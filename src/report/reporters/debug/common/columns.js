11a20c0c627e66ab028eb10974132fb065ced4a2
ehmicky <ehmicky@gmail.com>
2021-08-29 15:22:44 +0200

    Merge files

--- src/report/reporters/debug/common/columns.js
+++ src/report/reporters/debug/common/columns.js
@@ -2,8 +2,6 @@ import stringWidth from 'string-width'
 
 import { COLUMN_SEPARATOR } from '../../../utils/separator.js'
 
-import { getResponsiveColumns } from './responsive.js'
-
 export const getAllColumns = function (firstColumn, columns, screenWidth) {
   const columnsWidth = Math.max(...columns.map(getColumnWidth))
   const firstColumnWidth = Math.max(...firstColumn.map(stringWidth))
@@ -22,4 +20,38 @@ const getColumnWidth = function ({ cellStats, headerNames }) {
   const cellLengths = cellStats.map(stringWidth)
   const headerLengths = headerNames.map(stringWidth)
   return Math.max(...cellLengths, ...headerLengths)
+}
+
+// Fit several `columns` of same width (`columnsWidth`) responsively in a
+// terminal of `availableWidth` by putting them into several groups meant to be
+// printed on different lines.
+const getResponsiveColumns = function ({
+  availableWidth,
+  columnsWidth,
+  separatorWidth,
+  columns,
+}) {
+  const { allColumns } = columns.reduce(
+    addColumn.bind(undefined, { availableWidth, columnsWidth, separatorWidth }),
+    { allColumns: [], remainingWidth: 0 },
+  )
+  // eslint-disable-next-line fp/no-mutating-methods
+  return allColumns.reverse()
+}
+
+const addColumn = function (
+  { availableWidth, columnsWidth, separatorWidth },
+  { allColumns, allColumns: [columns, ...previousColumns], remainingWidth },
+  column,
+) {
+  const remainingWidthA = remainingWidth - columnsWidth - separatorWidth
+  return remainingWidthA >= 0
+    ? {
+        allColumns: [[...columns, column], ...previousColumns],
+        remainingWidth: remainingWidthA,
+      }
+    : {
+        allColumns: [[column], ...allColumns],
+        remainingWidth: availableWidth - columnsWidth,
+      }
 }
