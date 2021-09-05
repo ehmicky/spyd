06ea2a4b037efc32b594ece77c6de5f8e0447e74
ehmicky <ehmicky@gmail.com>
2021-09-05 13:23:18 +0200

    Join files

--- src/report/reporters/histogram/width.js
+++ src/report/reporters/histogram/width.js
@@ -1,6 +1,8 @@
+import stringWidth from 'string-width'
+
 import { getCombinationPaddedName } from '../../utils/name.js'
 
-import { getMinBlockWidth, getMaxBlockWidth } from './min_max.js'
+import { getPaddedMin, getPaddedMax } from './abscissa.js'
 
 // Retrieve the width of all blocks, in order:
 //  - Combination titles
@@ -17,3 +19,31 @@ export const getWidths = function (combinations, mini, screenWidth) {
   )
   return { titlesWidth, minBlockWidth, contentWidth }
 }
+
+// Retrieve the width of those blocks
+const getBlockWidth = function (getStat, combinations, mini) {
+  if (mini) {
+    return 0
+  }
+
+  return Math.max(
+    ...combinations.map((combination) =>
+      getCombinationWidth(combination, getStat),
+    ),
+  )
+}
+
+const getCombinationWidth = function ({ stats }, getStat) {
+  return stringWidth(getStat(stats))
+}
+
+const getMinStat = function ({ min }) {
+  return min === undefined ? '' : getPaddedMin(min)
+}
+
+const getMaxStat = function ({ max }) {
+  return max === undefined ? '' : getPaddedMax(max)
+}
+
+const getMinBlockWidth = getBlockWidth.bind(undefined, getMinStat)
+const getMaxBlockWidth = getBlockWidth.bind(undefined, getMaxStat)
