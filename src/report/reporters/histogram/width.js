b2162797e1556bfb420de954da1e953fc7dafe82
ehmicky <ehmicky@gmail.com>
2021-09-05 17:38:53 +0200

    Refactoring

--- src/report/reporters/histogram/width.js
+++ src/report/reporters/histogram/width.js
@@ -1,4 +1,4 @@
-import { getColWidths } from '../../utils/width.js'
+import { getCombinationPaddedName } from '../../utils/name.js'
 
 import { getPaddedStatLength } from './abscissa.js'
 
@@ -17,4 +17,25 @@ const getHistogramColWidth = function (statName, { stats }) {
   return stats[statName] === undefined
     ? 0
     : getPaddedStatLength(stats[statName])
+}
+
+const getColWidths = function ({
+  combinations,
+  minWidths,
+  maxWidths,
+  mini,
+  screenWidth,
+}) {
+  const titlesWidth = getCombinationPaddedName(combinations[0]).length
+  const minBlockWidth = getMinMaxBlockWidth(combinations, mini, minWidths)
+  const maxBlockWidth = getMinMaxBlockWidth(combinations, mini, maxWidths)
+  const contentWidth = Math.max(
+    screenWidth - titlesWidth - minBlockWidth - maxBlockWidth,
+    1,
+  )
+  return { titlesWidth, minBlockWidth, contentWidth }
+}
+
+const getMinMaxBlockWidth = function (combinations, mini, widths) {
+  return mini ? 0 : Math.max(...widths)
 }
