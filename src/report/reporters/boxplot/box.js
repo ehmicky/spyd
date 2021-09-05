98a4578ebdbe3aac8c349c6786586e1ad6b009c5
ehmicky <ehmicky@gmail.com>
2021-09-05 13:09:19 +0200

    Improve colors

--- src/report/reporters/boxplot/box.js
+++ src/report/reporters/boxplot/box.js
@@ -1,4 +1,4 @@
-import { goodColor } from '../../utils/colors.js'
+import { goodColor, separatorColor } from '../../utils/colors.js'
 
 // Retrieve the box plot
 export const getBox = function ({
@@ -30,7 +30,7 @@ const getMinLine = function (min, q1) {
   const minCharacter = min.index === q1.index ? '' : MIN_CHARACTER
   const leftLineWidth = Math.max(q1.index - min.index - minCharacter.length, 0)
   const leftLine = LINE_CHARACTER.repeat(leftLineWidth)
-  return `${minCharacter}${leftLine}`
+  return separatorColor(`${minCharacter}${leftLine}`)
 }
 
 const getQ1Box = function (q1, median) {
@@ -47,7 +47,7 @@ const getMaxLine = function (q3, max) {
   const maxCharacter = q3.index === max.index ? '' : MAX_CHARACTER
   const rightLineWidth = Math.max(max.index - q3.index - maxCharacter.length, 0)
   const rightLine = LINE_CHARACTER.repeat(rightLineWidth)
-  return `${rightLine}${maxCharacter}`
+  return separatorColor(`${rightLine}${maxCharacter}`)
 }
 
 // Works on most terminals
