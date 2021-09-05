260d0777daebbf9b3b8d2982d3ad204057f0efb7
ehmicky <ehmicky@gmail.com>
2021-09-05 15:54:51 +0200

    Simplify code

--- src/run/preview/update/actions.js
+++ src/run/preview/update/actions.js
@@ -3,13 +3,11 @@ import { goodColor, noteColor } from '../../../report/utils/colors.js'
 // Show keys available for user actions in previews.
 // When there are no actions available, we keep an empty line to avoid jitter.
 export const getActions = function (actions, leftWidth) {
-  const actionValues = Object.values(actions)
-
-  if (actionValues.length === 0) {
+  if (actions.length === 0) {
     return ''
   }
 
-  const actionsStr = actionValues.map(getAction).join(noteColor(', '))
+  const actionsStr = actions.map(getAction).join(noteColor(', '))
   return `${ACTIONS_LABEL.padEnd(leftWidth)}${actionsStr}`
 }
 
