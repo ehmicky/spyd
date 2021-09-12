4ba169806af8438841e66e21b0072c505d80ce96
ehmicky <ehmicky@gmail.com>
2021-09-12 17:29:36 +0200

    Move lines

--- src/combination/ids.js
+++ src/combination/ids.js
@@ -15,13 +15,6 @@ export const getCombinationsIds = function (combinations) {
   return combinations.flatMap(getCombinationIds).filter(isNotDuplicate)
 }
 
-export const getCombinationIds = function (combination) {
-  return DIMENSIONS.map(({ dimension, idName }) => ({
-    dimension,
-    id: combination[idName],
-  }))
-}
-
 // Remove duplicate ids with the same dimension, since this happens due to the
 // cartesian product.
 // Duplicate ids with a different dimension are validated later.
@@ -29,4 +22,11 @@ const isNotDuplicate = function ({ dimension, id }, index, idInfos) {
   return !idInfos
     .slice(index + 1)
     .some((idInfo) => idInfo.dimension === dimension && idInfo.id === id)
+}
+
+export const getCombinationIds = function (combination) {
+  return DIMENSIONS.map(({ dimension, idName }) => ({
+    dimension,
+    id: combination[idName],
+  }))
 }
