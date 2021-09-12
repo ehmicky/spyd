e0e3382c3e5a06f57eef8c056e961de02c434f11
ehmicky <ehmicky@gmail.com>
2021-09-12 19:27:42 +0200

    Add comment

--- src/combination/name.js
+++ src/combination/name.js
@@ -3,7 +3,11 @@ import { titleColor, noteColor } from '../report/utils/colors.js'
 import { getCombinationIds } from './ids.js'
 
 // Retrieve string with each combination's dimension id.
-// Used to name combinations in: `dev` and preview bottom bar.
+// This is in contrast with `combinationTitle`:
+//  - `combinationTitle` uses dimensions titles, which is more useful at
+//    report time
+//  - `combinationName` uses ids, which is more useful at `run`-time
+//    (`dev`, preview bottom bar) since users might use those on the next run
 export const getCombinationName = function (combination) {
   return getCombinationIds(combination)
     .map(getCombinationNamePart)
