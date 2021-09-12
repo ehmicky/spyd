defe2f5460315ae88494718f175052602ae39ee1
ehmicky <ehmicky@gmail.com>
2021-09-12 21:04:31 +0200

    Add listNoDimensions()

---
+++ src/combination/filter.js
@@ -0,0 +1,13 @@
+// Retrieve `noDimensions`, i.e. dimensions that have the same ids across all
+// combinations. Those are not reported, since they are redundant for users.
+export const listNoDimensions = function (dimensionsArray) {
+  const propNames = Object.keys(dimensionsArray[0])
+  return propNames.filter((propName) =>
+    isNoDimensions(dimensionsArray, propName),
+  )
+}
+
+const isNoDimensions = function (dimensionsArray, propName) {
+  const ids = dimensionsArray.map((dimensions) => dimensions[propName])
+  return [...new Set(ids)].length === 1
+}
