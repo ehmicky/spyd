77d8a19b476e0ca8aada56f72c26cef8aa1963d3
ehmicky <ehmicky@gmail.com>
2021-09-12 19:38:33 +0200

    Rename

--- src/combination/inputs.js
+++ src/combination/inputs.js
@@ -4,16 +4,16 @@
 // Values can be any JSON type. When using objects, variations must be used
 // to avoid ambiguity.
 // This can be converted from/to an object and an array shape.
-export const toInputsArr = function (inputsObj) {
-  return Object.entries(inputsObj).map(getInput)
+export const toInputsArr = function (inputs) {
+  return Object.entries(inputs).map(getInput)
 }
 
 const getInput = function ([inputId, inputValue]) {
   return { inputId, inputValue }
 }
 
-export const toInputsObj = function (inputs) {
-  return Object.assign({}, ...inputs.map(getInputPair))
+export const toInputsObj = function (inputsArr) {
+  return Object.assign({}, ...inputsArr.map(getInputPair))
 }
 
 const getInputPair = function ({ inputId, inputValue }) {
@@ -21,8 +21,8 @@ const getInputPair = function ({ inputId, inputValue }) {
 }
 
 // Retrieve all inputs identifiers
-export const getInputIds = function (inputs) {
-  return inputs.map(getInputId)
+export const getInputIds = function (inputsArr) {
+  return inputsArr.map(getInputId)
 }
 
 const getInputId = function ({ inputId }) {
