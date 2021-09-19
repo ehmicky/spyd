99f5e9bd31a6d25fcc660364a59db75864380ced
ehmicky <ehmicky@gmail.com>
2021-09-19 15:09:33 +0200

    Try improving outliers logic

--- src/stats/outliers_logic.js
+++ src/stats/outliers_logic.js
@@ -1,5 +1,4 @@
-import { getQuantiles, getSortedMedian } from './quantile.js'
-import { sortFloats } from './sort.js'
+import { getQuantiles } from './quantile.js'
 
 // Compute the best percentage of outliers to remove.
 // This tries to find the best percentage which:
@@ -14,24 +13,19 @@ import { sortFloats } from './sort.js'
 export const getOutliersPercentages = function (measures) {
   const length = OUTLIERS_GRANULARITY
   const quantiles = getQuantiles(measures, length)
+  const width = quantiles[length] - quantiles[0]
 
-  if (quantiles[0] === quantiles[length]) {
+  if (width === 0) {
     return { outliersMin: 0, outliersMax: 0 }
   }
 
-  const quantileWidths = Array.from({ length }, (_, index) =>
-    getQuantileWidth(index, quantiles),
-  )
-  const outlierWidth = getOutlierWidth(quantileWidths)
-  const outliersChecks = quantileWidths.map(
-    (quantileWidth) => quantileWidth > outlierWidth,
-  )
   const outliersMin = getOutliersPercentage(
     // eslint-disable-next-line fp/no-mutating-methods
-    [...outliersChecks].reverse(),
+    [...quantiles].reverse().map((quantile) => quantiles[length] - quantile),
     length,
+    width,
   )
-  const outliersMax = getOutliersPercentage(outliersChecks, length)
+  const outliersMax = getOutliersPercentage(quantiles, length, width)
   return { outliersMin, outliersMax }
 }
 
@@ -41,32 +35,34 @@ export const getOutliersPercentages = function (measures) {
 // an even number (due to its internal logic).
 // A higher value makes it slower to compute.
 // A lower value makes the value less accurate.
-const OUTLIERS_GRANULARITY = 2e3
+const OUTLIERS_GRANULARITY = 1e3
 
-const getQuantileWidth = function (index, quantiles) {
-  return quantiles[index + 1] - quantiles[index]
+// Return outliers percentage based on a specific outlier quantile
+const getOutliersPercentage = function (quantiles, length, width) {
+  const quantileRatios = Array.from({ length }, (_, index) =>
+    getQuantileRatio({ index, quantiles, length, width }),
+  )
+  const outliersIndex = getOutliersIndex(quantileRatios)
+  return outliersIndex === -1 ? 0 : (length - outliersIndex) / length
 }
 
-// Retrieve the average width of a quantile. We use a median to avoid big
-// outliers from increasing the average width, which would reduce the number
-// of outliers.
-const getOutlierWidth = function (quantileWidths) {
-  const quantileWidthsA = quantileWidths.filter(hasWidth)
-
-  if (quantileWidthsA.length === 0) {
-    return 0
-  }
-
-  sortFloats(quantileWidthsA)
-  const medianWidth = getSortedMedian(quantileWidthsA)
-  return medianWidth * MIN_OUTLIER_WIDTH
+const getQuantileRatio = function ({ index, quantiles, length, width }) {
+  const widthPercentage = (quantiles[length] - quantiles[index]) / width
+  const quantilePercentage = 1 - index / length
+  return widthPercentage / quantilePercentage
 }
 
-// If measures are integers, the distribution might have quantiles without any
-// widths. If there is a majority of them, the median width would be 0. This
-// would create too many outliers, so we remove it.
-const hasWidth = function (quantileWidth) {
-  return quantileWidth !== 0
+// Find the highest quantile index where a majority of the remaining quantiles
+// are outliers.
+// This method works well with:
+//  - Unsmoothed distributions where some outlier quantiles might be narrower
+//    due to statistical imprecision
+//  - Both fat and slim outliers tails
+//  - Removing small aggregate of outliers, while still keeping much bigger ones
+const getOutliersIndex = function (quantileRatios) {
+  return quantileRatios.findIndex(
+    (quantileRatio) => quantileRatio >= MIN_OUTLIER_WIDTH,
+  )
 }
 
 // Width threshold where a given quantile is considered an outlier.
@@ -78,39 +74,4 @@ const hasWidth = function (quantileWidth) {
 // A lower value is less accurate as more information is trimmed.
 // A higher value is less precise as outliers will have a higher impact on the
 // mean. It also results in poorer histograms.
-const MIN_OUTLIER_WIDTH = 40
-
-// Return outliers percentage based on a specific outlier quantile
-const getOutliersPercentage = function (outliersChecks, length) {
-  const outliersIndex = getOutliersIndex(outliersChecks, length)
-  return outliersIndex === undefined ? 0 : (length - outliersIndex) / length
-}
-
-// Find the highest quantile index where a majority of the remaining quantiles
-// are outliers.
-// This method works well with:
-//  - Unsmoothed distributions where some outlier quantiles might be narrower
-//    due to statistical imprecision
-//  - Both fat and slim outliers tails
-//  - Removing small aggregate of outliers, while still keeping much bigger ones
-const getOutliersIndex = function (outliersChecks, length) {
-  // eslint-disable-next-line fp/no-let
-  let totalOutliers = outliersChecks.filter(Boolean).length
-
-  // eslint-disable-next-line fp/no-loops, fp/no-mutation, fp/no-let
-  for (let index = 0; index < length; index += 1) {
-    const outlierLeft = length - index
-    const outlierRatio = totalOutliers / outlierLeft
-
-    // eslint-disable-next-line max-depth, no-magic-numbers
-    if (outlierRatio >= 0.5) {
-      return index
-    }
-
-    // eslint-disable-next-line max-depth
-    if (outliersChecks[index]) {
-      // eslint-disable-next-line fp/no-mutation
-      totalOutliers -= 1
-    }
-  }
-}
+const MIN_OUTLIER_WIDTH = 10
