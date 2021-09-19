83d2cead0ba97d9161b70384e502407915e8745d
ehmicky <ehmicky@gmail.com>
2021-09-19 23:14:24 +0200

    Start fixing reporters without combinationTitle

--- src/report/utils/combination_title.js
+++ src/report/utils/combination_title.js
@@ -3,24 +3,33 @@ import { TITLE_SEPARATOR, TITLE_SEPARATOR_COLORED } from './separator.js'
 
 // Retrieve a combination's title with padding and color
 export const getCombTitleColorPad = function (combination) {
-  return `${getCombinationTitleColor(combination)}${TITLE_SEPARATOR_COLORED}`
+  const combinationTitleColor = getCombinationTitleColor(combination)
+  return combinationTitleColor === ''
+    ? ''
+    : `${combinationTitleColor}${TITLE_SEPARATOR_COLORED}`
 }
 
 // Retrieve a combination's title with padding
 export const getCombinationTitlePad = function (combination) {
-  return `${getCombinationTitle(combination)}${TITLE_SEPARATOR}`
+  const combinationTitle = getCombinationTitle(combination)
+  return combinationTitle === '' ? '' : `${combinationTitle}${TITLE_SEPARATOR}`
 }
 
 // Retrieve the prettified combination title to used as a first column
 export const getCombinationTitleColor = function (combination) {
-  return titleColor(getCombinationTitle(combination))
+  const combinationTitle = getCombinationTitle(combination)
+  return combinationTitle === '' ? '' : titleColor(combinationTitle)
 }
 
+// `dimensions` might be empty, e.g. when there is a single combination in the
+// result. In that case, no `combinationTitle` should be shown.
 const getCombinationTitle = function ({ dimensions }) {
   const combinationTitle = Object.values(dimensions)
     .map(getTitlePadded)
     .join(COMBINATION_TITLE_MIDDLE)
-  return `${COMBINATION_TITLE_START}${combinationTitle}${COMBINATION_TITLE_END}`
+  return combinationTitle === ''
+    ? ''
+    : `${COMBINATION_TITLE_START}${combinationTitle}${COMBINATION_TITLE_END}`
 }
 
 const getTitlePadded = function ({ titlePadded }) {
