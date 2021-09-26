e54621b786b1963bc226e0162c564017d3445773
ehmicky <ehmicky@gmail.com>
2021-09-26 11:21:10 +0200

    Improve outliers

--- src/stats/outliers_logic.js
+++ src/stats/outliers_logic.js
@@ -81,17 +81,27 @@ const getOutliers = function (quantiles, reversedQuantiles, length) {
 // quantiles. Otherwise, when there is a bump on the outliers tail close the
 // exclusion threshold, the task might flicker between inclusion and exclusion,
 // creating big jumps for stdev, mean, min|max and histogram.
+// We only go through half of the quantiles:
+//  - This is because the closer widthPercentage is to 100%, the more imprecise
+//    it is.
+//  - This can lead to quantileRatio to be much higher than it should just
+//    because the measures close to `minIndex` happen to be close to each other.
 // eslint-disable-next-line max-statements, complexity
 const getNextOutliersIndex = function (quantiles, maxIndex, minIndex) {
   const max = quantiles[maxIndex]
   const min = quantiles[minIndex]
+  const width = max - min
 
-  if (max === min) {
+  if (width === 0) {
     return maxIndex
   }
 
+  const quantilesCount = minIndex - maxIndex
+  const startIndex = maxIndex + 1
+  const endIndex = maxIndex + Math.floor(quantilesCount / 2)
+
   // eslint-disable-next-line fp/no-loops, fp/no-let, fp/no-mutation
-  for (let index = maxIndex + 1; index < minIndex; index += 1) {
+  for (let index = startIndex; index < endIndex; index += 1) {
     const quantile = quantiles[index]
 
     // eslint-disable-next-line max-depth
@@ -99,8 +109,8 @@ const getNextOutliersIndex = function (quantiles, maxIndex, minIndex) {
       return maxIndex
     }
 
-    const widthPercentage = (max - quantile) / (max - min)
-    const quantilePercentage = (index - maxIndex) / (minIndex - maxIndex)
+    const widthPercentage = (max - quantile) / width
+    const quantilePercentage = (index - maxIndex) / quantilesCount
     const quantileRatio = getQuantileRatio(widthPercentage, quantilePercentage)
 
     // eslint-disable-next-line max-depth
