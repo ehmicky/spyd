2522fb6f3ef5d19583ef8051fb13008a4e3b4123
ehmicky <ehmicky@gmail.com>
2021-10-03 11:59:45 +0200

    Refactoring

--- src/combination/ids.js
+++ src/combination/ids.js
@@ -1,5 +1,24 @@
 import { getCombsDimensions, getCombDimensions } from './dimensions.js'
 
+// Check if two combinations have same identifiers for all dimensions
+export const hasSameCombinationIds = function (combinationA, combinationB) {
+  const combinationIdsA = getCombinationIds(combinationA)
+  const combinationIdsB = getCombinationIds(combinationB)
+  return combinationIdsA.every((combinationId) =>
+    combinationIdsB.includes(combinationId),
+  )
+}
+
+// Retrieve each dimension's id of a given combination
+// Follows `DIMENSIONS` array order.
+export const getCombinationIds = function (combination) {
+  return getDimensionIds(combination).map(getCombinationId)
+}
+
+const getCombinationId = function ({ id }) {
+  return id
+}
+
 // Retrieve all unique combinations identifiers.
 // For all combinations of a given result.
 // Follows `DIMENSIONS` array order.
@@ -35,22 +54,4 @@ export const getDimensionIds = function (combination) {
 const getDimensionId = function (combination, dimension) {
   const { id } = combination.dimensions[dimension.propName]
   return { id, dimension }
-}
-
-// Check if two combinations have same identifiers for all dimensions
-export const hasSameCombinationIds = function (combinationA, combinationB) {
-  const combinationIdsA = getCombinationIds(combinationA)
-  const combinationIdsB = getCombinationIds(combinationB)
-  return combinationIdsA.every((combinationId) =>
-    combinationIdsB.includes(combinationId),
-  )
-}
-
-// Retrieve each dimension's id of a given combination
-// Follows `DIMENSIONS` array order.
-export const getCombinationIds = function (combination) {
-  const dimensions = getCombDimensions(combination)
-  return dimensions.map(
-    (dimension) => combination.dimensions[dimension.propName].id,
-  )
 }
