70d157a79705124566e35d05f7b478086cd06863
ehmicky <ehmicky@gmail.com>
2021-10-31 13:54:48 +0100

    Add comments

--- src/stats/env_dev/try.js
+++ src/stats/env_dev/try.js
@@ -5,26 +5,29 @@ import { getMean } from '../sum.js'
 import { getEnvDev, CLUSTER_FACTOR } from './main.js'
 import { getSamples } from './samples.js'
 
-const sortNumbers = function (a, b) {
-  return a - b
+const sortNumbers = function (numA, numB) {
+  return numA - numB
 }
 
 const realStdevA = 10
 const realMeanA = 100
 const realStdevB = 40
 const realMeanB = 200
+// eslint-disable-next-line no-magic-numbers
 const realLength = 2 ** 12
 const samplesCount = 1e3
-const realMean = realMeanA * realMeanB
 
 const realPeriods = new Set([])
 const maxRealPeriodBase = Math.log(realLength) / Math.log(CLUSTER_FACTOR)
 const meanRatiosAccuracies = []
 const meanRatiosRstdevs = []
 
+// eslint-disable-next-line fp/no-loops
 for (
+  // eslint-disable-next-line fp/no-let
   let realPeriodBase = 0;
   realPeriodBase < maxRealPeriodBase;
+  // eslint-disable-next-line fp/no-mutation
   realPeriodBase += 1
 ) {
   const halfRealPeriod =
@@ -33,17 +36,19 @@ for (
     2
   const addedRealPeriods = [0, halfRealPeriod]
 
+  // eslint-disable-next-line fp/no-loops, max-depth
   for (const addedRealPeriod of addedRealPeriods) {
     const realPeriod = Math.round(
       CLUSTER_FACTOR ** realPeriodBase + addedRealPeriod,
     )
 
+    // eslint-disable-next-line max-depth
     if (realPeriods.has(realPeriod)) {
+      // eslint-disable-next-line no-continue
       continue
     }
 
     realPeriods.add(realPeriod)
-    // const realPeriod = 2 ** 6
 
     const meanRatiosProps = Array.from({ length: samplesCount }, () => {
       const samples = getSamples(
@@ -60,14 +65,18 @@ for (
       ({ meanStdevRatio }) => meanStdevRatio,
     )
     const allGroups = meanRatiosProps.map(({ groups }) =>
+      // eslint-disable-next-line max-nested-callbacks
       groups.map(({ varianceRatio }) => Math.sqrt(varianceRatio)),
     )
     const groupMedians = Array.from(
       { length: allGroups[0].length },
       (_, index) => {
+        // eslint-disable-next-line max-nested-callbacks
         const indexGroups = allGroups.map((groups) => groups[index])
         const groupMedian = getQuantile(
+          // eslint-disable-next-line fp/no-mutating-methods
           [...indexGroups].sort(sortNumbers),
+          // eslint-disable-next-line no-magic-numbers
           0.5,
           {},
         )
@@ -76,74 +85,34 @@ for (
     )
     const realMeanRatio = Math.max(...groupMedians)
     const meanRatiosMean = getMean(meanRatios)
-    const meanRatiosP01 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.001,
-      {},
-    )
-    const meanRatiosP1 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.01,
-      {},
-    )
-    const meanRatiosP5 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.05,
-      {},
-    )
     const meanRatiosMedian = getQuantile(
+      // eslint-disable-next-line fp/no-mutating-methods
       [...meanRatios].sort(sortNumbers),
+      // eslint-disable-next-line no-magic-numbers
       0.5,
       {},
-    )
-    const meanRatiosP95 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.95,
-      {},
-    )
-    const meanRatiosP99 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.99,
-      {},
-    )
-    const meanRatiosP999 = getQuantile(
-      [...meanRatios].sort(sortNumbers),
-      0.999,
-      {},
-    )
-    const meanRatiosStdev = Math.sqrt(
-      getVariance(meanRatios, { mean: meanRatiosMean }),
     )
     const meanRatiosRstdev =
       Math.sqrt(getVariance(meanRatios, { mean: meanRatiosMean })) /
       meanRatiosMedian
     const meanRatiosAccuracy = meanRatiosMedian / realMeanRatio
+    // eslint-disable-next-line fp/no-mutating-methods
     meanRatiosAccuracies.push(meanRatiosAccuracy)
+    // eslint-disable-next-line fp/no-mutating-methods
     meanRatiosRstdevs.push(meanRatiosRstdev)
-    //
-    // console.log([
-    // realPeriod,
-    // ...[
-    //     meanRatiosAccuracy,
-    //     meanRatiosRstdev,
-    //     meanRatiosP01,
-    //     meanRatiosP1,
-    //     meanRatiosP5,
-    //     meanRatiosP95,
-    //     meanRatiosP99,
-    //     meanRatiosP999,
-    //     meanRatiosStdev,
-    //     meanRatiosMean,
-    //     meanRatiosMedian,
-    //     realMeanRatio,
-    // ].map(float => float.toFixed(2))
-    // ].map(value => String(value).padStart(5)).join(' '))
-    //
   }
 }
 
+// eslint-disable-next-line no-restricted-globals, no-console
 console.log(`${[...realPeriods]
+  // eslint-disable-next-line no-magic-numbers
   .map((float) => String(float).padStart(5))
   .join(' ')}
-${meanRatiosAccuracies.map((float) => float.toFixed(2).padStart(5)).join(' ')}
-${meanRatiosRstdevs.map((float) => float.toFixed(2).padStart(5)).join(' ')}`)
+${
+  // eslint-disable-next-line no-magic-numbers
+  meanRatiosAccuracies.map((float) => float.toFixed(2).padStart(5)).join(' ')
+}
+${
+  // eslint-disable-next-line no-magic-numbers
+  meanRatiosRstdevs.map((float) => float.toFixed(2).padStart(5)).join(' ')
+}`)
