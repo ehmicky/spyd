9b3935bc6fe300410d0231d42c224781d785c600
ehmicky <ehmicky@gmail.com>
2021-12-19 23:57:24 +0100

    Add comments

--- src/stats/precision.js
+++ src/stats/precision.js
@@ -54,14 +54,18 @@ export const getPrecisionStats = function ({
 //  - That ratio follows a chi-squared distribution, just like `stdev` itself
 //     - Using a single tail
 //     - For example, for 97.5% of those 5% remaining runs, moe will be at most
-//       that many times higher for sample sizes 2, 3, 4 and 5: 31.9, 6.26,
-//       3.73, 2.87, 2.45
+//       that many times higher for sample sizes 2, 3, 4 and 5:
+//       31.9, 6.26, 3.73, 2.87 and 2.45
 //  - That ratio improves less and less as sample size increases, with most
 //    of the gain being when sample size goes from 2 to 5
-//  - With sample size 4, when moe was invalid, the difference ratio is 1.6x
-//    in average, 2.5x 90% of times, 5x 99% of times and 10x 99.9% of times
+//  - With sample size 4, when moe was invalid, the difference ratio is:
+//     - 1.6x in average
+//     - 2.5x 90% of times
+//     - 5x 99% of times
+//     - 10x 99.9% of times
 // A higher value:
 //  - Makes tasks with very low `rstdev` last longer that they should
+//     - This is especially a problem for slow tasks
 //  - Delays when `stdev`, `moe` and expected duration are first reported in
 //    previews
 // A lower value makes it more likely to use imprecise `stdev` and `moe` which
@@ -72,6 +76,7 @@ export const getPrecisionStats = function ({
 //     - Due to the preview's smoothing algorithm, imprecise stdev in the first
 //       previews have an impact on the next previews.
 //  - Reported
+//     - I.e. the first previews might be imprecise
 const MIN_STDEV_LOOPS = 4
 
 // We allow means to be 0 since some tasks might really return always the same
