c298be46d41d5df4088cb438f098bec7466a68e0
ehmicky <ehmicky@gmail.com>
2021-12-19 15:36:19 +0100

    Refactoring

--- src/combination/ids/get.js
+++ src/combination/ids/get.js
@@ -30,14 +30,18 @@ const getCombsDimensionIds = function (combinations, dimension) {
 // Remove duplicate ids with the same dimension, since this happens due to the
 // cartesian product.
 // Duplicate ids with a different dimension are validated later.
-const isNotDuplicateId = function ({ dimension, id }, index, dimensionsIds) {
+const isNotDuplicateId = function (dimensionsIdsA, index, dimensionsIds) {
   return !dimensionsIds
     .slice(0, index)
-    .some(
-      (dimensionsId) =>
-        dimensionsId.dimension.propName === dimension.propName &&
-        dimensionsId.id === id,
-    )
+    .some((dimensionsIdB) => isSameDimensionsId(dimensionsIdsA, dimensionsIdB))
+}
+
+// Check if two ids have same value and same dimension
+const isSameDimensionsId = function (dimensionsIdA, dimensionIdsB) {
+  return (
+    dimensionsIdA.id === dimensionIdsB.id &&
+    dimensionsIdA.dimension.propName === dimensionIdsB.dimension.propName
+  )
 }
 
 // Retrieve each dimension and its id for a given combination
