c2226bd62a5bbbb0247030f582ea8332e43a35ed
ehmicky <ehmicky@gmail.com>
2022-01-02 17:54:50 +0100

    Fix selection of show|remove

--- src/history/data/main.js
+++ src/history/data/main.js
@@ -2,6 +2,7 @@ import inquirer from 'inquirer'
 import sortOn from 'sort-on'
 
 import { isTtyInput } from '../../report/tty.js'
+import { validateMatches } from '../../select/validate.js'
 import { pickLast } from '../../utils/last.js'
 import { applyMainDelta, applySinceDelta } from '../delta/find.js'
 import { compressRawResult } from '../normalize/compress.js'
@@ -60,6 +61,7 @@ export const getFromHistory = async function (config) {
     historyA,
     config,
   )
+  validateMatches(rawResult, config)
   return { rawResult, history: historyB }
 }
 
