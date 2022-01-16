74627be74a1c1e3cfd15d4e88a696066aab969fe
ehmicky <ehmicky@gmail.com>
2022-01-16 21:38:48 +0100

    Add more logic to retrieve names

--- src/config/normalize/name.js
+++ src/config/normalize/name.js
@@ -1,3 +1,5 @@
+import isPlainObj from 'is-plain-obj'
+
 // Parse a configuration property path string into an array of tokens.
 // Dots are used for object properties, e.g. `one.two`
 // Brackets are used for array elements, e.g. `one[5]`
@@ -39,8 +41,8 @@ const normalizeProp = function ({
   const name = getName(propName, propIndex)
   const isArray = propIndex !== undefined
   const isAny = propNameAny !== undefined || propIndexAny !== undefined
-  const looseBool = loose !== undefined
-  return { name, isArray, isAny, loose: looseBool }
+  const isStrict = loose === undefined
+  return { name, isArray, isAny, isStrict }
 }
 
 const getName = function (propName, propIndex) {
@@ -49,4 +51,44 @@ const getName = function (propName, propIndex) {
   }
 
   return propIndex === '*' ? propIndex : Number(propIndex)
+}
+
+export const getByPropPath = function (object, propPath) {
+  return propPath.reduce(getValuesByProp, [object])
+}
+
+const getValuesByProp = function (values, propElem) {
+  return values.flatMap((value) => getValueByProp(value, propElem))
+}
+
+const getValueByProp = function (value, { name, isArray, isAny, isStrict }) {
+  if (isArray) {
+    if (!Array.isArray(value)) {
+      if (isStrict) {
+        return [value]
+      }
+
+      return []
+    }
+
+    if (isAny) {
+      return value
+    }
+
+    return [value[name]]
+  }
+
+  if (!isPlainObj(value)) {
+    if (isStrict) {
+      return [undefined]
+    }
+
+    return []
+  }
+
+  if (isAny) {
+    return Object.values(value)
+  }
+
+  return value[name]
 }
