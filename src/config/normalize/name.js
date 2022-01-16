5cb10ed616d4c4a7eb06a18a4fbc8b778cd0878d
ehmicky <ehmicky@gmail.com>
2022-01-16 21:36:57 +0100

    Improve name parsing

--- src/config/normalize/name.js
+++ src/config/normalize/name.js
@@ -1,20 +1,49 @@
-// TODO: validate when invalid `name`
-export const parsePropName = function (name) {
-  const propPath = [...`.${name}`.matchAll(PROP_NAME_REGEXP)].map(normalizeProp)
+// Parse a configuration property path string into an array of tokens.
+// Dots are used for object properties, e.g. `one.two`
+// Brackets are used for array elements, e.g. `one[5]`
+// Dots and brackets can be used deeply, e.g. `one.two[5]`
+// Wildcards are used with both objects and arrays to recurse over their
+// children, e.g. `one.*` or `one[*]`.
+// Unless question marks are appended to dots and brackets, the parent objects
+// or arrays will be validated as such and created if undefined.
+export const parsePropPath = function (propPathStr) {
+  const results = [...`.${propPathStr}`.matchAll(PROP_NAME_REGEXP)]
+  validatePropName(results, propPathStr)
+  const propPath = results.map(normalizeProp)
   return propPath
 }
 
 const PROP_NAME_REGEXP =
-  /(?<loose>\?)?((\.(?<propName>[^.?[\]]+))|(\[(?<propIndex>[\d*]+)\]))/guy
+  /(?<loose>\?)?((\.(?<propName>([^.?[\]*\d][^.?[\]*]*|(?<propNameAny>\*))))|(\[(?<propIndex>([\d]+|(?<propIndexAny>\*)))\]))/guy
 
-const normalizeProp = function ({ groups: { propName, propIndex, loose } }) {
-  const value = getPropValue(propName, propIndex)
-  const isArray = propIndex !== undefined
-  const looseBool = loose !== undefined
-  return { value, isArray, loose: looseBool }
+// TODO: add more error messages for common mistakes
+const validatePropName = function (results, propPathStr) {
+  const matchedPath = results.map(getMatch).join('').slice(1)
+
+  if (matchedPath === propPathStr) {
+    return
+  }
+
+  throw new Error(
+    `Syntax error in path "${propPathStr}" (starting at index ${matchedPath.length})`,
+  )
 }
 
-const getPropValue = function (propName, propIndex) {
+const getMatch = function ([match]) {
+  return match
+}
+
+const normalizeProp = function ({
+  groups: { propName, propNameAny, propIndex, propIndexAny, loose },
+}) {
+  const name = getName(propName, propIndex)
+  const isArray = propIndex !== undefined
+  const isAny = propNameAny !== undefined || propIndexAny !== undefined
+  const looseBool = loose !== undefined
+  return { name, isArray, isAny, loose: looseBool }
+}
+
+const getName = function (propName, propIndex) {
   if (propName !== undefined) {
     return propName
   }
