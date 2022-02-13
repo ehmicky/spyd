f761209013b4b3553f04706f76dc7ef94cb92c42
ehmicky <ehmicky@gmail.com>
2022-02-13 19:46:34 +0100

    Add default `example`

--- src/config/normalize/lib/definition.js
+++ src/config/normalize/lib/definition.js
@@ -1,17 +1,23 @@
-export const normalizeDefinition = function ({
-  name,
-  pick,
-  condition,
-  default: defaultValue,
-  compute,
-  path = false,
-  glob = false,
-  required = false,
-  example,
-  validate,
-  transform,
-  rename,
-}) {
+// Validate and normalize definitions
+export const normalizeDefinition = function (
+  {
+    name,
+    pick,
+    condition,
+    default: defaultValue,
+    compute,
+    path = false,
+    glob = false,
+    required = false,
+    example,
+    validate,
+    transform,
+    rename,
+  },
+  index,
+  definitions,
+) {
+  const exampleA = addDefaultExample(example, name, definitions)
   return {
     name,
     pick,
@@ -21,9 +27,23 @@ export const normalizeDefinition = function ({
     path,
     glob,
     required,
-    example,
+    example: exampleA,
     validate,
     transform,
     rename,
   }
+}
+
+// `definition.example` only needs to be defined once per `definition.name`,
+// defaulting to the first value from other definitions
+const addDefaultExample = function (example, name, definitions) {
+  if (example !== undefined) {
+    return example
+  }
+
+  const definitionA = definitions.find(
+    (definition) =>
+      definition.name === name && definition.example !== undefined,
+  )
+  return definitionA === undefined ? undefined : definitionA.example
 }
