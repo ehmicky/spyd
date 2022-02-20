520620c2c2449fafe5e673366cf6ad61bfd401ca
ehmicky <ehmicky@gmail.com>
2022-02-20 17:41:18 +0100

    Add comment

--- src/config/plugin/lib/location_info.js
+++ src/config/plugin/lib/location_info.js
@@ -7,6 +7,19 @@ import { isAbsolute } from 'path'
 //         (pluginConfig) => ({ plugin, ...pluginConfig })
 //       which is passed as argument to this library
 //  - A builtin identifier among `opts.builtins`
+//     - Each value is an async function returning the plugin object
+//     - In most cases, the function should just do a dynamic import to either
+//       a local file or a Node module name
+//        - This ensures plugins are lazy loaded
+//     - We require users to do that dynamic import themselves, instead of just
+//       supplying the file path or Node module name, because the `cwd` used
+//       during resolution should be:
+//        - From the library user's perspective, i.e. the file defining
+//          `opts.builtins` (which is done automatically by dynamic `import()`
+//          through implicit usage of `import.meta.file`)
+//        - Not from the library's perspective (current `import.meta.file` of
+//          this file)
+//        - Nor from the library's consumers (`opts.cwd`)
 //  - A file path starting with . or /
 //  - A Node module prefixed with `modulePrefix` (which is optional)
 export const getLocationInfo = function (
