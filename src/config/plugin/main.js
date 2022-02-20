941df074b4c5b59a2fda6142816c1c73079463b0
ehmicky <ehmicky@gmail.com>
2022-02-20 23:45:01 +0100

    Fix default values for plugins

--- src/config/plugin/main.js
+++ src/config/plugin/main.js
@@ -1,3 +1,5 @@
+import omit from 'omit.js'
+
 import { normalizeReporters } from '../../report/config/main.js'
 import { REPORTER_PLUGIN_TYPE } from '../../report/reporters/plugin/main.js'
 import { RUNNER_PLUGIN_TYPE } from '../../runners/plugin/main.js'
@@ -12,10 +14,6 @@ export const getPluginsConfigProps = function () {
   return PLUGIN_TYPES.map(getPluginTypeName)
 }
 
-const getPluginTypeName = function ({ name }) {
-  return name
-}
-
 // Handle the configuration plugins: runners and reporters
 export const normalizePluginsConfig = async function ({
   config,
@@ -23,54 +21,72 @@ export const normalizePluginsConfig = async function ({
   context,
   cwd,
 }) {
-  const configEntries = await Promise.all(
-    Object.entries(config).map(
-      normalizeConfigProp.bind(undefined, { command, config, context, cwd }),
+  const { includedPluginTypes, excludedPluginTypes } = splitPluginTypes(command)
+  const configA = excludePlugins(config, excludedPluginTypes)
+  const allPluginsEntries = await Promise.all(
+    includedPluginTypes.map((pluginType) =>
+      normalizePluginConfigs({ pluginType, config: configA, context, cwd }),
     ),
   )
-  const configA = Object.fromEntries(configEntries.filter(Boolean))
-  const configB = normalizeReporters(configA, command)
-  return configB
+  const allPlugins = Object.fromEntries(allPluginsEntries)
+  const configB = { ...configA, ...allPlugins }
+  const configC = normalizeReporters(configB, command)
+  return configC
 }
 
-const normalizeConfigProp = async function (
-  { command, config, context, cwd },
-  [propName, propValue],
-) {
-  const pluginType = PLUGIN_TYPES.find(({ name }) => name === propName)
+const splitPluginTypes = function (command) {
+  const includedPluginTypes = PLUGIN_TYPES.filter((pluginType) =>
+    shouldKeepPluginType(pluginType, command),
+  )
+  const excludedPluginTypes = PLUGIN_TYPES.filter(
+    (pluginType) => !shouldKeepPluginType(pluginType, command),
+  )
+  return { includedPluginTypes, excludedPluginTypes }
+}
 
-  if (pluginType === undefined) {
-    return [propName, propValue]
-  }
+const PLUGIN_TYPES = [RUNNER_PLUGIN_TYPE, REPORTER_PLUGIN_TYPE]
 
-  const { commands, ...pluginTypeA } = pluginType
+const shouldKeepPluginType = function (pluginType, command) {
+  return isAmongCommands(pluginType.commands, command)
+}
 
-  if (!isAmongCommands(commands, command)) {
-    return
-  }
+const excludePlugins = function (config, excludedPluginTypes) {
+  const omittedPlugins = excludedPluginTypes.map(getPluginTypeName)
+  return omit.default(config, omittedPlugins)
+}
 
+const getPluginTypeName = function ({ name }) {
+  return name
+}
+
+const normalizePluginConfigs = async function ({
+  pluginType,
+  pluginType: { name },
+  config,
+  context,
+  cwd,
+}) {
+  const pluginConfigs = config[name]
   const plugins = await addConfigPlugins({
-    propValue,
-    pluginType: pluginTypeA,
+    pluginConfigs,
+    pluginType,
     config,
     context,
     cwd,
   })
   const pluginsA = plugins.map(normalizePlugin)
-  return [propName, pluginsA]
+  return [name, pluginsA]
 }
 
-const PLUGIN_TYPES = [RUNNER_PLUGIN_TYPE, REPORTER_PLUGIN_TYPE]
-
 const addConfigPlugins = async function ({
-  propValue,
+  pluginConfigs,
   pluginType,
   config,
   context,
   cwd,
 }) {
   try {
-    return await addPlugins(propValue, pluginType, {
+    return await addPlugins(pluginConfigs, pluginType, {
       sharedConfig: config,
       context,
       cwd,
