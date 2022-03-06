e4cf42c0e9b63d83ba58591b4f99dae8b007a762
ehmicky <ehmicky@gmail.com>
2022-03-06 13:40:02 +0100

    Improve parent()

--- src/config/normalize/lib/star_dot_path/parsing/parent.js
+++ src/config/normalize/lib/star_dot_path/parsing/parent.js
@@ -1,6 +1,19 @@
-import { SEPARATOR } from './special.js'
+import { parse } from './parse.js'
 
 // Check if a query is a parent of another
-export const parent = function (parentQuery, childQuery) {
-  return childQuery.startsWith(`${parentQuery}${SEPARATOR}`)
+export const parent = function (parentQueryOrPath, childQueryOrPath) {
+  const parentPath = parse(parentQueryOrPath)
+  const childPath = parse(childQueryOrPath)
+  return (
+    childPath.length > parentPath.length &&
+    childPath.every(
+      (childToken, index) =>
+        index >= parentPath.length ||
+        isSameToken(childToken, parentPath[index]),
+    )
+  )
+}
+
+const isSameToken = function (tokenA, tokenB) {
+  return Object.is(tokenA, tokenB)
 }
