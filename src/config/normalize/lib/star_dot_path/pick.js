72e1e223d2c845ebc7f7b802c9e402be10227368
ehmicky <ehmicky@gmail.com>
2022-03-06 11:31:02 +0100

    Fix comment

--- src/config/normalize/lib/star_dot_path/pick.js
+++ src/config/normalize/lib/star_dot_path/pick.js
@@ -1,7 +1,7 @@
 import { list } from './get.js'
 import { set } from './set.js'
 
-// Inverse of `omit()`. Returns an object with only the properties being queried
+// Returns an object with only the properties being queried.
 export const pick = function (target, queryOrPath) {
   const entries = list(target, queryOrPath)
   return entries.reduce(includeEntry, {})
