c353683d4f1f1513b42dfc337e3789c4a05694d6
ehmicky <ehmicky@gmail.com>
2022-03-06 19:27:27 +0100

    Improve get()

--- src/config/normalize/lib/star_dot_path/get.js
+++ src/config/normalize/lib/star_dot_path/get.js
@@ -11,17 +11,13 @@ export const list = function (target, queryOrPath) {
 // Retrieve a single property's value in `target` matching a query string.
 // Wildcards can be used, but only the first value is returned.
 export const get = function (target, queryOrPath) {
-  const entries = listExistingEntries(target, queryOrPath)
-  return entries.length === 0 ? undefined : entries[0].value
+  const entry = listEntries(target, queryOrPath).find(isExisting)
+  return entry === undefined ? undefined : entry.value
 }
 
 // Check if a property is not missing according to a query
 export const has = function (target, queryOrPath) {
   return listEntries(target, queryOrPath).some(isExisting)
-}
-
-const listExistingEntries = function (target, queryOrPath) {
-  return listEntries(target, queryOrPath).filter(isExisting)
 }
 
 const isExisting = function ({ missing }) {
