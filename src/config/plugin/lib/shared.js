f72a71914a793b212e18620249c52ffedbf908c8
ehmicky <ehmicky@gmail.com>
2022-03-06 13:51:15 +0100

    Fix bug

--- src/config/plugin/lib/shared.js
+++ src/config/plugin/lib/shared.js
@@ -1,9 +1,9 @@
-import { list, parent, set } from '../../normalize/lib/star_dot_path/main.js'
+import { list, set } from '../../normalize/lib/star_dot_path/main.js'
 
 // Retrieve top-level properties that are shared with all plugins of a specific
 // type. Those are merged with plugin-specific properties.
 export const getSharedConfig = function (sharedConfig, item = []) {
-  const sharedPropNames = getSharedConfigPropNames(item)
+  const sharedPropNames = [...new Set(item.map(getRuleName))]
   const sharedConfigProps = sharedPropNames.flatMap((sharedPropName) =>
     list(sharedConfig, sharedPropName),
   )
@@ -11,21 +11,8 @@ export const getSharedConfig = function (sharedConfig, item = []) {
   return { sharedConfig: sharedConfigA, sharedPropNames }
 }
 
-// Retrieve all unique `name` of shared config properties, excluding their
-// children
-const getSharedConfigPropNames = function (item) {
-  const sharedPropNames = item.map(getRuleName)
-  return [...new Set(sharedPropNames)].filter(hasNoParent)
-}
-
 const getRuleName = function ({ name }) {
   return name
-}
-
-const hasNoParent = function (nameA, indexA, names) {
-  return names.every(
-    (nameB, indexB) => indexB === indexA || !parent(nameB, nameA),
-  )
 }
 
 const addSharedConfigProp = function (sharedConfig, { path, value }) {
