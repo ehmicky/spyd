1cd3e788d67683e45fd1550087664f18511f2492
ehmicky <ehmicky@gmail.com>
2022-03-06 17:27:56 +0100

    Refactoring

--- src/config/normalize/lib/star_dot_path/tokens/escape.js
+++ src/config/normalize/lib/star_dot_path/tokens/escape.js
@@ -18,7 +18,7 @@ const SPECIAL_CHARS = new Set([ESCAPE, SEPARATOR, ANY, MINUS, REGEXP_DELIM])
 
 // Escape special characters
 export const escapeSpecialChars = function (string) {
-  return string.replace(SPECIAL_CHARS_REGEXP, '\\$&')
+  return string.replace(SPECIAL_CHARS_REGEXP, `${ESCAPE}$&`)
 }
 
 const SPECIAL_CHARS_REGEXP = /[\\.*]|^[/-]/gu
