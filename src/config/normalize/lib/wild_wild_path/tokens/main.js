e83d5a498c33c1cc0465c8dd9f564569f7662954
ehmicky <ehmicky@gmail.com>
2022-03-06 19:51:54 +0100

    Simplify code

--- src/config/normalize/lib/wild_wild_path/tokens/main.js
+++ src/config/normalize/lib/wild_wild_path/tokens/main.js
@@ -1,4 +1,4 @@
-import { OTHER_TOKEN_TYPES, getOtherStringTokenType } from './other.js'
+import { OTHER_TOKEN_TYPES } from './other.js'
 import { PROP_TOKEN } from './prop.js'
 
 // Retrieve the type of a given token parsed object
@@ -6,15 +6,12 @@ export const getObjectTokenType = function (token) {
   return TOKEN_TYPES.find((tokenType) => tokenType.testObject(token))
 }
 
-// Order is significant as they are tested serially
-const TOKEN_TYPES = [...OTHER_TOKEN_TYPES, PROP_TOKEN]
-
 // Retrieve the type of a given token serialized string
 export const getStringTokenType = function (chars, isProp) {
-  if (isProp) {
-    return PROP_TOKEN
-  }
-
-  const tokenType = getOtherStringTokenType(chars)
-  return tokenType === undefined ? PROP_TOKEN : tokenType
+  return isProp
+    ? PROP_TOKEN
+    : TOKEN_TYPES.find((tokenType) => tokenType.testString(chars))
 }
+
+// Order is significant as they are tested serially
+const TOKEN_TYPES = [...OTHER_TOKEN_TYPES, PROP_TOKEN]
