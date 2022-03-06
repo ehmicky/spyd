ed73162514582648ec8363cb192615ae2b04004f
ehmicky <ehmicky@gmail.com>
2022-03-06 11:31:00 +0100

    Simplify code

--- src/config/normalize/lib/star_dot_path/pick.js
+++ src/config/normalize/lib/star_dot_path/pick.js
@@ -3,17 +3,8 @@ import { set } from './set.js'
 
 // Inverse of `omit()`. Returns an object with only the properties being queried
 export const pick = function (target, queryOrPath) {
-  return include(target, queryOrPath, returnTrue)
-}
-
-const returnTrue = function () {
-  return true
-}
-
-// Same but using a function returning the value to set
-const include = function (target, queryOrPath, condition) {
   const entries = list(target, queryOrPath)
-  return entries.filter(condition).reduce(includeEntry, {})
+  return entries.reduce(includeEntry, {})
 }
 
 const includeEntry = function (newTarget, { value, path }) {
