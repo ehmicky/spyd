112c890fa681c7d56b68b91a4f0ba80bcea338b2
ehmicky <ehmicky@gmail.com>
2022-03-06 17:36:27 +0100

    Add comments

--- src/config/normalize/lib/star_dot_path/tokens/main.js
+++ src/config/normalize/lib/star_dot_path/tokens/main.js
@@ -6,12 +6,12 @@ import { REGEXP_TOKEN } from './regexp.js'
 // Order is significant as they are tested serially
 const TOKEN_TYPES = [ANY_TOKEN, REGEXP_TOKEN, ARRAY_TOKEN, PROP_TOKEN]
 
-// Retrieve the type of a given token object
+// Retrieve the type of a given token parsed object
 export const getObjectTokenType = function (token) {
   return TOKEN_TYPES.find((tokenType) => tokenType.testObject(token))
 }
 
-// Retrieve the type of a given token string
+// Retrieve the type of a given token serialized string
 export const getStringTokenType = function (token) {
   return TOKEN_TYPES.find((tokenType) => tokenType.testString(token))
 }
