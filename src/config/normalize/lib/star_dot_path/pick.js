051cf220f2a5b4364bf842ac355db184203ef4c2
ehmicky <ehmicky@gmail.com>
2022-03-06 11:31:07 +0100

    Refactoring

--- src/config/normalize/lib/star_dot_path/pick.js
+++ src/config/normalize/lib/star_dot_path/pick.js
@@ -1,9 +1,11 @@
-import { list } from './get.js'
+import { listEntries } from './entries/main.js'
+import { maybeParse } from './parsing/parse.js'
 import { set } from './set.js'
 
 // Returns an object with only the properties being queried.
 export const pick = function (target, queryOrPath) {
-  const entries = list(target, queryOrPath)
+  const path = maybeParse(queryOrPath)
+  const entries = listEntries(target, path)
   return entries.reduce(includeEntry, {})
 }
 
