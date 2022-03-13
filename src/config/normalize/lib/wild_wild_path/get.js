f576342731a5b68ed56f852cdc1db8a8c188ee8b
ehmicky <ehmicky@gmail.com>
2022-03-13 11:53:16 +0100

    Fix get()

--- src/config/normalize/lib/wild_wild_path/get.js
+++ src/config/normalize/lib/wild_wild_path/get.js
@@ -10,8 +10,13 @@ export const list = function (target, queryOrPath) {
 // Retrieve a single property's value in `target` matching a query string.
 // Wildcards can be used, but only the first value is returned.
 export const get = function (target, queryOrPath) {
-  const entry = iterate(target, queryOrPath).find(isExisting)
-  return entry === undefined ? undefined : entry.value
+  // eslint-disable-next-line fp/no-loops
+  for (const { value, missing } of iterate(target, queryOrPath)) {
+    // eslint-disable-next-line max-depth
+    if (!missing) {
+      return value
+    }
+  }
 }
 
 // Check if a property is not missing according to a query
