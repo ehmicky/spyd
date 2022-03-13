60be65d155ad65153b7d92d08a7b39c674d502fc
ehmicky <ehmicky@gmail.com>
2022-03-13 16:02:33 +0100

    Fix linting

--- src/config/normalize/lib/wild_wild_path/iterate/main.js
+++ src/config/normalize/lib/wild_wild_path/iterate/main.js
@@ -15,6 +15,7 @@ export const iterate = function* (
   query,
   { childFirst = false, sort = false, classes = false } = {},
 ) {
+  const opts = { childFirst, sort, classes }
   const queryArrays = parseQuery(query)
   const entries = queryArrays.map((queryArray) => ({
     queryArray,
@@ -22,27 +23,20 @@ export const iterate = function* (
     path: [],
     missing: false,
   }))
-  yield* iterateLevel({ entries, childFirst, sort, classes, index: 0 })
+  yield* iterateLevel(entries, 0, opts)
 }
 
-const iterateLevel = function* ({ entries, childFirst, sort, classes, index }) {
+const iterateLevel = function* (entries, index, opts) {
   const entriesA = removeDuplicates(entries)
   const parentEntries = getParentEntries(entriesA, index)
 
-  if (!childFirst) {
+  if (!opts.childFirst) {
     yield* parentEntries
   }
 
-  yield* iterateChildEntries({
-    entries,
-    parentEntries,
-    childFirst,
-    sort,
-    classes,
-    index,
-  })
+  yield* iterateChildEntries({ entries, parentEntries, index, opts })
 
-  if (childFirst) {
+  if (opts.childFirst) {
     yield* parentEntries
   }
 }
@@ -61,10 +55,8 @@ const normalizeEntry = function ({ value, path, missing }) {
 const iterateChildEntries = function* ({
   entries,
   parentEntries,
-  childFirst,
-  sort,
-  classes,
   index,
+  opts,
 }) {
   if (parentEntries.length === entries.length) {
     return
@@ -72,45 +64,27 @@ const iterateChildEntries = function* ({
 
   const childEntries = entries
     .filter(({ queryArray }) => queryArray.length !== index)
-    .flatMap((entry) => expandToken(entry, index, classes))
+    .flatMap((entry) => expandToken(entry, index, opts.classes))
 
   if (childEntries.length === 0) {
     return
   }
 
-  yield* iterateChildren({ childEntries, childFirst, sort, classes, index })
+  yield* iterateChildren(childEntries, index, opts)
 }
 
-const iterateChildren = function* ({
-  childEntries,
-  childFirst,
-  sort,
-  classes,
-  index,
-}) {
+const iterateChildren = function* (childEntries, index, opts) {
   const nextIndex = index + 1
 
   if (childEntries.length === 1) {
-    yield* iterateLevel({
-      entries: childEntries,
-      childFirst,
-      sort,
-      classes,
-      index: nextIndex,
-    })
+    yield* iterateLevel(childEntries, nextIndex, opts)
     return
   }
 
-  const childEntriesGroups = groupSortChildEntries(childEntries, sort)
+  const childEntriesGroups = groupSortChildEntries(childEntries, opts.sort)
 
   // eslint-disable-next-line fp/no-loops
   for (const entries of childEntriesGroups) {
-    yield* iterateLevel({
-      entries,
-      childFirst,
-      sort,
-      classes,
-      index: nextIndex,
-    })
+    yield* iterateLevel(entries, nextIndex, opts)
   }
 }
