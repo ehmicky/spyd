8f4fdfe23eaa22fc3a8138db0ad58204216fe1d5
ehmicky <ehmicky@gmail.com>
2022-03-13 15:52:21 +0100

    Simplify code

--- src/config/normalize/lib/wild_wild_path_utils/include.js
+++ src/config/normalize/lib/wild_wild_path_utils/include.js
@@ -1,10 +1,4 @@
-import {
-  set,
-  get,
-  remove,
-  iterate,
-  isParentPath,
-} from '../wild_wild_path/main.js'
+import { set, remove, list, isParentPath } from '../wild_wild_path/main.js'
 
 // Returns an object with only the properties being queried.
 export const pick = function (target, query, { sort } = {}) {
@@ -14,10 +8,6 @@ export const pick = function (target, query, { sort } = {}) {
     query,
     sort,
   })
-}
-
-const pickEntry = function (target, { path, value }) {
-  return set(target, path, value)
 }
 
 const returnTrue = function () {
@@ -35,10 +25,13 @@ export const include = function (target, query, condition, { sort } = {}) {
   })
 }
 
+const pickEntry = function (target, { path, value }) {
+  return set(target, path, value)
+}
+
 // Remove values matching a query
 export const exclude = function (target, query, condition) {
-  const conditionA = shouldExclude.bind(undefined, condition)
-  return reduceParents(excludeEntry, conditionA, {
+  return reduceParents(excludeEntry, condition, {
     target,
     newTarget: target,
     query,
@@ -50,44 +43,27 @@ const excludeEntry = function (target, { path }) {
   return remove(target, path)
 }
 
-const shouldExclude = function (condition, { path, query, missing }, target) {
-  const value = get(target, path)
-  return condition({ path, query, value, missing })
-}
-
 // Modify a target object multiple times for each matched property.
-// Ignore properties when one of their ancestors was matched too.
-// Uses `iterate()` to keep memory consumption low.
 const reduceParents = function (
   setFunc,
   condition,
   { target, newTarget, query, sort },
 ) {
-  const paths = []
-
-  // eslint-disable-next-line fp/no-loops
-  for (const entry of iterate(target, query, { childFirst: false, sort })) {
-    // eslint-disable-next-line max-depth
-    if (shouldUseEntry({ entry, paths, newTarget, condition })) {
-      // eslint-disable-next-line fp/no-mutating-methods
-      paths.push(entry.path)
-      // eslint-disable-next-line fp/no-mutation, no-param-reassign
-      newTarget = setFunc(newTarget, entry, 0)
-    }
-  }
-
-  return newTarget
+  const entries = list(target, query, { childFirst: false, sort })
+  return entries
+    .filter((entry) => shouldUseEntry(entry, target, condition))
+    .filter(hasNoParentSet)
+    .reduce((targetA, entry) => setFunc(targetA, entry, 0), newTarget)
 }
 
-const shouldUseEntry = function ({ entry, paths, newTarget, condition }) {
-  return (
-    !entry.missing &&
-    !parentIsSet(paths, entry.path) &&
-    condition(entry, newTarget)
-  )
+const shouldUseEntry = function (entry, target, condition) {
+  return !entry.missing && condition(entry, target)
 }
 
 // If both a parent and a child property are set, the parent prevails
-const parentIsSet = function (paths, path) {
-  return paths.some((previousPath) => isParentPath(previousPath, path))
+const hasNoParentSet = function ({ path: pathA }, indexA, entries) {
+  return entries.every(
+    ({ path: pathB }, indexB) =>
+      indexA <= indexB || !isParentPath(pathB, pathA),
+  )
 }
