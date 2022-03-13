f6c25b7ad7efb153b2f8c032c2a6db9e25e3ab3c
ehmicky <ehmicky@gmail.com>
2022-03-13 21:08:27 +0100

    Rename file

--- src/config/plugin/lib/shared.js
+++ src/config/plugin/lib/shared.js
@@ -1,5 +1,5 @@
 import { parseQuery } from '../../normalize/lib/wild_wild_path_parser/main.js'
-import { pick } from '../../normalize/lib/wild_wild_path_utils/main.js'
+import { pick } from '../../normalize/lib/wild_wild_utils/main.js'
 
 // Retrieve top-level properties that are shared with all plugins of a specific
 // type. Those are merged with plugin-specific properties.
