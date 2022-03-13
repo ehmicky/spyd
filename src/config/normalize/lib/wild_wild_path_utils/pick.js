66bf00e198b044eee26a191280ab833816e69acf
ehmicky <ehmicky@gmail.com>
2022-03-13 11:58:17 +0100

    Improve `include()`

--- src/config/normalize/lib/wild_wild_path_utils/pick.js
+++ src/config/normalize/lib/wild_wild_path_utils/pick.js
@@ -1,6 +1,6 @@
 import { set } from '../wild_wild_path/main.js'
 
-import { listExisting, reduceParents } from './common.js'
+import { reduceParents } from './common.js'
 
 // Returns an object with only the properties being queried.
 export const pick = function (target, queryOrPaths) {
@@ -17,17 +17,12 @@ const pickEntry = function (target, { path, value }) {
 }
 
 // Remove values matching a query
-export const include = function (target, queryOrPath, condition) {
-  const entries = listExisting(target, queryOrPath)
-  return entries.reduce(includeEntry.bind(undefined, condition), {})
-}
-
-const includeEntry = function (
-  condition,
-  target,
-  { path, query, value, missing },
-) {
-  return condition({ path, query, value, missing })
-    ? set(target, path, value)
-    : target
+export const include = function (target, queryOrPaths, condition) {
+  return reduceParents({
+    target,
+    newTarget: {},
+    queryOrPaths,
+    setFunc: pickEntry,
+    condition,
+  })
 }
