0e9503f7b4a6acb1a662c4c8d59d398b0110e9be
ehmicky <ehmicky@gmail.com>
2022-03-20 11:15:40 +0100

    Improve performance

--- src/config/normalize/lib/wild_wild_utils/merge.js
+++ src/config/normalize/lib/wild_wild_utils/merge.js
@@ -1,22 +1,46 @@
 import { map } from './map.js'
 
 // eslint-disable-next-line max-params
-const pushUnshift = function (mapFunc, target, query, newValue, opts = {}) {
-  return map(target, query, (value) => mapFunc(value, newValue, opts), {
+const pushUnshift = function (mapFunc, target, query, newValues, opts = {}) {
+  return map(target, query, (value) => mapFunc(value, newValues, opts), {
     ...opts,
     entries: false,
   })
 }
 
-const pushValue = function (value, newValue) {
-  return Array.isArray(value) ? [...value, ...newValue] : newValue
+const pushValue = function (value, newValues, { mutate }) {
+  if (!Array.isArray(value)) {
+    return newValues
+  }
+
+  if (!mutate) {
+    return [...value, ...newValues]
+  }
+
+  newValues.forEach((newValue) => {
+    // eslint-disable-next-line fp/no-mutating-methods
+    value.push(newValue)
+  })
+  return value
 }
 
 // Like `set()` but push an array of values to the target array instead
 export const push = pushUnshift.bind(undefined, pushValue)
 
-const unshiftValue = function (value, newValue) {
-  return Array.isArray(value) ? [...newValue, ...value] : newValue
+const unshiftValue = function (value, newValues, { mutate }) {
+  if (!Array.isArray(value)) {
+    return newValues
+  }
+
+  if (!mutate) {
+    return [...newValues, ...value]
+  }
+
+  newValues.forEach((newValue) => {
+    // eslint-disable-next-line fp/no-mutating-methods
+    value.unshift(newValue)
+  })
+  return value
 }
 
 // Like `push()` but from the beginning
