d671bbea7130e1a3c7a89c7abcd86dfdc1ddf477
ehmicky <ehmicky@gmail.com>
2022-03-20 23:54:14 +0100

    Move file

--- src/config/normalize/lib/wild_wild_parser/main.js
+++ src/config/normalize/lib/wild_wild_parser/main.js
@@ -5,6 +5,6 @@ export {
   isParentPath,
 } from './compare.js'
 export { normalizeQuery, normalizePath } from './normalize.js'
-export { parseQuery, parsePath } from './parse.js'
+export { parseQuery, parsePath } from './parse/main.js'
 export { serializeQuery, serializePath } from './serialize.js'
 export { getTokenType } from './tokens/main.js'
--- src/config/normalize/lib/wild_wild_parser/parse/main.js
+++ src/config/normalize/lib/wild_wild_parser/parse/main.js
@@ -4,11 +4,11 @@ import {
   ARRAY_SEPARATOR_NAME,
   TOKEN_SEPARATOR,
   SPECIAL_CHARS,
-} from './tokens/escape.js'
-import { getStringTokenType } from './tokens/main.js'
-import { normalizeArraysPath } from './validate/path.js'
-import { validateEmptyQuery, validateQueryString } from './validate/string.js'
-import { throwQueryError } from './validate/throw.js'
+} from '../tokens/escape.js'
+import { getStringTokenType } from '../tokens/main.js'
+import { normalizeArraysPath } from '../validate/path.js'
+import { validateEmptyQuery, validateQueryString } from '../validate/string.js'
+import { throwQueryError } from '../validate/throw.js'
 
 // Parse a query string into an array of tokens.
 // Also validate and normalize it.
