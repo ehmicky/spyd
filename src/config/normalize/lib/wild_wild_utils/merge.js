015095646ddf7d02c0cdc43b65240eaebca84504
ehmicky <ehmicky@gmail.com>
2022-03-20 23:59:51 +0100

    Refactoring

--- src/config/normalize/lib/wild_wild_utils/merge.js
+++ src/config/normalize/lib/wild_wild_utils/merge.js
@@ -1,23 +1,23 @@
 import { map } from './map.js'
 
-// Like `set()` but push an array of values to the target array instead
 // eslint-disable-next-line max-params
-export const push = function (target, query, newValue, opts = {}) {
-  const mapFunc = pushValue.bind(undefined, newValue)
-  return map(target, query, mapFunc, { ...opts, entries: false })
+const pushUnshift = function (mapFunc, target, query, newValue, opts = {}) {
+  return map(target, query, mapFunc.bind(undefined, newValue), {
+    ...opts,
+    entries: false,
+  })
 }
 
 const pushValue = function (newValue, value) {
   return Array.isArray(value) ? [...value, ...newValue] : newValue
 }
 
-// Like `push()` but from the beginning
-// eslint-disable-next-line max-params
-export const unshift = function (target, query, newValue, opts = {}) {
-  const mapFunc = unshiftValue.bind(undefined, newValue)
-  return map(target, query, mapFunc, { ...opts, entries: false })
-}
+// Like `set()` but push an array of values to the target array instead
+export const push = pushUnshift.bind(undefined, pushValue)
 
 const unshiftValue = function (newValue, value) {
   return Array.isArray(value) ? [...newValue, ...value] : newValue
 }
+
+// Like `push()` but from the beginning
+export const unshift = pushUnshift.bind(undefined, unshiftValue)
