9dafb900ed788a0d7a62cc81d80872d9d77c0190
ehmicky <ehmicky@gmail.com>
2022-03-20 11:18:12 +0100

    Add merge()

--- src/config/normalize/lib/wild_wild_utils/merge.js
+++ src/config/normalize/lib/wild_wild_utils/merge.js
@@ -1,14 +1,17 @@
+import deepmerge from 'deepmerge'
+import isPlainObj from 'is-plain-obj'
+
 import { map } from './map.js'
 
 // eslint-disable-next-line max-params
 const pushUnshift = function (mapFunc, target, query, newValues, opts = {}) {
-  return map(target, query, (value) => mapFunc(value, newValues, opts.mutate), {
+  return map(target, query, (value) => mapFunc(value, newValues, opts), {
     ...opts,
     entries: false,
   })
 }
 
-const pushValue = function (value, newValues, mutate) {
+const pushValue = function (value, newValues, { mutate }) {
   if (!Array.isArray(value)) {
     return newValues
   }
@@ -27,7 +30,7 @@ const pushValue = function (value, newValues, mutate) {
 // Like `set()` but push an array of values to the target array instead
 export const push = pushUnshift.bind(undefined, pushValue)
 
-const unshiftValue = function (value, newValues, mutate) {
+const unshiftValue = function (value, newValues, { mutate }) {
   if (!Array.isArray(value)) {
     return newValues
   }
@@ -45,3 +48,49 @@ const unshiftValue = function (value, newValues, mutate) {
 
 // Like `push()` but from the beginning
 export const unshift = pushUnshift.bind(undefined, unshiftValue)
+
+// Only own properties are currently merged, even if `inherited` is `true`.
+// Non-enumerable properties are ignored.
+const mergeValue = function (value, newValue, { mutate, classes, deep }) {
+  const isMergeableObject = getIsMergeableObject(classes)
+  return deep
+    ? deepmerge(value, newValue, { clone: !mutate, isMergeableObject })
+    : shallowMergeValue({ value, newValue, mutate, isMergeableObject })
+}
+
+const shallowMergeValue = function ({
+  value,
+  newValue,
+  mutate,
+  isMergeableObject,
+}) {
+  if (!isMergeableObject(value)) {
+    return newValue
+  }
+
+  if (!mutate) {
+    return { ...value, ...newValue }
+  }
+
+  Object.keys(newValue).forEach((key) => {
+    // eslint-disable-next-line no-param-reassign, fp/no-mutation
+    value[key] = newValue[key]
+  })
+  return value
+}
+
+// Use similar recursion logic as `iterate()` depending on `classes`
+const getIsMergeableObject = function (classes) {
+  return classes ? isObjArr : isPlainObjArr
+}
+
+const isObjArr = function (value) {
+  return typeof value === 'object' && value !== null
+}
+
+const isPlainObjArr = function (value) {
+  return isPlainObj(value) || Array.isArray(value)
+}
+
+// Merge object values
+export const merge = pushUnshift.bind(undefined, mergeValue)
