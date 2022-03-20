d3a701749ae289bac868e7719e338e89a40147e2
ehmicky <ehmicky@gmail.com>
2022-03-20 23:59:25 +0100

    Add push()

---
+++ src/config/normalize/lib/wild_wild_utils/merge.js
@@ -0,0 +1,12 @@
+import { map } from './map.js'
+
+// Like `set()` but push an array of values to the target array instead
+// eslint-disable-next-line max-params
+export const push = function (target, query, newValue, opts = {}) {
+  const mapFunc = pushValue.bind(undefined, newValue)
+  return map(target, query, mapFunc, { ...opts, entries: false })
+}
+
+const pushValue = function (newValue, value) {
+  return Array.isArray(value) ? [...value, ...newValue] : newValue
+}
