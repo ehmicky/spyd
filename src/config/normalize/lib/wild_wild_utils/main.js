35ff0d18ad12af34eaaffd9af1e79c2ac7a1925c
ehmicky <ehmicky@gmail.com>
2022-03-20 23:59:30 +0100

    Add unshift()

--- src/config/normalize/lib/wild_wild_utils/main.js
+++ src/config/normalize/lib/wild_wild_utils/main.js
@@ -1,4 +1,4 @@
 export { find } from './find.js'
 export { pick, include, exclude } from './include/main.js'
 export { map } from './map.js'
-export { push } from './merge.js'
+export { push, unshift } from './merge.js'
--- src/config/normalize/lib/wild_wild_utils/merge.js
+++ src/config/normalize/lib/wild_wild_utils/merge.js
@@ -10,3 +10,14 @@ export const push = function (target, query, newValue, opts = {}) {
 const pushValue = function (newValue, value) {
   return Array.isArray(value) ? [...value, ...newValue] : newValue
 }
+
+// Like `push()` but from the beginning
+// eslint-disable-next-line max-params
+export const unshift = function (target, query, newValue, opts = {}) {
+  const mapFunc = unshiftValue.bind(undefined, newValue)
+  return map(target, query, mapFunc, { ...opts, entries: false })
+}
+
+const unshiftValue = function (newValue, value) {
+  return Array.isArray(value) ? [...newValue, ...value] : newValue
+}
