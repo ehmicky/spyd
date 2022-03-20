d05a00569f2d7cc011d521c844badaa200502d33
ehmicky <ehmicky@gmail.com>
2022-03-20 11:47:40 +0100

    Use wild-wild-path for recursion

--- src/config/empty.js
+++ src/config/empty.js
@@ -1,33 +1,13 @@
-import filterObj from 'filter-obj'
-import isPlainObj from 'is-plain-obj'
-
-import { mapValues } from '../utils/map.js'
+import { exclude } from './normalize/lib/wild_wild_utils/main.js'
 
 // We remove `null` values so that those can be used to unset properties from
 // shared configurations.
 // We remove `undefined` values so `Object.keys()` does not show them and to
 // make printing configuration nicer.
 export const removeEmptyValues = function (object) {
-  const objectA = removeEmptyChildren(object)
-  return mapValues(objectA, removeEmptyChildren, { deep: true })
+  return exclude(object, '**', isEmptyValue)
 }
 
-const removeEmptyChildren = function (value) {
-  if (isPlainObj(value)) {
-    return filterObj(value, isNotEmptyPair)
-  }
-
-  if (Array.isArray(value)) {
-    return value.filter(isNotEmptyValue)
-  }
-
-  return value
-}
-
-const isNotEmptyPair = function (key, value) {
-  return isNotEmptyValue(value)
-}
-
-const isNotEmptyValue = function (value) {
-  return value !== null && value !== undefined
+const isEmptyValue = function (value) {
+  return value === null || value === undefined
 }
