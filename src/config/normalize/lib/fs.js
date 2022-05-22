ec7a8418eeaaa716852085e115d0ac2d8c974c3f
ehmicky <ehmicky@gmail.com>
2022-05-22 17:39:08 +0200

    Remove dead code

--- src/config/normalize/lib/fs.js
+++ src/config/normalize/lib/fs.js
@@ -1,15 +1,9 @@
 import { pathExists } from 'path-exists'
-import { isFile, isDirectory } from 'path-type'
+import { isDirectory } from 'path-type'
 
 export const validateFileExists = async function (value) {
   if (!(await pathExists(value))) {
     throw new Error('must be an existing file.')
-  }
-}
-
-export const validateRegularFile = async function (value) {
-  if ((await pathExists(value)) && !(await isFile(value))) {
-    throw new Error('must be a regular file.')
   }
 }
 
