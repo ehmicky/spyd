cff11907bc880f1dba1ce77d038da822231ba4b1
ehmicky <ehmicky@gmail.com>
2022-05-22 11:59:09 +0200

    Fix comments

--- src/config/normalize/lib/cwd.js
+++ src/config/normalize/lib/cwd.js
@@ -8,9 +8,9 @@ import { wrapError } from '../../../error/wrap.js'
 import { callValueFunc, callUserFunc } from './call.js'
 import { checkPath } from './path.js'
 
-// A `cwd(opts)` option can be specified to customize the `cwd`.
-//  - The default value is `.`, not some `process.cwd()` evaluated at load time,
-//    to ensure it is evaluated at runtime.
+// A `cwd[(opts)]` option can be specified to customize the `cwd`.
+//  - The default value is `.`, not `process.cwd()`, to ensure it is evaluated
+//    at runtime, not load time.
 export const getCwd = async function ({ cwd = DEFAULT_CWD, opts }) {
   const cwdA = await callUserFunc(cwd, opts)
   await callValueFunc(checkCwd, cwdA, opts)
