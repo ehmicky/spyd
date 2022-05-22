d9ef5d0c4f42fb26e01096caa961092f2797ce0e
ehmicky <ehmicky@gmail.com>
2022-05-22 15:52:20 +0200

    Fix linting

--- src/runners/plugin/shared.js
+++ src/runners/plugin/shared.js
@@ -1,8 +1,8 @@
 import { DEFAULT_TASKS } from '../../combination/tasks/find.js'
 import { amongCommands } from '../../config/normalize/pick.js'
 import { normalizeArray } from '../../config/normalize/transform.js'
-import { validateJson } from '../../config/normalize/validate/type.js'
 import { validateRegularFile } from '../../config/normalize/validate/fs.js'
+import { validateJson } from '../../config/normalize/validate/type.js'
 import { normalizeConfigSelectors } from '../../config/select/normalize.js'
 
 const pick = amongCommands(['dev', 'run'])
