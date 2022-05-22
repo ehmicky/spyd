f4fef7fc08fd21b143c88e2af64f0472cfb5a385
ehmicky <ehmicky@gmail.com>
2022-05-22 11:59:44 +0200

    Add comment

--- src/config/normalize/lib/prefix.js
+++ src/config/normalize/lib/prefix.js
@@ -1,7 +1,7 @@
 import { callUserFunc } from './call.js'
 
 // The `prefix` option is the name of the type of property to show in error
-// message such as "Option".
+// message and warnings such as "Option".
 export const getPrefix = async function (prefix, opts) {
   const prefixA = await callUserFunc(prefix, opts)
   return prefixA === undefined ? DEFAULT_PREFIX : String(prefixA).trim()
