74ec2e2eae51f64ec15fd352f6c450d84d1a7469
ehmicky <ehmicky@gmail.com>
2022-05-22 11:59:21 +0200

    Improve comments

--- src/config/normalize/lib/apply.js
+++ src/config/normalize/lib/apply.js
@@ -48,7 +48,7 @@ export const applyRule = async function (
   return { value: valueC, renamedPath, newPaths, warnings }
 }
 
-// Apply `pick(value, opts)` which omits the current value if `false` is
+// Apply `pick[(value, opts)]` which omits the current value if `false` is
 // returned. It also skips the current rule.
 // For example, this is useful when several commands share some properties but
 // not all.
@@ -56,7 +56,7 @@ const againstPick = async function (value, pick, opts) {
   return pick !== undefined && !(await callValueFunc(pick, value, opts))
 }
 
-// Apply `condition(value, opts)` which skips the current rule if `false`
+// Apply `condition[(value, opts)]` which skips the current rule if `false`
 // is returned.
 const againstCondition = async function (value, condition, opts) {
   return (
@@ -64,12 +64,13 @@ const againstCondition = async function (value, condition, opts) {
   )
 }
 
-// Apply `compute(opts)` which sets a value from the system, instead of the user
+// Apply `compute[(opts)]` which sets a value from the system, instead of the
+// user
 const computeValue = async function (value, compute, opts) {
   return compute === undefined ? value : await callUserFunc(compute, opts)
 }
 
-// Apply `default(opts)` which assigns a default value
+// Apply `default[(opts)]` which assigns a default value
 const addDefaultValue = async function (value, defaultValue, opts) {
   return value === undefined ? await callUserFunc(defaultValue, opts) : value
 }
