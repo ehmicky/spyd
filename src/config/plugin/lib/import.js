775188b790dd9558c3a3c580a1a94b02d2699351
ehmicky <ehmicky@gmail.com>
2022-05-29 13:21:40 +0200

    Use default exports with plugins

--- src/config/plugin/lib/import.js
+++ src/config/plugin/lib/import.js
@@ -1,22 +1,24 @@
 import { pathToFileURL } from 'url'
 
+import isPlainObj from 'is-plain-obj'
+
 import { wrapError } from '../../../error/wrap.js'
 
 import { PluginError } from './error.js'
 
 // Builtin modules are lazy loaded for performance reasons.
-// The return value is shallow cloned to make it a plain object instead of
-// a dynamic `Module` instance.
-//  - It also ensure plugins of same location and type but different configs do
-//    not share the same top-level properties. However, they will share deep
-//    properties by reference.
+// We shallow clone it the return value to ensure plugins of same location and
+// type but different configs do not share the same top-level properties.
+//   - However, they will share deep properties by reference.
 export const importPlugin = async function (
   location,
   locationType,
   { name, builtins },
 ) {
   try {
-    return await IMPORTERS[locationType](location, builtins)
+    const { plugin, path } = await IMPORTERS[locationType](location, builtins)
+    const pluginA = isPlainObj(plugin) ? { ...plugin } : plugin
+    return { plugin: pluginA, path }
   } catch (error) {
     throw wrapError(error, `Could not load "${name}"\n`, PluginError)
   }
@@ -26,15 +28,31 @@ const importInline = function (location) {
   return { plugin: location }
 }
 
+// We do not assume `builtins` methods are calling `import()`: they could return
+// the plugin object as is.
+// However, they are likely to call `import()`, therefore we handle a `default`
+// import too.
 const importBuiltin = async function (location, builtins) {
   const builtinPlugin = await builtins[location]()
-  return { plugin: { ...builtinPlugin } }
+  const plugin = isPlainObj(builtinPlugin.default)
+    ? builtinPlugin.default
+    : builtinPlugin
+  return { plugin }
 }
 
+// We enforce default exports because:
+//  - It allows plugin to make other exports
+//  - It does not require wildcard imports when importing the plugin
+//    programmatically
 const importPath = async function (location) {
   // eslint-disable-next-line import/no-dynamic-require
-  const plugin = await import(pathToFileURL(location))
-  return { plugin: { ...plugin }, path: location }
+  const { default: plugin } = await import(pathToFileURL(location))
+
+  if (plugin === undefined) {
+    throw new PluginError('The plugin must use a default export.')
+  }
+
+  return { plugin, path: location }
 }
 
 const IMPORTERS = {
