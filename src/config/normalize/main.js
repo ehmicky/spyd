b2978d4369b07937735898be5e66ad8e5edaf2b8
ehmicky <ehmicky@gmail.com>
2022-05-29 19:50:45 +0200

    Simplify code

--- src/config/normalize/main.js
+++ src/config/normalize/main.js
@@ -5,19 +5,15 @@ import { normalizeInputs } from './lib/main.js'
 
 // Normalize the configuration properties, including default values and
 // validation.
-export const normalizeConfig = async function (
-  config,
-  rules,
-  { UserErrorType = UserError, all = {}, ...opts },
-) {
+export const normalizeConfig = async function (config, rules, opts) {
   const { inputs, error } = await normalizeInputs(config, rules, {
     ...opts,
-    all: { ...all, prefix: PREFIX },
+    all: { ...opts.all, prefix: PREFIX },
     soft: true,
   })
 
   if (error) {
-    throw wrapError(error, '', UserErrorType)
+    throw wrapError(error, '', UserError)
   }
 
   return inputs
