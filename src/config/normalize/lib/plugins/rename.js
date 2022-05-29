fc9cb7597b86ea5aab85a3e5fcbb088ed1b3c376
ehmicky <ehmicky@gmail.com>
2022-05-29 23:45:19 +0200

    Remove comment

--- src/config/normalize/lib/plugins/rename.js
+++ src/config/normalize/lib/plugins/rename.js
@@ -4,9 +4,6 @@ export const input = true
 
 // Apply `rename[(value, opts)]` which transforms the property's name.
 // This can be used for aliasing and deprecation.
-// TODO: This is only applied if the destination value is `undefined`.
-// Like this, if both an old alias and a new one are specified, the new one
-// has priority.
 export const main = function (rename) {
   return { rename }
 }
