4846644821ec96fc9691167a293cc7008f07281a
ehmicky <ehmicky@gmail.com>
2022-05-29 23:35:49 +0200

    Improve validate()

--- src/config/normalize/lib/plugins/validate.js
+++ src/config/normalize/lib/plugins/validate.js
@@ -1,8 +1,4 @@
+// Apply `validate(value, opts)` which throws on validation errors
 export const name = 'validate'
 
 export const input = true
-
-// Apply `validate(value, opts)` which throws on validation errors
-export const main = async function (validate, value) {
-  await validate(value)
-}
