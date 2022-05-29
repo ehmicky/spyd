83282b39c1a5fb97dcace41a40be834755213073
ehmicky <ehmicky@gmail.com>
2022-05-29 11:52:03 +0200

    Add comment

--- src/config/normalize/lib/keywords/list/required.js
+++ src/config/normalize/lib/keywords/list/required.js
@@ -2,7 +2,8 @@ export const name = 'required'
 // eslint-disable-next-line unicorn/no-null
 export const undefinedInput = null
 
-// Apply `required[(opts)]` which throws if `true` and input is `undefined`
+// Apply `required[(opts)]` which throws if `true` and input is `undefined`.
+// On `array.*`, this validates against sparse arrays.
 export const main = function (definition) {
   if (definition) {
     throw new Error('must be defined.')
