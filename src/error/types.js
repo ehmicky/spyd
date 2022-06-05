9ff31a8e29c2e745ca4c458693a9a765ec3db180
ehmicky <ehmicky@gmail.com>
2022-06-05 11:43:11 +0200

    Cleanup code

--- src/error/types.js
+++ src/error/types.js
@@ -1,11 +1,11 @@
+import { mergeErrorCause } from './merge/main.js'
 import { normalizeError } from './normalize/main.js'
-import { wrapError } from './wrap.js'
 
 // Ensure an error is among a specific set of error types.
 // Otherwise, assign a default error type (`errorTypes[0]`).
-export const allowErrorTypes = function (error, errorTypes) {
+export const allowErrorTypes = function (error, ErrorTypes) {
   const errorA = normalizeError(error)
-  return errorTypes.some((errorType) => errorA instanceof errorType)
+  return ErrorTypes.some((ErrorType) => errorA instanceof ErrorType)
     ? errorA
-    : wrapError(errorA, '', errorTypes[0])
+    : mergeErrorCause(new ErrorTypes[0]('', { cause: errorA }))
 }
