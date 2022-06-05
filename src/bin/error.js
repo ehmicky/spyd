4a6097ad8b5cb6bbcdaa4193b86873ad7d33a107
ehmicky <ehmicky@gmail.com>
2022-06-05 12:00:41 +0200

    Add TODO

--- src/bin/error.js
+++ src/bin/error.js
@@ -4,6 +4,7 @@ import { normalizeError } from '../error/normalize/main.js'
 import { addPadding } from '../report/utils/indent.js'
 
 // Print CLI errors and exit, depending on the error type
+// TODO: allow error types to not print anything
 export const handleCliError = function (error) {
   const errorA = normalizeError(error)
   const { exitCode, stack, indented } = ERROR_PROPS[getErrorName(errorA)]
