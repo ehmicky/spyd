684c80e39469fbedaafb925d039663871883296a
ehmicky <ehmicky@gmail.com>
2022-06-05 17:24:41 +0200

    Refactoring

--- src/error/modern/main.js
+++ src/error/modern/main.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line n/file-extension-in-import, import/no-unassigned-import
 import 'error-cause/auto'
-import errorType from 'error-type'
 
 import { onError } from './handler.js'
 import { getOpts } from './opts.js'
-import { createSystemError } from './system.js'
+import {
+  validateErrorNames,
+  createSystemError,
+  createErrorTypes,
+} from './types.js'
 
 // Create error types by passing an array of error names.
 // Also returns an `onError(error) => error` function to use as a top-level
@@ -17,22 +20,14 @@ import { createSystemError } from './system.js'
 //  - Chrome: `node-source-map-support`
 //  - Any browsers: `stacktrace.js`
 export const modernErrors = function (errorNames, opts) {
+  validateErrorNames(errorNames)
   const { onCreate } = getOpts(opts)
-  const SystemError = createSystemError(errorNames)
-  const ErrorTypes = getErrorTypes(errorNames, onCreate)
+  const SystemError = createSystemError()
+  const ErrorTypes = createErrorTypes(errorNames, onCreate)
   const onErrorHandler = onError.bind(
     undefined,
     Object.values(ErrorTypes),
     SystemError,
   )
   return { ...ErrorTypes, onError: onErrorHandler }
-}
-
-// Custom error `onCreate()` logic can be specified
-//  - To make it type-specific, an object of functions should be used, then
-//    `object[error.name]` should be used inside `onCreate()`
-const getErrorTypes = function (errorNames, onCreate) {
-  return Object.fromEntries(
-    errorNames.map((errorName) => [errorName, errorType(errorName, onCreate)]),
-  )
 }
