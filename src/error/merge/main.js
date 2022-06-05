3db449b494d433358abb169a4a8c1e9b04d6b5ea
ehmicky <ehmicky@gmail.com>
2022-06-05 11:21:48 +0200

    Split file

--- src/error/merge/main.js
+++ src/error/merge/main.js
@@ -1,6 +1,7 @@
 import { normalizeError } from '../normalize/main.js'
 import { setErrorProperty } from '../normalize/set.js'
 
+import { createError } from './create.js'
 import { mergeMessage } from './message.js'
 import { copyProps } from './props.js'
 import { hasStack, fixStack } from './stack.js'
@@ -57,51 +58,6 @@ const mergeCause = function (parent, parentErrors, hasChildStack) {
   mergeAggregate(mergedError, parentErrors, child)
   copyProps(mergedError, parent, child)
   return normalizeError(mergedError)
-}
-
-// Ensure both the prototype and `error.name` are correct, by creating a new
-// instance with the right constructor.
-// The parent error's type has priority unless:
-//  - It is Error|AggregateError, which allows wrapping an error message or
-//    properties without changing its type
-//  - Its constructor throws
-// If both parent and child constructors throw, we default to Error.
-// If both parent and child are Error|AggregateError, we privilege
-// AggregateError if any is an instance, since the `errors` property should only
-// be used on AggregateError.
-const createError = function (parent, child, message) {
-  if (isSimpleErrorType(parent)) {
-    return createCauseError(parent, child, message)
-  }
-
-  try {
-    return new parent.constructor(message)
-  } catch {
-    return createCauseError(parent, child, message)
-  }
-}
-
-const createCauseError = function (parent, child, message) {
-  if (isSimpleErrorType(child)) {
-    return createSimpleErrorType(parent, child, message)
-  }
-
-  try {
-    return new child.constructor(message)
-  } catch {
-    return createSimpleErrorType(parent, child, message)
-  }
-}
-
-const isSimpleErrorType = function (error) {
-  return error.constructor === Error || error.constructor === AggregateError
-}
-
-const createSimpleErrorType = function (parent, child, message) {
-  return parent.constructor === AggregateError ||
-    child.constructor === AggregateError
-    ? new AggregateError([], message)
-    : new Error(message)
 }
 
 // Keep `error.errors` when merging errors.
