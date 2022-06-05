929b7fb6de8f424fe9348275f9f5fbb67af0b56e
ehmicky <ehmicky@gmail.com>
2022-06-05 23:25:19 +0200

    Refactoring

--- src/report/reporters/plugin/shared.js
+++ src/report/reporters/plugin/shared.js
@@ -11,121 +11,99 @@ import { isTtyOutput } from '../../tty.js'
 
 const pick = amongCommands(['remove', 'run', 'show'])
 
-const any = {
-  name: '**',
-  pick,
-  validate: validateJson,
-}
-
-// The reporter's output is decided by (in priority order):
-//  - `config.reporter.output` (user-defined, reporter-specific)
-//  - `config.output` (user-defined, any reporters): merged in a previous step
-//  - `reporter.defaultOutput` (reporter-defined, reporter-specific)
-//  - "stdout" (system-defined, any reporters)
-// The reporter's output also determines the format.
-const output = {
-  name: 'output',
-  pick,
-  default({
-    context: {
-      plugin: { defaultOutput },
-    },
-  }) {
-    return defaultOutput
-  },
-  path: normalizeOutputPath,
-}
-
-const format = {
-  name: 'format',
-  pick,
-  compute: computeFormat,
-}
-
-const outputFormat = {
-  name: 'output',
-  pick,
-  validate: validateOutputFormat,
-}
-
-// `reporter.config.tty` is `true` when output is interactive terminal.
-// Several reporter's configuration properties default to `true` only when
-// the output is an interactive terminal.
-const tty = {
-  name: 'tty',
-  pick,
-  compute({ inputs }) {
-    return inputs.output === 'stdout' && isTtyOutput()
-  },
-}
-
-const colors = {
-  name: 'colors',
-  pick,
-  default({ inputs }) {
-    return inputs.tty
-  },
-  schema: { type: 'boolean' },
-}
-
-const quiet = {
-  name: 'quiet',
-  pick: amongCommands(['run']),
-  default({ inputs }) {
-    return !inputs.tty
-  },
-  schema: { type: 'boolean' },
-}
-
-const showDiff = {
-  name: 'showDiff',
-  pick,
-  default({ inputs }) {
-    return inputs.tty
-  },
-  schema: { type: 'boolean' },
-}
-
-const showMetadata = {
-  name: 'showMetadata',
-  pick,
-  default: getShowMetadataDefault,
-  schema: { type: 'boolean' },
-}
-
-const showPrecision = {
-  name: 'showPrecision',
-  pick,
-  default: DEFAULT_SHOW_PRECISION,
-  schema: { type: 'boolean' },
-}
-
-const showSystem = {
-  name: 'showSystem',
-  pick,
-  schema: { type: 'boolean' },
-}
-
-const showTitles = {
-  name: 'showTitles',
-  pick,
-  default: DEFAULT_SHOW_TITLES,
-  schema: { type: 'boolean' },
-}
-
 // Reporter-specific shared configuration properties
-export const shared = normalizeConfigSelectors([
-  any,
-  output,
-  format,
-  outputFormat,
-  tty,
-  colors,
-  quiet,
-  showDiff,
-  showMetadata,
-  showPrecision,
-  showSystem,
-  showTitles,
-])
+const sharedRules = [
+  {
+    name: '**',
+    pick,
+    validate: validateJson,
+  },
+  // The reporter's output is decided by (in priority order):
+  //  - `config.reporter.output` (user-defined, reporter-specific)
+  //  - `config.output` (user-defined, any reporters): merged in a previous step
+  //  - `reporter.defaultOutput` (reporter-defined, reporter-specific)
+  //  - "stdout" (system-defined, any reporters)
+  // The reporter's output also determines the format.
+  {
+    name: 'output',
+    pick,
+    default({
+      context: {
+        plugin: { defaultOutput },
+      },
+    }) {
+      return defaultOutput
+    },
+    path: normalizeOutputPath,
+  },
+  {
+    name: 'format',
+    pick,
+    compute: computeFormat,
+  },
+  {
+    name: 'output',
+    pick,
+    validate: validateOutputFormat,
+  },
+  // `reporter.config.tty` is `true` when output is interactive terminal.
+  // Several reporter's configuration properties default to `true` only when
+  // the output is an interactive terminal.
+  {
+    name: 'tty',
+    pick,
+    compute({ inputs }) {
+      return inputs.output === 'stdout' && isTtyOutput()
+    },
+  },
+  {
+    name: 'colors',
+    pick,
+    default({ inputs }) {
+      return inputs.tty
+    },
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'quiet',
+    pick: amongCommands(['run']),
+    default({ inputs }) {
+      return !inputs.tty
+    },
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'showDiff',
+    pick,
+    default({ inputs }) {
+      return inputs.tty
+    },
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'showMetadata',
+    pick,
+    default: getShowMetadataDefault,
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'showPrecision',
+    pick,
+    default: DEFAULT_SHOW_PRECISION,
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'showSystem',
+    pick,
+    schema: { type: 'boolean' },
+  },
+  {
+    name: 'showTitles',
+    pick,
+    default: DEFAULT_SHOW_TITLES,
+    schema: { type: 'boolean' },
+  },
+]
+
+export const shared = normalizeConfigSelectors(sharedRules)
 /* eslint-enable max-lines */
--- src/runners/plugin/shared.js
+++ src/runners/plugin/shared.js
@@ -6,39 +6,33 @@ import { normalizeConfigSelectors } from '../../config/select/normalize.js'
 
 const pick = amongCommands(['dev', 'run'])
 
-const any = {
-  name: '**',
-  pick,
-  validate: validateJson,
-}
-
-const tasks = {
-  name: 'tasks',
-  pick,
-  default: DEFAULT_TASKS,
-  transform: normalizeArray,
-}
-
-const tasksAny = {
-  name: 'tasks.*',
-  required: true,
-  pick,
-  glob: true,
-  example: DEFAULT_TASKS,
-}
-
-const tasksFlatten = {
-  name: 'tasks',
-  pick,
-  transform(value) {
-    return value.flat()
-  },
-}
-
 // Runner-specific shared configuration properties
-export const shared = normalizeConfigSelectors([
-  any,
-  tasks,
-  tasksAny,
-  tasksFlatten,
-])
+const sharedRules = [
+  {
+    name: '**',
+    pick,
+    validate: validateJson,
+  },
+  {
+    name: 'tasks',
+    pick,
+    default: DEFAULT_TASKS,
+    transform: normalizeArray,
+  },
+  {
+    name: 'tasks.*',
+    required: true,
+    pick,
+    glob: true,
+    example: DEFAULT_TASKS,
+  },
+  {
+    name: 'tasks',
+    pick,
+    transform(value) {
+      return value.flat()
+    },
+  },
+]
+
+export const shared = normalizeConfigSelectors(sharedRules)
