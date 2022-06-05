2702ed41e677bb8b88bd5a1601c41759a42d2845
ehmicky <ehmicky@gmail.com>
2022-06-05 23:26:21 +0200

    Improve createErrorType()

--- src/error/create.js
+++ src/error/create.js
@@ -1,8 +1,12 @@
-// Create an error type with a specific `name`
+// Create an error type with a specific `name`.
+// The constructor allows setting either `error.cause` or any properties:
+// `new ErrorType('message', { anyProp: true })`
 export const createErrorType = function (name) {
   return class extends Error {
-    constructor(message, opts) {
-      super(message, opts)
+    constructor(message, { cause, ...props } = {}) {
+      super(message, { cause })
+      // eslint-disable-next-line fp/no-this, fp/no-mutating-assign
+      Object.assign(this, props)
       // eslint-disable-next-line fp/no-this, fp/no-mutation
       this.name = name
     }
