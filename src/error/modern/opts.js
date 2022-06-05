75bb72f29b73e50b583152da271b145cb642b591
ehmicky <ehmicky@gmail.com>
2022-06-05 17:25:29 +0200

    Start adding bugsUrl

--- src/error/modern/opts.js
+++ src/error/modern/opts.js
@@ -4,9 +4,10 @@ export const getOpts = function (opts = {}) {
     throw new TypeError(`Options must be a plain object: ${opts}`)
   }
 
-  const { onCreate } = opts
+  const { onCreate, bugsUrl } = opts
   validateOnCreate(onCreate)
-  return { onCreate }
+  const bugsUrlA = normalizeBugsUrl(bugsUrl)
+  return { onCreate, bugsUrl: bugsUrlA }
 }
 
 const isObject = function (value) {
@@ -18,3 +19,33 @@ const validateOnCreate = function (onCreate) {
     throw new TypeError(`"onCreate" option must be a function: ${onCreate}`)
   }
 }
+
+const normalizeBugsUrl = function (bugsUrl) {
+  if (bugsUrl === undefined) {
+    return
+  }
+
+  if (typeof bugsUrl !== 'string') {
+    throw new TypeError(`"bugsUrl" option must be a string: ${bugsUrl}`)
+  }
+
+  try {
+    return String(new URL(bugsUrl))
+  } catch (error) {
+    throw new Error(
+      `"bugsUrl" option "${bugsUrl}" must be ${getUrlError(error, bugsUrl)}`,
+    )
+  }
+}
+
+const getUrlError = function (error, bugsUrl) {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(bugsUrl, EXAMPLE_ORIGIN)
+    return 'an absolute URL.'
+  } catch {
+    return `a valid URL: ${error.message}.`
+  }
+}
+
+const EXAMPLE_ORIGIN = 'https://www.example.com'
