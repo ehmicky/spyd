1ca9850d1cd8bba619dcfe4326bee61a8a6a8a4b
ehmicky <ehmicky@gmail.com>
2022-06-05 11:21:39 +0200

    Split file

--- src/error/merge/main.js
+++ src/error/merge/main.js
@@ -2,6 +2,8 @@
 import { normalizeError } from '../normalize/main.js'
 import { setErrorProperty } from '../normalize/set.js'
 
+import { copyProps } from './props.js'
+
 // Merge `error.cause` recursively to a single error.
 // This allows consumers to conveniently wrap errors using:
 //   try {
@@ -212,43 +214,4 @@ const getMergedErrors = function (parentErrors, childErrors) {
 
   return [...childErrors, ...parentErrors]
 }
-
-// Merge error properties, shallowly, with parent error having priority
-const copyProps = function (mergedError, parent, child) {
-  mergeProps(mergedError, child)
-  mergeProps(mergedError, parent)
-}
-
-// Do not merge inherited properties nor non-enumerable properties.
-// Works with symbol properties.
-const mergeProps = function (mergedError, error) {
-  // eslint-disable-next-line guard-for-in, fp/no-loops
-  for (const propName in error) {
-    mergeProp(mergedError, error, propName)
-  }
-
-  // eslint-disable-next-line fp/no-loops
-  for (const propName of Object.getOwnPropertySymbols(error)) {
-    mergeProp(mergedError, error, propName)
-  }
-}
-
-const mergeProp = function (mergedError, error, propName) {
-  const descriptor = Object.getOwnPropertyDescriptor(error, propName)
-
-  if (descriptor !== undefined && !CORE_ERROR_PROPS.has(propName)) {
-    // eslint-disable-next-line fp/no-mutating-methods
-    Object.defineProperty(mergedError, propName, descriptor)
-  }
-}
-
-// Do not copy core error properties.
-// Does not assume they are not enumerable.
-const CORE_ERROR_PROPS = new Set([
-  'name',
-  'message',
-  'stack',
-  'cause',
-  'errors',
-])
 /* eslint-enable max-lines */
