0923a859e3524c54c0c65edc7aaba5c79598c245
ehmicky <ehmicky@gmail.com>
2022-06-05 23:41:53 +0200

    Fix bug

--- src/error/normalize/set.js
+++ src/error/normalize/set.js
@@ -1,7 +1,7 @@
 // Error properties are non-enumerable
 export const setErrorProperty = function (error, propName, value) {
   // eslint-disable-next-line fp/no-mutating-methods
-  Object.defineProperty(error, 'name', {
+  Object.defineProperty(error, propName, {
     value,
     writable: true,
     enumerable: false,
