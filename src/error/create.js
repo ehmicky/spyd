3df1ab220b231b7eeff8e788f57772781325e746
ehmicky <ehmicky@gmail.com>
2022-06-05 11:44:20 +0200

    Add `onCreate()`

--- src/error/create.js
+++ src/error/create.js
@@ -1,18 +1,32 @@
 // Create an error type with a specific `name`.
 // The constructor allows setting either `error.cause` or any properties:
 // `new ErrorType('message', { anyProp: true })`
-export const createErrorType = function (errorName) {
+export const createErrorType = function (
+  errorName,
+  onCreate = defaultOnCreate,
+) {
   const ErrorType = class extends Error {
     // eslint-disable-next-line no-unused-vars
-    constructor(message, { cause, name, ...props } = {}) {
+    constructor(message, { cause, name, ...opts } = {}) {
       const errorOpts = cause === undefined ? {} : { cause }
       super(message, errorOpts)
-      // eslint-disable-next-line fp/no-this, fp/no-mutating-assign
-      Object.assign(this, props)
+      // eslint-disable-next-line fp/no-this
+      onCreate(this, opts)
     }
   }
   setErrorName(ErrorType, errorName)
   return ErrorType
+}
+
+// `onCreate()` allows custom logic at initialization time.
+// The construction arguments are passed.
+//  - They can be validated, normalized, etc.
+//  - No third argument is passed. This enforces calling named parameters
+//    `new ErrorType('message', opts)` instead of positional ones, which is
+//    cleaner.
+const defaultOnCreate = function (error, opts) {
+  // eslint-disable-next-line fp/no-mutating-assign
+  Object.assign(error, opts)
 }
 
 // To mimic native error types and to print correctly with `util.inspect()`:
