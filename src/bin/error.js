b6582f0d57f4df02b49a46721bae2a7f4c9c1b90
ehmicky <ehmicky@gmail.com>
2022-06-05 12:00:24 +0200

    Rename variable

--- src/bin/error.js
+++ src/bin/error.js
@@ -6,8 +6,8 @@ import { addPadding } from '../report/utils/indent.js'
 // Print CLI errors and exit, depending on the error type
 export const handleCliError = function (error) {
   const errorA = normalizeError(error)
-  const { exitCode, printStack, indented } = ERROR_PROPS[getErrorName(errorA)]
-  const errorMessage = printStack ? errorA.stack : errorA.message
+  const { exitCode, stack, indented } = ERROR_PROPS[getErrorName(errorA)]
+  const errorMessage = stack ? errorA.stack : errorA.message
   const errorMessageA = indented ? addPadding(errorMessage) : errorMessage
   console.error(errorMessageA)
   process.exitCode = exitCode
@@ -19,12 +19,12 @@ const getErrorName = function ({ name }) {
 
 // Error type-specific behavior
 const ERROR_PROPS = {
-  CoreError: { exitCode: 5, printStack: true, indented: false },
-  PluginError: { exitCode: 4, printStack: true, indented: false },
-  UserCodeError: { exitCode: 3, printStack: true, indented: false },
-  UserError: { exitCode: 2, printStack: false, indented: false },
-  LimitError: { exitCode: 1, printStack: false, indented: false },
-  StopError: { exitCode: 0, printStack: false, indented: true },
+  CoreError: { exitCode: 5, stack: true, indented: false },
+  PluginError: { exitCode: 4, stack: true, indented: false },
+  UserCodeError: { exitCode: 3, stack: true, indented: false },
+  UserError: { exitCode: 2, stack: false, indented: false },
+  LimitError: { exitCode: 1, stack: false, indented: false },
+  StopError: { exitCode: 0, stack: false, indented: true },
 }
 
 const DEFAULT_ERROR_NAME = 'CoreError'
