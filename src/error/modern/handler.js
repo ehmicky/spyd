368184612b6796df0f8d8b2b7f8d1354a67411e5
ehmicky <ehmicky@gmail.com>
2022-06-05 17:23:38 +0200

    Simplify code

--- src/error/modern/handler.js
+++ src/error/modern/handler.js
@@ -1,17 +1,10 @@
 import mergeErrorCause from 'merge-error-cause'
-import normalizeException from 'normalize-exception'
 
 // Error handler that normalizes an error, merge its `error.cause` and ensure
 // its type is among an allowed list of types.
 // Otherwise, assign a default SystemError type.
 export const onError = function (ErrorTypes, SystemError, error) {
   const errorA = mergeErrorCause(error)
-  const errorB = allowErrorTypes(errorA, ErrorTypes, SystemError)
-  return errorB
-}
-
-const allowErrorTypes = function (error, ErrorTypes, SystemError) {
-  const errorA = normalizeException(error)
 
   if (ErrorTypes.some((ErrorType) => errorA instanceof ErrorType)) {
     return errorA
