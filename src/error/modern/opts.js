fdfc9ac04ccb58349d596489b70d1be0721a9c34
ehmicky <ehmicky@gmail.com>
2022-06-05 17:23:47 +0200

    Validate `opts`

--- src/error/modern/opts.js
+++ src/error/modern/opts.js
@@ -1,4 +1,13 @@
 // Normalize and retrieve options
-export const getOpts = function ({ onCreate } = {}) {
+export const getOpts = function (opts = {}) {
+  if (!isObject(opts)) {
+    throw new TypeError(`Options must be a plain object: ${opts}`)
+  }
+
+  const { onCreate } = opts
   return { onCreate }
+}
+
+const isObject = function (value) {
+  return typeof value === 'object' && value !== null
 }
